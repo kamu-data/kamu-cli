606ba2869767eccd5e2003ba827ce170fc91d3da
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2019-08-11 19:02:22 -0700

    Use public images

--- src/main/scala/dev/kamu/cli/external/DockerImages.scala
+++ src/main/scala/dev/kamu/cli/external/DockerImages.scala
@@ -1,13 +1,13 @@
 package dev.kamu.cli.external
 
 object DockerImages {
-  val SPARK = "kamu/spark:2.4.0_0.0.1"
-  val LIVY = "kamu/spark-py:2.4.0_0.0.1"
-  val JUPYTER = "kamu/jupyter:0.0.1"
+  val SPARK = "kamudata/spark-py-uber:2.4.0_0.0.1"
+  val LIVY = SPARK
+  val JUPYTER = "kamudata/jupyter-uber:0.0.1"
 
   val ALL = Array(
     SPARK,
     LIVY,
     JUPYTER
-  )
+  ).distinct
 }
