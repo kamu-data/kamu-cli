797b7cd30482862ce0c7ec9afb55057297320e07
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2021-10-02 20:03:13 -0700

    Implement progressive data ingest in test workspace script

--- tools/src/create_test_workspace.rs
+++ tools/src/create_test_workspace.rs
@@ -10,6 +10,7 @@
 #![feature(exit_status_error)]
 
 use std::{
+    io::BufRead,
     path::{Path, PathBuf},
     process::Command,
     str::FromStr,
@@ -25,35 +26,41 @@ fn main() {
 
     init_workspace(&work_dir, matches.is_present("force"));
 
-    let source_data = download_source_data(
-        &work_dir,
-        &[
-            "ca.bccdc.covid19.case-details",
-            "ca.ontario.data.covid19.case-details",
-        ],
-    );
+    // BC dataset does not contain unique IDs so it's a pain to slice and we pull it whole
+    Command::new("kamu")
+        .current_dir(&work_dir)
+        .args(&["pull", "ca.bccdc.covid19.case-details"])
+        .status()
+        .unwrap()
+        .exit_ok()
+        .unwrap();
+
+    let source_data = download_source_data(&work_dir, &["ca.ontario.data.covid19.case-details"]);
 
     // TODO: Split input into many batches
     for (dataset_id, data_path) in source_data {
-        let data_url = url::Url::from_file_path(&data_path.0).unwrap().to_string();
-        Command::new("kamu")
-            .current_dir(&work_dir)
-            .args(&["pull", &dataset_id, "--fetch", &data_url])
-            .status()
-            .unwrap()
-            .exit_ok()
-            .unwrap();
+        for data_slice_path in slice_data_file(data_path.as_ref(), 50, true) {
+            let data_url = url::Url::from_file_path(&data_slice_path)
+                .unwrap()
+                .to_string();
+
+            Command::new("kamu")
+                .current_dir(&work_dir)
+                .args(&["pull", &dataset_id, "--fetch", &data_url])
+                .status()
+                .unwrap()
+                .exit_ok()
+                .unwrap();
+        }
     }
 
-    for dataset_id in ["ca.covid19.case-details", "ca.covid19.daily-cases"] {
-        Command::new("kamu")
-            .current_dir(&work_dir)
-            .args(&["pull", dataset_id])
-            .status()
-            .unwrap()
-            .exit_ok()
-            .unwrap();
-    }
+    Command::new("kamu")
+        .current_dir(&work_dir)
+        .args(&["pull", "--all"])
+        .status()
+        .unwrap()
+        .exit_ok()
+        .unwrap();
 
     Command::new("kamu")
         .current_dir(&work_dir)
@@ -143,7 +150,87 @@ fn download_source_data(work_dir: &Path, datasets: &[&str]) -> Vec<(String, Drop
     root_data
 }
 
+fn slice_data_file(data_path: &Path, num_slices: usize, has_header: bool) -> Slices {
+    Slices::new(data_path, num_slices, has_header)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+struct Slices {
+    data_path: PathBuf,
+    lines: Vec<String>,
+    current_slice: usize,
+    num_slices: usize,
+    has_header: bool,
+}
+
+impl Slices {
+    fn new(data_path: &Path, num_slices: usize, has_header: bool) -> Self {
+        let file = std::fs::File::open(data_path).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let lines: Vec<String> = reader.lines().map(|r| r.unwrap()).collect();
+
+        Self {
+            data_path: data_path.to_owned(),
+            lines,
+            current_slice: 0,
+            num_slices,
+            has_header,
+        }
+    }
+}
+
+impl Iterator for Slices {
+    type Item = DropFile;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::io::Write;
+
+        let num_data_lines = self.lines.len() - if self.has_header { 1 } else { 0 };
+        let lines_per_slice =
+            (((num_data_lines as f64) / (self.num_slices as f64)).ceil()) as usize;
+
+        let slice_start =
+            lines_per_slice * self.current_slice + if self.has_header { 1 } else { 0 };
+        let slice_end = std::cmp::min(slice_start + lines_per_slice, self.lines.len());
+
+        if slice_start >= self.lines.len() {
+            return None;
+        }
+
+        let mut slice_path = self.data_path.clone();
+        let new_file_name = format!(
+            "{}.{}.bin",
+            slice_path.file_stem().unwrap().to_string_lossy(),
+            self.current_slice
+        );
+        slice_path.set_file_name(new_file_name);
+
+        let mut file = std::fs::File::create(&slice_path).unwrap();
+
+        if self.has_header {
+            writeln!(file, "{}", &self.lines[0]).unwrap();
+        }
+
+        for line in &self.lines[slice_start..slice_end] {
+            writeln!(file, "{}", line).unwrap();
+        }
+
+        self.current_slice += 1;
+
+        Some(DropFile(slice_path))
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 struct DropFile(PathBuf);
+
+impl AsRef<Path> for DropFile {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
 
 impl Drop for DropFile {
     fn drop(&mut self) {
