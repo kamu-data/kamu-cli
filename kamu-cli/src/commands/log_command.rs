bc10c14cd9a00cd84f7be50da258a11a2ee487ff
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2021-12-27 11:27:33 -0800

    Improve log command to show identity info

--- kamu-cli/src/commands/log_command.rs
+++ kamu-cli/src/commands/log_command.rs
@@ -18,6 +18,7 @@ use console::style;
 use opendatafabric::serde::yaml::YamlMetadataBlockSerializer;
 use opendatafabric::serde::MetadataBlockSerializer;
 use opendatafabric::MetadataBlock;
+use std::collections::BTreeMap;
 use std::fmt::Display;
 use std::io::Write;
 use std::sync::Arc;
@@ -62,12 +63,18 @@ impl LogCommand {
 
 impl Command for LogCommand {
     fn run(&mut self) -> Result<(), CLIError> {
+        let id_to_name_lookup: BTreeMap<_, _> = self
+            .metadata_repo
+            .get_all_datasets()
+            .map(|h| (h.id, h.name))
+            .collect();
+
         let mut renderer: Box<dyn MetadataRenderer> = match (
             self.outout_format.as_ref().map(|s| s.as_str()),
             self.output_config.is_tty && self.output_config.verbosity_level == 0,
         ) {
-            (None, true) => Box::new(PagedAsciiRenderer::new()),
-            (None, false) => Box::new(AsciiRenderer::new()),
+            (None, true) => Box::new(PagedAsciiRenderer::new(id_to_name_lookup)),
+            (None, false) => Box::new(AsciiRenderer::new(id_to_name_lookup)),
             (Some("yaml"), true) => Box::new(PagedYamlRenderer::new()),
             (Some("yaml"), false) => Box::new(YamlRenderer::new()),
             _ => panic!("Unexpected output format combination"),
@@ -99,98 +106,105 @@ trait MetadataRenderer {
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-struct AsciiRenderer;
+struct AsciiRenderer {
+    id_to_name_lookup: BTreeMap<DatasetID, DatasetName>,
+}
 
 impl AsciiRenderer {
-    fn new() -> Self {
-        Self
+    fn new(id_to_name_lookup: BTreeMap<DatasetID, DatasetName>) -> Self {
+        Self { id_to_name_lookup }
     }
 
     fn render_blocks(
+        &self,
         output: &mut impl Write,
         blocks: &mut dyn Iterator<Item = (Multihash, MetadataBlock)>,
     ) -> Result<(), std::io::Error> {
         for (hash, block) in blocks {
-            Self::render_block(output, &hash, &block)?;
+            self.render_block(output, &hash, &block)?;
             writeln!(output)?;
         }
         Ok(())
     }
 
     fn render_block(
+        &self,
         output: &mut impl Write,
         hash: &Multihash,
         block: &MetadataBlock,
     ) -> Result<(), std::io::Error> {
-        Self::render_header(output, hash, block)?;
-        Self::render_property(
+        self.render_header(output, hash, block)?;
+        self.render_property(
             output,
+            0,
             "SystemTime",
             &block
                 .system_time
                 .to_rfc3339_opts(SecondsFormat::AutoSi, true),
         )?;
 
-        if let Some(ref s) = block.output_slice {
-            Self::render_property(output, "Output.Offset.Start", &s.data_interval.start)?;
-            Self::render_property(output, "Output.Offset.End", &s.data_interval.end)?;
-            Self::render_property(
-                output,
-                "Output.Records",
-                &(s.data_interval.end - s.data_interval.start + 1),
-            )?;
-            Self::render_property(output, "Output.LogicalHash", &s.data_logical_hash)?;
-        }
-
-        if let Some(ref wm) = block.output_watermark {
-            Self::render_property(
-                output,
-                "Output.Watermark",
-                &wm.to_rfc3339_opts(SecondsFormat::AutoSi, true),
-            )?;
+        if let Some(id) = &block.seed {
+            self.render_property(output, 0, "Seed", &id)?;
         }
 
         if let Some(slices) = &block.input_slices {
-            for s in slices.iter() {
+            self.render_section(output, 0, "Inputs")?;
+
+            for (i, s) in slices.iter().enumerate() {
+                self.render_section(output, 1, &format!("Input[{}]", i))?;
+
+                self.render_property(output, 2, "ID", &s.dataset_id)?;
+
+                if let Some(name) = self.id_to_name_lookup.get(&s.dataset_id) {
+                    self.render_property(output, 2, "Name", name)?;
+                }
+
                 if let Some(bi) = &s.block_interval {
-                    Self::render_property(
-                        output,
-                        &format!("Input[{}].Blocks.Start", s.dataset_id),
-                        &bi.start,
-                    )?;
-                    Self::render_property(
-                        output,
-                        &format!("Input[{}].Blocks.End", s.dataset_id),
-                        &bi.end,
-                    )?;
+                    self.render_property(output, 2, "Blocks.Start", &bi.start)?;
+                    self.render_property(output, 2, "Blocks.End", &bi.end)?;
                 }
                 if let Some(iv) = &s.data_interval {
-                    Self::render_property(
-                        output,
-                        &format!("Input[{}].Offset.Start", s.dataset_id),
-                        &iv.start,
-                    )?;
-                    Self::render_property(
-                        output,
-                        &format!("Input[{}].Offset.End", s.dataset_id),
-                        &iv.end,
-                    )?;
-                    Self::render_property(
-                        output,
-                        &format!("Input[{}].Records", s.dataset_id),
-                        &(iv.end - iv.start + 1),
-                    )?;
+                    self.render_property(output, 2, "Offset.Start", &iv.start)?;
+                    self.render_property(output, 2, "Offset.End", &iv.end)?;
+                    self.render_property(output, 2, "Records", &(iv.end - iv.start + 1))?;
                 }
             }
+        }
+
+        if let Some(ref s) = block.output_slice {
+            self.render_section(output, 0, "Output")?;
+            self.render_property(output, 1, "Offset.Start", &s.data_interval.start)?;
+            self.render_property(output, 1, "Offset.End", &s.data_interval.end)?;
+            self.render_property(
+                output,
+                1,
+                "Records",
+                &(s.data_interval.end - s.data_interval.start + 1),
+            )?;
+            self.render_property(output, 1, "LogicalHash", &s.data_logical_hash)?;
+            self.render_property(output, 1, "PhysicalHash", &s.data_physical_hash)?;
+        }
+
+        if let Some(ref wm) = block.output_watermark {
+            if !block.output_slice.is_some() {
+                self.render_section(output, 0, "Output")?;
+            }
+
+            self.render_property(
+                output,
+                1,
+                "Watermark",
+                &wm.to_rfc3339_opts(SecondsFormat::AutoSi, true),
+            )?;
         }
 
         if let Some(ref source) = block.source {
             match source {
                 DatasetSource::Root { .. } => {
-                    Self::render_property(output, "Source", &"<Root source updated>")?
+                    self.render_property(output, 0, "Source", &"<Root source updated>")?
                 }
                 DatasetSource::Derivative { .. } => {
-                    Self::render_property(output, "Source", &"<Derivative source updated>")?
+                    self.render_property(output, 0, "Source", &"<Derivative source updated>")?
                 }
             }
         }
@@ -199,6 +213,7 @@ impl AsciiRenderer {
     }
 
     fn render_header(
+        &self,
         output: &mut impl Write,
         hash: &Multihash,
         _block: &MetadataBlock,
@@ -211,11 +226,24 @@ impl AsciiRenderer {
         )
     }
 
-    fn render_property<T: Display>(
+    fn render_section(
+        &self,
         output: &mut impl Write,
+        indent: i32,
+        name: &str,
+    ) -> Result<(), std::io::Error> {
+        self.indent(output, indent)?;
+        writeln!(output, "{}{}", style(name).dim(), style(":").dim())
+    }
+
+    fn render_property<T: Display>(
+        &self,
+        output: &mut impl Write,
+        indent: i32,
         name: &str,
         value: &T,
     ) -> Result<(), std::io::Error> {
+        self.indent(output, indent)?;
         writeln!(
             output,
             "{}{} {}",
@@ -223,6 +251,13 @@ impl AsciiRenderer {
             style(":").dim(),
             value
         )
+    }
+
+    fn indent(&self, output: &mut impl Write, level: i32) -> Result<(), std::io::Error> {
+        for _ in 0..level {
+            write!(output, "  ")?;
+        }
+        Ok(())
     }
 }
 
@@ -232,18 +267,20 @@ impl MetadataRenderer for AsciiRenderer {
         _dataset_handle: &DatasetHandle,
         blocks: &mut dyn Iterator<Item = (Multihash, MetadataBlock)>,
     ) -> Result<(), CLIError> {
-        Self::render_blocks(&mut std::io::stdout(), blocks)?;
+        self.render_blocks(&mut std::io::stdout(), blocks)?;
         Ok(())
     }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-struct PagedAsciiRenderer;
+struct PagedAsciiRenderer {
+    id_to_name_lookup: BTreeMap<DatasetID, DatasetName>,
+}
 
 impl PagedAsciiRenderer {
-    fn new() -> Self {
-        Self
+    fn new(id_to_name_lookup: BTreeMap<DatasetID, DatasetName>) -> Self {
+        Self { id_to_name_lookup }
     }
 }
 
@@ -257,7 +294,9 @@ impl MetadataRenderer for PagedAsciiRenderer {
         pager.set_exit_strategy(minus::ExitStrategy::PagerQuit);
         pager.set_prompt(&dataset_handle.name);
 
-        AsciiRenderer::render_blocks(&mut WritePager(&mut pager), blocks)?;
+        let renderer = AsciiRenderer::new(self.id_to_name_lookup.clone());
+        renderer.render_blocks(&mut WritePager(&mut pager), blocks)?;
+
         minus::page_all(pager).unwrap();
         Ok(())
     }
