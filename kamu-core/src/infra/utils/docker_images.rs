5edd87470ba66bb7eb84f10aa34e8ed1eea2b6cf
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2022-03-22 16:03:05 -0700

    Add more libs to Jupyter image

--- kamu-core/src/infra/utils/docker_images.rs
+++ kamu-core/src/infra/utils/docker_images.rs
@@ -10,7 +10,7 @@
 pub const SPARK: &str = "docker.io/kamudata/engine-spark:0.15.1-spark_3.1.2";
 pub const LIVY: &str = SPARK;
 pub const FLINK: &str = "docker.io/kamudata/engine-flink:0.13.0-flink_1.13.1-scala_2.12-java8";
-pub const JUPYTER: &str = "docker.io/kamudata/jupyter:0.1.0";
+pub const JUPYTER: &str = "docker.io/kamudata/jupyter:0.2.0";
 
 // Test Images
 pub const HTTPD: &str = "docker.io/httpd:2.4";
