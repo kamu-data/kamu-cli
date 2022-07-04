dc97df6ea2b97ec1e0432336bc70de6688afe86e
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2022-07-04 12:22:01 -0700

    Fix up covid demo

--- images/demo/user-home/01 - Kamu Basics (COVID-19 example)/init-chapter-3.sh
+++ images/demo/user-home/01 - Kamu Basics (COVID-19 example)/init-chapter-3.sh
@@ -4,9 +4,8 @@ set -e
 rm -rf .kamu
 kamu init
 
-kamu add datasets/british-columbia.case-details.yaml
 kamu repo add kamu-hub s3+http://minio/kamu-hub
-kamu pull kamu-hub/covid19.ontario.case-details --as ontario.case-details
-kamu add datasets/canada.case-details.yaml
-kamu add datasets/canada.daily-cases.yaml
-kamu pull --all
+kamu pull kamu-hub/covid19.british-columbia.case-details --as british-columbia.case-details --no-alias
+kamu pull kamu-hub/covid19.canada.case-details --as canada.case-details
+kamu pull kamu-hub/covid19.canada.daily-cases --as canada.daily-cases --no-alias
+kamu pull kamu-hub/covid19.ontario.case-details --as ontario.case-details --no-alias
