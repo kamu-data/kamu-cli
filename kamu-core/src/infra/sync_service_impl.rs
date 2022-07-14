829d2c479a389e67ab0599566d3c085a781247bf
Sergei Zaychenko <szaychenko@kamu.dev>
2022-07-14 01:55:48 -0700

    Added unit tests about forcing synchronization + corrected IPFS synchronization

--- kamu-core/src/infra/sync_service_impl.rs
+++ kamu-core/src/infra/sync_service_impl.rs
@@ -352,7 +352,7 @@ impl SyncServiceImpl {
         // Analyze the block interval
         let num_blocks = match src_dataset
             .as_metadata_chain()
-            .iter_blocks_interval(&src_head, dst_head.as_ref(), false)
+            .iter_blocks_interval(&src_head, dst_head.as_ref(), opts.force)
             .try_count()
             .await
         {
