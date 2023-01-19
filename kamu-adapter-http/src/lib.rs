08971f97dc83464f63f7dd93b65871a260aad62e
Sergei Zaychenko <szaychenko@kamu.dev>
2023-01-19 03:06:16 -0800

    Manually described smart protocol message structures

--- kamu-adapter-http/src/lib.rs
+++ kamu-adapter-http/src/lib.rs
@@ -7,6 +7,8 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+pub(crate) mod messages;
+
 mod http_server_constants;
 pub use http_server_constants::*;
 
---
+++ kamu-adapter-http/src/messages/smart_transfer_protocol_messages.rs
@@ -0,0 +1,197 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use url::Url;
+use chrono::{DateTime, Utc};
+
+use opendatafabric::Multihash;
+
+
+/// Initial dataset pull request message
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPullRequest {
+    pub begin_after: Option<Multihash>,
+    pub stop_at: Option<Multihash>,
+}
+
+
+/// Response to initial dataset pull request
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPullResponse {
+    pub size_estimation: TransferSizeEstimation,
+}
+
+
+/// Pull stage 1: request metadata update
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPullMetadataRequest {}
+
+
+/// Pull stage 1: metadata update response
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetMetadataPullResponse {
+    pub blocks: ObjectsBatch,
+}
+
+
+/// Pull stage 2: object transfer request
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPullObjectsTransferRequest {
+    pub object_files: Vec<ObjectFileReference>,
+}
+
+
+// Pull stage 2: object transfer response
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPullObjectsTransferResponse {
+    object_transfer_strategies: Vec<PullObjectTransferStrategy>,
+}
+
+
+/// Pull object transfer strategy
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct PullObjectTransferStrategy {
+    pub object_file: ObjectFileReference,
+    pub pull_strategy: ObjectPullStrategy,
+    pub download_from: TransferUrl,
+}
+
+
+// Object pull strategy enumeration
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum ObjectPullStrategy {
+    HttpDownload,
+}
+
+
+/// Initial dataset push request message
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushRequest {
+    pub current_head: Multihash,
+    pub size_estimation: TransferSizeEstimation,
+}
+
+
+/// Response to initial dataset push request
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushRequestAccepted {}
+
+
+/// Push phase 1: push metadata request
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushMetadata {
+    pub new_blocks: ObjectsBatch,
+}
+
+
+/// Push phase 1: push metadata accepted
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushMetadataAccepted {}
+
+
+/// Push phase 2: object transfer request
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushObjectsTransferRequest {
+    pub object_files: Vec<ObjectFileReference>,
+}
+
+
+/// Push phase 2: object transfer response
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushObjectsTransferResponse {
+    pub object_transfer_strategies: Vec<PushObjectTransferStrategy>,
+}
+
+
+/// Push object transfer strategy
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct PushObjectTransferStrategy {
+    pub object_file: ObjectFileReference,
+    pub push_strategy: ObjectPushStrategy,
+    pub upload_to: Option<TransferUrl>,
+}
+
+
+
+// Object push strategy enumeration
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum ObjectPushStrategy {
+    SkipUpload,
+    HttpUpload,
+}
+
+
+/// Push stage 3: complete handshake indication
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushComplete {}
+
+
+/// Push stage 3: complete handshake acknowledge
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetPushCompleteConfirmed {}
+
+
+/// Error message
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct DatasetError {
+    pub error_details: ErrorDetails,
+}
+
+
+/// Error message details
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct ErrorDetails {
+    pub error_code: String,
+    pub description: String,
+}
+
+/// Packed object batch
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct ObjectsBatch {
+    pub objects_count: u32,
+    pub object_type: ObjectType,
+    pub media_type: String,
+    pub encoding: String,
+    pub payload: Vec<u8>,
+}
+
+
+/// Transfer size estimation
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TransferSizeEstimation {
+    pub num_blocks: u32,
+    pub num_objects: u32,
+    pub bytes_in_raw_blocks: u64,
+    pub bytes_in_raw_objects: u64,
+}
+
+
+// Object file reference
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct ObjectFileReference {
+    pub object_type: ObjectType,
+    pub physical_hash: Multihash,
+}
+
+
+// Object type enumeration
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum ObjectType {
+    MetadataBlock,
+    DataSlice,
+    Checkpoint,
+}
+
+
+// Transfer URL
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TransferUrl {
+    pub url: Url,
+    pub expires_at: Option<DateTime<Utc>>,
+}
