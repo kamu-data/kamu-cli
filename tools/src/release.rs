7e1b0218a167a6c013c94e6fd15b71cff3ed80ef
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2023-03-19 16:54:03 -0700

    Fix license header error

--- tools/src/release.rs
+++ tools/src/release.rs
@@ -6,6 +6,7 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
+
 #![feature(exit_status_error)]
 
 extern crate semver;
