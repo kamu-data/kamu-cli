9d2289179d98b5b4bee141779b923439725391c5
Sergei Zaychenko <szaychenko@kamu.dev>
2023-03-22 08:33:43 -0700

    Simple transfer protocol HTTP statuses & tracing improved

--- kamu-adapter-http/src/http_server_simple_transfer_protocol.rs
+++ kamu-adapter-http/src/http_server_simple_transfer_protocol.rs
@@ -12,7 +12,7 @@ use crate::{
     PARAMETER_DATASET_NAME, PARAMETER_PHYSICAL_HASH, PARAMETER_REF,
 };
 use axum::extract::Extension;
-use kamu::domain::{BlockRef, Dataset};
+use kamu::domain::*;
 
 use opendatafabric::{
     serde::{flatbuffers::FlatbuffersMetadataBlockSerializer, MetadataBlockSerializer},
@@ -21,75 +21,132 @@ use opendatafabric::{
 use std::{collections::HashMap, str::FromStr, sync::Arc};
 use url::Url;
 
+/////////////////////////////////////////////////////////////////////////////////
+
 pub async fn dataset_refs_handler(
     dataset: Extension<Arc<dyn Dataset>>,
     axum::extract::Path(params): axum::extract::Path<HashMap<String, String>>,
-) -> String {
+) -> Result<String, axum::http::StatusCode> {
     let ref_param = params.get(PARAMETER_REF).unwrap();
-    let block_ref = BlockRef::from_str(ref_param.as_str()).unwrap();
-    let hash = dataset
-        .as_metadata_chain()
-        .get_ref(&block_ref)
-        .await
-        .unwrap();
 
-    hash.to_string()
+    let block_ref = match BlockRef::from_str(ref_param.as_str()) {
+        Ok(block_ref) => Ok(block_ref),
+        Err(_) => Err(axum::http::StatusCode::NOT_FOUND),
+    }?;
+
+    let get_ref_result = dataset.as_metadata_chain().get_ref(&block_ref).await;
+
+    match get_ref_result {
+        Ok(hash) => Ok(hash.to_string()),
+        Err(GetRefError::NotFound(_)) => Err(axum::http::StatusCode::NOT_FOUND),
+        Err(_) => {
+            tracing::debug!("Internal error while resolving reference '{}'", ref_param);
+            return Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 pub async fn dataset_blocks_handler(
     dataset: Extension<Arc<dyn Dataset>>,
     axum::extract::Path(params): axum::extract::Path<HashMap<String, String>>,
-) -> Vec<u8> {
+) -> Result<Vec<u8>, axum::http::StatusCode> {
     let block_hash_param = params.get(PARAMETER_BLOCK_HASH).unwrap();
-    let block_hash = Multihash::from_multibase_str(block_hash_param.as_str()).unwrap();
-    let block = dataset
-        .as_metadata_chain()
-        .get_block(&block_hash)
-        .await
-        .unwrap();
 
-    let block_bytes = FlatbuffersMetadataBlockSerializer
-        .write_manifest(&block)
-        .unwrap();
+    let block_hash: Multihash = match Multihash::from_multibase_str(block_hash_param.as_str()) {
+        Ok(block_hash) => block_hash,
+        Err(e) => {
+            tracing::debug!("MultihashError: {}, {}", block_hash_param, e);
+            return Err(axum::http::StatusCode::BAD_REQUEST);
+        }
+    };
 
-    block_bytes.collapse_vec()
+    let block = match dataset.as_metadata_chain().get_block(&block_hash).await {
+        Ok(block) => block,
+        Err(GetBlockError::NotFound(_)) => return Err(axum::http::StatusCode::NOT_FOUND),
+        Err(e) => {
+            tracing::debug!("GetBlockError: {}, {}", block_hash_param, e);
+            return Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    match FlatbuffersMetadataBlockSerializer.write_manifest(&block) {
+        Ok(block_bytes) => Ok(block_bytes.collapse_vec()),
+        Err(e) => {
+            tracing::debug!("Block serialization failed: {}, {}", block_hash_param, e);
+            Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 pub async fn dataset_data_handler(
     dataset: Extension<Arc<dyn Dataset>>,
     axum::extract::Path(params): axum::extract::Path<HashMap<String, String>>,
-) -> axum::response::Response {
+) -> Result<axum::response::Response, axum::http::StatusCode> {
     let physical_hash_param = params.get(PARAMETER_PHYSICAL_HASH).unwrap();
-    let physical_hash = Multihash::from_multibase_str(physical_hash_param.as_str()).unwrap();
-    let data_stream = dataset
-        .as_data_repo()
-        .get_stream(&physical_hash)
-        .await
-        .unwrap();
-    let body = axum_extra::body::AsyncReadBody::new(data_stream);
 
-    axum::response::Response::builder()
+    let physical_hash = match Multihash::from_multibase_str(physical_hash_param.as_str()) {
+        Ok(physical_hash) => physical_hash,
+        Err(e) => {
+            tracing::debug!("MultihashError: {}, {}", physical_hash_param, e);
+            return Err(axum::http::StatusCode::BAD_REQUEST);
+        }
+    };
+
+    let data_stream = match dataset.as_data_repo().get_stream(&physical_hash).await {
+        Ok(stream) => stream,
+        Err(GetError::NotFound(_)) => return Err(axum::http::StatusCode::NOT_FOUND),
+        Err(e) => {
+            tracing::debug!("Data GetError: {}, {}", physical_hash_param, e);
+            return Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let body = axum_extra::body::AsyncReadBody::new(data_stream);
+    Ok(axum::response::Response::builder()
         .body(axum::body::boxed(body))
-        .unwrap()
+        .unwrap())
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 pub async fn dataset_checkpoints_handler(
     dataset: Extension<Arc<dyn Dataset>>,
     axum::extract::Path(params): axum::extract::Path<HashMap<String, String>>,
-) -> axum::response::Response {
+) -> Result<axum::response::Response, axum::http::StatusCode> {
     let physical_hash_param = params.get(PARAMETER_PHYSICAL_HASH).unwrap();
-    let physical_hash = Multihash::from_multibase_str(physical_hash_param.as_str()).unwrap();
-    let checkpoint_stream = dataset
+
+    let physical_hash = match Multihash::from_multibase_str(physical_hash_param.as_str()) {
+        Ok(physical_hash) => physical_hash,
+        Err(e) => {
+            tracing::debug!("MultihashError: {}, {}", physical_hash_param, e);
+            return Err(axum::http::StatusCode::BAD_REQUEST);
+        }
+    };
+
+    let checkpoint_stream = match dataset
         .as_checkpoint_repo()
         .get_stream(&physical_hash)
         .await
-        .unwrap();
-    let body = axum_extra::body::AsyncReadBody::new(checkpoint_stream);
+    {
+        Ok(stream) => stream,
+        Err(GetError::NotFound(_)) => return Err(axum::http::StatusCode::NOT_FOUND),
+        Err(e) => {
+            tracing::debug!("Checkpoint GetError: {}, {}", physical_hash_param, e);
+            return Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
 
-    axum::response::Response::builder()
+    let body = axum_extra::body::AsyncReadBody::new(checkpoint_stream);
+    Ok(axum::response::Response::builder()
         .body(axum::body::boxed(body))
-        .unwrap()
+        .unwrap())
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 pub async fn dataset_push_ws_upgrade_handler(
     ws: axum::extract::ws::WebSocketUpgrade,
@@ -99,6 +156,8 @@ pub async fn dataset_push_ws_upgrade_handler(
         ws_smart_transfer_protocol_axum_server::dataset_push_ws_handler(socket, dataset.0)
     })
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 pub async fn dataset_pull_ws_upgrade_handler(
     ws: axum::extract::ws::WebSocketUpgrade,
@@ -122,6 +181,8 @@ pub async fn dataset_pull_ws_upgrade_handler(
     })
 }
 
+/////////////////////////////////////////////////////////////////////////////////
+
 pub async fn resolve_dataset_by_name(
     dataset_resolver: axum::Extension<Arc<dyn DatasetResolver>>,
     axum::extract::Path(params): axum::extract::Path<HashMap<String, String>>,
@@ -141,12 +202,17 @@ pub async fn resolve_dataset_by_name(
         Err(e) => match e {
             kamu::domain::GetDatasetError::NotFound(_) => Err(axum::http::StatusCode::NOT_FOUND),
             kamu::domain::GetDatasetError::Internal(_) => {
-                println!("Dataset resolution error");
+                tracing::debug!(
+                    "Internal error while resolving dataset '{}'",
+                    dataset_name_param
+                );
                 Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR)
             }
         },
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
 
 fn resolve_base_api_url(api_host: axum::headers::Host) -> Url {
     let mut base_url_str = String::from("http://");
@@ -159,3 +225,5 @@ fn resolve_base_api_url(api_host: axum::headers::Host) -> Url {
 
     Url::parse(base_url_str.as_str()).unwrap()
 }
+
+/////////////////////////////////////////////////////////////////////////////////
