7ed8bfd6b957fda88b1803c3475ca9510a85ebfe
Sergei Zaychenko <szaychenko@kamu.dev>
2023-03-28 03:03:53 -0700

    DI-friendlier version of smart pull tests

--- kamu-adapter-http/tests/harness/client_side_harness.rs
+++ kamu-adapter-http/tests/harness/client_side_harness.rs
@@ -11,12 +11,8 @@ use std::{path::PathBuf, sync::Arc};
 
 use container_runtime::ContainerRuntime;
 use kamu::{
-    domain::{DatasetRepository, PullOptions, PullResponse, PullService, SyncOptions},
-    infra::{
-        self, DatasetFactoryImpl, DatasetRepositoryLocalFs, EngineProvisionerNull,
-        IngestServiceImpl, IpfsGateway, PullServiceImpl, RemoteAliasesRegistryImpl,
-        RemoteRepositoryRegistryImpl, SyncServiceImpl, TransformServiceImpl, WorkspaceLayout,
-    },
+    domain::*,
+    infra::{utils::smart_transfer_protocol::SmartTransferProtocolClient, *},
 };
 use kamu_adapter_http::WsSmartTransferProtocolClient;
 use opendatafabric::DatasetRefAny;
@@ -34,45 +30,48 @@ pub struct ClientSideHarness {
 impl ClientSideHarness {
     pub fn new() -> Self {
         let tempdir = tempfile::tempdir().unwrap();
-        let workspace_layout = Arc::new(WorkspaceLayout::create(tempdir.path()).unwrap());
+        let workspace_layout = WorkspaceLayout::create(tempdir.path()).unwrap();
 
-        let catalog = dill::CatalogBuilder::new()
-            .add_value(DatasetRepositoryLocalFs::new(workspace_layout.clone()))
-            .bind::<dyn DatasetRepository, infra::DatasetRepositoryLocalFs>()
-            .build();
+        let mut b = dill::CatalogBuilder::new();
 
-        let client_repo = catalog.get_one::<dyn DatasetRepository>().unwrap();
-        let remote_repo_reg = Arc::new(RemoteRepositoryRegistryImpl::new(workspace_layout.clone()));
-        let remote_alias_reg = Arc::new(RemoteAliasesRegistryImpl::new(
-            client_repo.clone(),
-            workspace_layout.clone(),
-        ));
-        let ingest_svc = Arc::new(IngestServiceImpl::new(
-            workspace_layout.clone(),
-            client_repo.clone(),
-            Arc::new(EngineProvisionerNull),
-            Arc::new(ContainerRuntime::default()),
-        ));
-        let transform_svc = Arc::new(TransformServiceImpl::new(
-            client_repo.clone(),
-            Arc::new(EngineProvisionerNull),
-            workspace_layout.clone(),
-        ));
-        let sync_svc = Arc::new(SyncServiceImpl::new(
-            remote_repo_reg.clone(),
-            client_repo.clone(),
-            Arc::new(DatasetFactoryImpl::new()),
-            Arc::new(WsSmartTransferProtocolClient {}),
-            Arc::new(kamu::infra::utils::ipfs_wrapper::IpfsClient::default()),
-            IpfsGateway::default(),
-        ));
-        let pull_service = Arc::new(PullServiceImpl::new(
-            client_repo.clone(),
-            remote_alias_reg.clone(),
-            ingest_svc,
-            transform_svc,
-            sync_svc,
-        ));
+        b.add::<DatasetRepositoryLocalFs>()
+            .bind::<dyn DatasetRepository, DatasetRepositoryLocalFs>();
+
+        b.add::<RemoteRepositoryRegistryImpl>()
+            .bind::<dyn RemoteRepositoryRegistry, RemoteRepositoryRegistryImpl>();
+
+        b.add::<RemoteAliasesRegistryImpl>()
+            .bind::<dyn RemoteAliasesRegistry, RemoteAliasesRegistryImpl>();
+
+        b.add_value(EngineProvisionerNull)
+            .bind::<dyn EngineProvisioner, EngineProvisionerNull>();
+
+        b.add::<IngestServiceImpl>()
+            .bind::<dyn IngestService, IngestServiceImpl>();
+
+        b.add::<DatasetFactoryImpl>()
+            .bind::<dyn DatasetFactory, DatasetFactoryImpl>();
+
+        b.add::<WsSmartTransferProtocolClient>()
+            .bind::<dyn SmartTransferProtocolClient, WsSmartTransferProtocolClient>();
+
+        b.add::<SyncServiceImpl>()
+            .bind::<dyn SyncService, SyncServiceImpl>();
+
+        b.add::<TransformServiceImpl>()
+            .bind::<dyn TransformService, TransformServiceImpl>();
+
+        b.add::<PullServiceImpl>()
+            .bind::<dyn PullService, PullServiceImpl>();
+
+        b.add_value(workspace_layout.clone())
+            .add_value(ContainerRuntime::default())
+            .add_value(utils::ipfs_wrapper::IpfsClient::default())
+            .add_value(IpfsGateway::default());
+
+        let catalog = b.build();
+
+        let pull_service = catalog.get_one::<dyn PullService>().unwrap();
 
         Self {
             tempdir,
