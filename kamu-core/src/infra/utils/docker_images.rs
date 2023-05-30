d0c5f67bf4842382753766f09afb472cb4707f26
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2023-05-29 17:55:46 -0700

    Upgrade to new Spark image

--- kamu-core/src/infra/utils/docker_images.rs
+++ kamu-core/src/infra/utils/docker_images.rs
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-pub const SPARK: &str = "ghcr.io/kamu-data/engine-spark:0.18.2-spark_3.1.2";
+pub const SPARK: &str = "ghcr.io/kamu-data/engine-spark:0.19.0-spark_3.1.2";
 pub const FLINK: &str = "ghcr.io/kamu-data/engine-flink:0.15.1-flink_1.16.0-scala_2.12-java8";
 pub const DATAFUSION: &str = "ghcr.io/kamu-data/engine-datafusion:0.1.2";
 
