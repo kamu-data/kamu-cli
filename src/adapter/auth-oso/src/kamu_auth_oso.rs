0b852df229d88c8e3e2bac43ce4464d4f8ede1a7
Sergei Zaychenko <szaychenko@kamu.dev>
2023-08-10 07:02:33 -0700

    Tests for OsoDatasetAuthorizer

--- src/adapter/auth-oso/src/kamu_auth_oso.rs
+++ src/adapter/auth-oso/src/kamu_auth_oso.rs
@@ -39,33 +39,33 @@ impl KamuAuthOso {
 
         oso.register_class(DatasetResource::get_polar_class())?;
         oso.register_class(UserActor::get_polar_class())?;
-    
+
         oso.load_str(
             r#"
             actor UserActor {}
-    
+
             resource DatasetResource {
                 permissions = ["read", "write"];
             }
-    
+
             has_permission(actor: UserActor, "read", dataset: DatasetResource) if
-                dataset.allows_public_read or 
+                dataset.allows_public_read or
                 dataset.created_by == actor.name or (
                     actor_name = actor.name and
                     dataset.authorized_users.(actor_name) in ["Reader", "Editor"]
                 );
-    
+
             has_permission(actor: UserActor, "write", dataset: DatasetResource) if
                 dataset.created_by == actor.name or (
                     actor_name = actor.name and
                     dataset.authorized_users.(actor_name) == "Editor"
                 );
-    
+
             allow(actor: UserActor, action: String, dataset: DatasetResource) if
                 has_permission(actor, action, dataset);
         "#,
         )?;
-    
+
         Ok(oso)
     }
 }
