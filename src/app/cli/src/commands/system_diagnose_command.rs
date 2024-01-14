48f79f7b0292f6166555d078b2e4e8b6bb3c5d69
rmn-boiko <roman.bv20@gmail.com>
2024-01-14 14:45:08 +0100

    Refactore error messages

--- src/app/cli/src/commands/system_diagnose_command.rs
+++ src/app/cli/src/commands/system_diagnose_command.rs
@@ -6,16 +6,18 @@
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::Write;
+use std::process::Output;
 use std::sync::Arc;
 
 use console::style;
-use container_runtime::ContainerRuntime;
+use container_runtime::{ContainerRuntime, RunArgs};
 use futures::TryStreamExt;
 use internal_error::{InternalError, ResultIntoInternal};
 use kamu::domain::{
     DatasetRepository,
+    OwnedFile,
     VerificationMultiListener,
     VerificationOptions,
     VerificationService,
@@ -26,7 +28,6 @@ use thiserror::Error;
 use super::{CLIError, Command};
 use crate::VerificationMultiProgress;
 
-const TEST_CONTAINER_NAME: &str = "test-volume-mount";
 const SUCCESS_MESSAGE: &str = "ok";
 const FAILED_MESSAGE: &str = "failed";
 
@@ -60,13 +61,48 @@ impl Command for SystemDiagnoseCommand {
     }
 
     async fn run(&mut self) -> Result<(), CLIError> {
-        let diagnose_check = RunCheck::new(
-            self.dataset_repo.clone(),
-            self.verification_svc.clone(),
-            self.container_runtime.clone(),
-            self.is_in_workpace,
-        );
-        diagnose_check.run().await?;
+        let mut out = std::io::stdout();
+
+        let mut diagnose_checks: Vec<Box<dyn DiagCheck>> = vec![
+            Box::new(CheckContainerRuntimeIsInstalled {
+                container_runtime: self.container_runtime.clone(),
+            }),
+            Box::new(CheckContainerRuntimeImagePull {
+                container_runtime: self.container_runtime.clone(),
+            }),
+            Box::new(CheckContainerRuntimeRootlessRun {
+                container_runtime: self.container_runtime.clone(),
+            }),
+            Box::new(CheckContainerRuntimeVolumeMount {
+                container_runtime: self.container_runtime.clone(),
+            }),
+        ];
+        // Add checks which required workspace initialization
+        if self.is_in_workpace {
+            diagnose_checks.push(Box::new(CheckWorkspaceConsistent {
+                dataset_repo: self.dataset_repo.clone(),
+                verification_svc: self.verification_svc.clone(),
+            }));
+        }
+
+        for diag_check in diagnose_checks.iter() {
+            write!(out, "{}... ", diag_check.name())?;
+            match diag_check.run().await {
+                Ok(_) => write!(out, "{}\n", style(SUCCESS_MESSAGE).green())?,
+                Err(err) => {
+                    write!(out, "{}\n", style(FAILED_MESSAGE).red())?;
+                    write!(out, "{}\n", style(err.to_string()).red())?;
+                }
+            }
+        }
+
+        if !self.is_in_workpace {
+            write!(
+                out,
+                "{}\n",
+                style("Directory is not kamu workspace").yellow()
+            )?;
+        }
         Ok(())
     }
 }
@@ -74,47 +110,34 @@ impl Command for SystemDiagnoseCommand {
 ///////////////////////////////////////////////////////////////////////////////
 
 #[derive(thiserror::Error, Debug)]
-pub enum DiagStepError {
+enum DiagCheckError {
     #[error(transparent)]
-    InvalidOutputMessage(#[from] InvalidOutputMessageError),
-    #[error(transparent)]
-    CommandExecFailed(#[from] CommandExecError),
+    Failed(#[from] CommandExecError),
     #[error(transparent)]
     Internal(
         #[from]
         #[backtrace]
         InternalError,
     ),
-    #[error(transparent)]
-    IoError(
-        #[from]
-        #[backtrace]
-        InputOutputError,
-    ),
 }
 
 #[derive(Error, Debug)]
-#[error("Cannot parse output message {message}")]
-pub struct InvalidOutputMessageError {
-    pub message: String,
-}
-
-#[derive(Error, Debug)]
-#[error("Cannot execute command: {message}")]
+#[error(
+    "{message} \n {error_log}
+    Make sure to follow kamu installation instructions to correctly configure \
+     docker or podman:
+        https://docs.kamu.dev/cli/get-started/installation/"
+)]
 pub struct CommandExecError {
     pub message: String,
+    pub error_log: String,
 }
 
-#[derive(Error, Debug)]
-#[error("Cannot perform io operation: {message}")]
-pub struct InputOutputError {
-    pub message: String,
-}
-
-impl From<std::io::Error> for DiagStepError {
+impl From<std::io::Error> for DiagCheckError {
     fn from(e: std::io::Error) -> Self {
-        Self::IoError(InputOutputError {
-            message: e.to_string(),
+        Self::Failed(CommandExecError {
+            message: "Unable to perform io operation".to_string(),
+            error_log: e.to_string(),
         })
     }
 }
@@ -124,51 +147,38 @@ impl From<std::io::Error> for DiagStepError {
 #[async_trait::async_trait]
 trait DiagCheck {
     fn name(&self) -> String;
-    async fn run(&self) -> Result<(), DiagStepError>;
+    async fn run(&self) -> Result<(), DiagCheckError>;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-struct CheckContainerInstallation {
+struct CheckContainerRuntimeIsInstalled {
     container_runtime: Arc<ContainerRuntime>,
 }
 
 #[async_trait::async_trait]
-impl DiagCheck for CheckContainerInstallation {
+impl DiagCheck for CheckContainerRuntimeIsInstalled {
     fn name(&self) -> String {
         format!("{} installed", self.container_runtime.config.runtime)
     }
-    async fn run(&self) -> Result<(), DiagStepError> {
+    async fn run(&self) -> Result<(), DiagCheckError> {
         let command_res = self.container_runtime.info().output().await.int_err()?;
-        match command_res.status.success() {
-            true => Ok(()),
-            false => {
-                let err_msg = String::from_utf8(command_res.stderr).map_err(|e| {
-                    DiagStepError::InvalidOutputMessage(InvalidOutputMessageError {
-                        message: e.to_string(),
-                    })
-                })?;
-
-                Err(DiagStepError::CommandExecFailed(CommandExecError {
-                    message: err_msg,
-                }))
-            }
-        }
+        handle_output_result(command_res)
     }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-struct CheckContainerImagePull {
+struct CheckContainerRuntimeImagePull {
     container_runtime: Arc<ContainerRuntime>,
 }
 
 #[async_trait::async_trait]
-impl DiagCheck for CheckContainerImagePull {
+impl DiagCheck for CheckContainerRuntimeImagePull {
     fn name(&self) -> String {
         format!("{} can pull images", self.container_runtime.config.runtime)
     }
-    async fn run(&self) -> Result<(), DiagStepError> {
+    async fn run(&self) -> Result<(), DiagCheckError> {
         self.container_runtime
             .pull_image(BUSYBOX, None)
             .await
@@ -180,80 +190,69 @@ impl DiagCheck for CheckContainerImagePull {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-struct CheckContainerRootlessRun {
+struct CheckContainerRuntimeRootlessRun {
     container_runtime: Arc<ContainerRuntime>,
 }
 
 #[async_trait::async_trait]
-impl DiagCheck for CheckContainerRootlessRun {
+impl DiagCheck for CheckContainerRuntimeRootlessRun {
     fn name(&self) -> String {
         format!(
             "{} rootless run check",
             self.container_runtime.config.runtime
         )
     }
-    async fn run(&self) -> Result<(), DiagStepError> {
-        let command_status = self
+    async fn run(&self) -> Result<(), DiagCheckError> {
+        let run_args = RunArgs {
+            image: BUSYBOX.to_string(),
+            container_name: Some("check-rootless-run".to_owned()),
+            ..RunArgs::default()
+        };
+
+        let command_res = self
             .container_runtime
-            .run_attached(BUSYBOX)
-            .init(true)
-            .spawn()
-            .unwrap()
-            .wait()
+            .run_cmd(run_args)
+            .output()
             .await
             .int_err()?;
 
-        if !command_status.success() {
-            return Err(DiagStepError::CommandExecFailed(CommandExecError {
-                message: "Failed to run docker container".to_string(),
-            }));
-        }
-
-        Ok(())
+        handle_output_result(command_res)
     }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-struct CheckContainerVolumeMount {
+struct CheckContainerRuntimeVolumeMount {
     container_runtime: Arc<ContainerRuntime>,
 }
 
 #[async_trait::async_trait]
-impl DiagCheck for CheckContainerVolumeMount {
+impl DiagCheck for CheckContainerRuntimeVolumeMount {
     fn name(&self) -> String {
         format!(
             "{} volume mounts work",
             self.container_runtime.config.runtime
         )
     }
-    async fn run(&self) -> Result<(), DiagStepError> {
-        let dir_to_mount = std::env::current_dir()?.join("tmp");
-        std::fs::create_dir(dir_to_mount.clone())?;
+    async fn run(&self) -> Result<(), DiagCheckError> {
+        let dir_to_mount = std::env::current_dir()?;
         let file_path = dir_to_mount.join("tmp.txt");
         let _ = File::create(file_path.clone())?;
+        let _ = OwnedFile::new(file_path);
+        let run_args = RunArgs {
+            image: BUSYBOX.to_string(),
+            container_name: Some("check-volume-mount".to_owned()),
+            ..RunArgs::default()
+        };
 
-        let command_status = self
+        let command_res = self
             .container_runtime
-            .run_attached(BUSYBOX)
-            .volume((dir_to_mount.clone(), "/out"))
-            .container_name(TEST_CONTAINER_NAME)
-            .args(["cat", "/out/tmp.txt"])
-            .init(true)
-            .spawn()
-            .unwrap()
-            .wait()
+            .run_cmd(run_args)
+            .output()
             .await
             .int_err()?;
-        fs::remove_dir_all(dir_to_mount)?;
 
-        if !command_status.success() {
-            return Err(DiagStepError::CommandExecFailed(CommandExecError {
-                message: "Failed to mount volume in container".to_string(),
-            }));
-        }
-
-        Ok(())
+        handle_output_result(command_res)
     }
 }
 
@@ -269,7 +268,7 @@ impl DiagCheck for CheckWorkspaceConsistent {
     fn name(&self) -> String {
         "workspace consistent".to_string()
     }
-    async fn run(&self) -> Result<(), DiagStepError> {
+    async fn run(&self) -> Result<(), DiagCheckError> {
         let progress = VerificationMultiProgress::new();
         let listener_option = Arc::new(progress.clone());
 
@@ -304,71 +303,23 @@ impl DiagCheck for CheckWorkspaceConsistent {
     }
 }
 
-pub struct RunCheck {
-    dataset_repo: Arc<dyn DatasetRepository>,
-    verification_svc: Arc<dyn VerificationService>,
-    container_runtime: Arc<ContainerRuntime>,
-    is_in_workpace: bool,
-}
+///////////////////////////////////////////////////////////////////////////////
 
-impl RunCheck {
-    fn new(
-        dataset_repo: Arc<dyn DatasetRepository>,
-        verification_svc: Arc<dyn VerificationService>,
-        container_runtime: Arc<ContainerRuntime>,
-        is_in_workpace: bool,
-    ) -> Self {
-        Self {
-            dataset_repo,
-            verification_svc,
-            container_runtime,
-            is_in_workpace,
+fn handle_output_result(result: Output) -> Result<(), DiagCheckError> {
+    match result.status.success() {
+        true => Ok(()),
+        false => {
+            let err_msg = String::from_utf8(result.stderr).map_err(|e| {
+                DiagCheckError::Failed(CommandExecError {
+                    message: "Cannot parse command execution error".to_string(),
+                    error_log: e.to_string(),
+                })
+            })?;
+
+            Err(DiagCheckError::Failed(CommandExecError {
+                message: "Container runtime command unavailable".to_string(),
+                error_log: err_msg,
+            }))
         }
-    }
-
-    pub async fn run(&self) -> Result<(), CLIError> {
-        let mut out = std::io::stdout();
-
-        let mut diagnose_checks: Vec<Box<dyn DiagCheck>> = vec![
-            Box::new(CheckContainerInstallation {
-                container_runtime: self.container_runtime.clone(),
-            }),
-            Box::new(CheckContainerImagePull {
-                container_runtime: self.container_runtime.clone(),
-            }),
-            Box::new(CheckContainerRootlessRun {
-                container_runtime: self.container_runtime.clone(),
-            }),
-            Box::new(CheckContainerVolumeMount {
-                container_runtime: self.container_runtime.clone(),
-            }),
-        ];
-        // Add checks which required workspace initialization
-        if self.is_in_workpace {
-            diagnose_checks.push(Box::new(CheckWorkspaceConsistent {
-                dataset_repo: self.dataset_repo.clone(),
-                verification_svc: self.verification_svc.clone(),
-            }));
-        }
-
-        for diag_check in diagnose_checks.iter() {
-            write!(out, "{}... ", diag_check.name())?;
-            match diag_check.run().await {
-                Ok(_) => write!(out, "{}\n", style(SUCCESS_MESSAGE).green())?,
-                Err(err) => {
-                    write!(out, "{}\n", style(FAILED_MESSAGE).red())?;
-                    write!(out, "{}\n", style(err.to_string()).red())?;
-                }
-            }
-        }
-
-        if !self.is_in_workpace {
-            write!(
-                out,
-                "{}\n",
-                style("Directory is not kamu workspace").yellow()
-            )?;
-        }
-        Ok(())
     }
 }
