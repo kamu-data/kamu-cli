4d90e67ab53411afa535a78e0c56b7fab9c6085b
Dima Pristupa <dima@pristupa.dev>
2024-01-18 13:21:50 +0200

    SystemTimeSource::wake_after(): introduce
    - Update implementations: SystemTimeSourceDefault,
                              SystemTimeSourceStub
    - Implement SystemTimeSource::simulate_time_passage()
    - Write unit-tests for SystemTimeSourceStub

--- src/domain/core/src/utils/time_source.rs
+++ src/domain/core/src/utils/time_source.rs
@@ -9,13 +9,17 @@
 
 use std::sync::{Arc, Mutex};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use tokio::sync::oneshot;
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 /// Abstracts the system time source
+#[async_trait::async_trait]
 pub trait SystemTimeSource: Send + Sync {
     fn now(&self) -> DateTime<Utc>;
+
+    async fn wake_after(&self, duration: Duration);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -24,46 +28,132 @@ pub trait SystemTimeSource: Send + Sync {
 #[dill::interface(dyn SystemTimeSource)]
 pub struct SystemTimeSourceDefault;
 
+#[async_trait::async_trait]
 impl SystemTimeSource for SystemTimeSourceDefault {
     fn now(&self) -> DateTime<Utc> {
         Utc::now()
+    }
+
+    async fn wake_after(&self, duration: Duration) {
+        let std_duration = duration.to_std().unwrap();
+
+        tokio::time::sleep(std_duration).await;
     }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
+type AwaitingCallerId = usize;
+type WokeUpCallersIds = Vec<AwaitingCallerId>;
+
+#[derive(Debug)]
+struct AwaitingCaller {
+    id: AwaitingCallerId,
+    time_to_wake_up: Duration,
+    waker_tx: Option<oneshot::Sender<()>>,
+}
+
+#[derive(Debug, Default)]
+struct SystemTimeSourceStubState {
+    t: Option<DateTime<Utc>>,
+    next_caller_id: AwaitingCallerId,
+    awaiting_callers: Vec<AwaitingCaller>,
+}
+
 #[derive(Debug, Clone)]
 pub struct SystemTimeSourceStub {
-    t: Arc<Mutex<Option<DateTime<Utc>>>>,
+    state: Arc<Mutex<SystemTimeSourceStubState>>,
 }
 
 impl SystemTimeSourceStub {
     pub fn new() -> Self {
         Self {
-            t: Arc::new(Mutex::new(None)),
+            state: Arc::new(Mutex::new(SystemTimeSourceStubState::default())),
         }
     }
 
     pub fn new_set(t: DateTime<Utc>) -> Self {
+        let state = SystemTimeSourceStubState {
+            t: Some(t),
+            next_caller_id: 1,
+            awaiting_callers: vec![],
+        };
+
         Self {
-            t: Arc::new(Mutex::new(Some(t))),
+            state: Arc::new(Mutex::new(state)),
         }
     }
 
     pub fn set(&self, t: DateTime<Utc>) {
-        *self.t.lock().unwrap() = Some(t);
+        let mut state = self.state.lock().unwrap();
+
+        state.t = Some(t);
     }
 
     pub fn unset(&self) {
-        *self.t.lock().unwrap() = None;
+        let mut state = self.state.lock().unwrap();
+
+        state.t = None;
+    }
+
+    pub fn simulate_time_passage(&self, time_quantum: Duration) -> WokeUpCallersIds {
+        let mut state = self.state.lock().unwrap();
+        let mut woke_up_callers_ids = vec![];
+
+        state.awaiting_callers.retain_mut(|awaiting_caller| {
+            awaiting_caller.time_to_wake_up =
+                Duration::zero().max(awaiting_caller.time_to_wake_up - time_quantum);
+
+            let is_time_up = awaiting_caller.time_to_wake_up.is_zero();
+
+            if is_time_up {
+                woke_up_callers_ids.push(awaiting_caller.id);
+
+                let tx = awaiting_caller.waker_tx.take().unwrap();
+
+                tx.send(()).unwrap();
+            }
+
+            !is_time_up
+        });
+
+        if let Some(ref mut t) = state.t {
+            *t += time_quantum;
+        }
+
+        woke_up_callers_ids
     }
 }
 
+#[async_trait::async_trait]
 impl SystemTimeSource for SystemTimeSourceStub {
     fn now(&self) -> DateTime<Utc> {
-        match *self.t.lock().unwrap() {
+        let state = self.state.lock().unwrap();
+
+        match state.t {
             None => Utc::now(),
             Some(ref t) => *t,
         }
     }
+
+    async fn wake_after(&self, duration: Duration) {
+        let (tx, rx) = oneshot::channel();
+
+        {
+            let mut state = self.state.lock().unwrap();
+
+            let new_awaiting_caller = AwaitingCaller {
+                id: state.next_caller_id,
+                time_to_wake_up: duration,
+                waker_tx: Some(tx),
+            };
+
+            state.awaiting_callers.push(new_awaiting_caller);
+            state.next_caller_id += 1;
+        }
+
+        rx.await.unwrap();
+    }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
---
+++ src/domain/core/tests/tests/utils/test_time_source.rs
@@ -0,0 +1,178 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::assert_matches::assert_matches;
+use std::time::Duration as StdDuration;
+
+use chrono::{DateTime, Duration, TimeZone, Utc};
+use kamu_core::{SystemTimeSource, SystemTimeSourceStub};
+use tokio::time::timeout;
+
+#[tokio::test]
+async fn test_stub_wake_after_without_simulate_time_passage() {
+    let t0 = point_in_time_in_a_parallel_universe();
+    let system_time_source = SystemTimeSourceStub::new_set(t0);
+
+    let wake_after_1_sec_fut = system_time_source.wake_after(Duration::seconds(1));
+    let wake_up_result_or_timeout =
+        timeout(StdDuration::from_millis(50), wake_after_1_sec_fut).await;
+
+    assert_matches!(wake_up_result_or_timeout, Err(_));
+    assert_eq!(system_time_source.now(), t0);
+}
+
+#[tokio::test]
+async fn test_stub_wake_after_with_lacking_simulate_time_passage() {
+    let t0 = point_in_time_in_a_parallel_universe();
+    let system_time_source = SystemTimeSourceStub::new_set(t0);
+
+    {
+        let dt1 = Duration::seconds(30);
+        let t1 = t0 + dt1;
+
+        let wake_up_result_or_timeout = {
+            let wake_after_60_sec_fut = system_time_source.wake_after(Duration::seconds(60));
+            let simulate_30_sec_passage_fut =
+                Box::pin(async { system_time_source.simulate_time_passage(dt1) });
+            let wake_after_fut =
+                futures::future::join(wake_after_60_sec_fut, simulate_30_sec_passage_fut);
+
+            timeout(StdDuration::from_millis(50), wake_after_fut).await
+        };
+
+        assert_matches!(wake_up_result_or_timeout, Err(_));
+        assert_eq!(system_time_source.now(), t1);
+    };
+}
+
+#[tokio::test]
+async fn test_stub_wake_after_with_several_simulate_time_passage() {
+    let t0 = point_in_time_in_a_parallel_universe();
+    let system_time_source = SystemTimeSourceStub::new_set(t0);
+    // 1. Start waiting 4 parallel futures:
+    //    - a)  60 seconds
+    //    - b)  90 seconds
+    //    - c) 120 seconds
+    //    - d) 120 seconds
+    let a_wake_after_60_sec_fut = system_time_source.wake_after(Duration::seconds(60));
+    let b_wake_after_90_sec_fut = system_time_source.wake_after(Duration::seconds(90));
+    let c_wake_after_120_sec_fut = system_time_source.wake_after(Duration::seconds(120));
+    let d_wake_after_120_sec_fut = system_time_source.wake_after(Duration::seconds(120));
+
+    let main_inner = async {
+        // 2. Simulate +30 seconds:
+        //    - a) +00:30/01:00 - waiting
+        //    - b) +00:30/01:30 - waiting
+        //    - c) +00:30/02:00 - waiting
+        //    - d) +00:30/02:00 - waiting
+        let t1 = {
+            let dt1 = Duration::seconds(30);
+            let t1 = t0 + dt1;
+
+            let woke_up_callers_ids = system_time_source.simulate_time_passage(dt1);
+
+            assert!(woke_up_callers_ids.is_empty());
+            assert_eq!(system_time_source.now(), t1);
+
+            t1
+        };
+        // 3. Simulate +30 seconds:
+        //    - a) +01:00/01:00 - done
+        //    - b) +01:00/01:30 - waiting
+        //    - c) +01:00/02:00 - waiting
+        //    - d) +01:00/02:00 - waiting
+        let t2 = {
+            let dt2 = Duration::seconds(30);
+            let t2 = t1 + dt2;
+
+            let woke_up_callers_ids = system_time_source.simulate_time_passage(dt2);
+
+            assert_eq!(woke_up_callers_ids, [1]);
+            assert_eq!(system_time_source.now(), t2);
+
+            t2
+        };
+        // 4. Simulate +30 seconds:
+        //    - a) +01:30/01:00 - done (before)
+        //    - b) +01:30/01:30 - done
+        //    - c) +01:30/02:00 - waiting
+        //    - d) +01:30/02:00 - waiting
+        let t3 = {
+            let dt3 = Duration::seconds(30);
+            let t3 = t2 + dt3;
+
+            let woke_up_callers_ids = system_time_source.simulate_time_passage(dt3);
+
+            assert_eq!(woke_up_callers_ids, [2]);
+            assert_eq!(system_time_source.now(), t3);
+
+            t3
+        };
+        // 4. Simulate +30 seconds:
+        //    - a) +01:30/01:00 - done (before)
+        //    - b) +01:30/01:30 - done (before)
+        //    - c) +01:30/02:00 - done
+        //    - d) +01:30/02:00 - done
+        {
+            let dt4 = Duration::seconds(30);
+            let t2 = t3 + dt4;
+
+            let woke_up_callers_ids = system_time_source.simulate_time_passage(dt4);
+
+            assert_eq!(woke_up_callers_ids, [3, 4]);
+            assert_eq!(system_time_source.now(), t2);
+        };
+    };
+
+    let main = futures::future::join5(
+        a_wake_after_60_sec_fut,
+        b_wake_after_90_sec_fut,
+        c_wake_after_120_sec_fut,
+        d_wake_after_120_sec_fut,
+        main_inner,
+    );
+    let main_result_or_timeout = timeout(StdDuration::from_millis(50), main).await;
+
+    assert_matches!(main_result_or_timeout, Ok(_));
+}
+
+#[tokio::test]
+async fn test_stub_wake_after_with_simulate_exceeding_passage() {
+    let t0 = point_in_time_in_a_parallel_universe();
+    let system_time_source = SystemTimeSourceStub::new_set(t0);
+    // 1. Start waiting 2 parallel futures:
+    //    - a) 60 seconds
+    //    - b) 90 seconds
+    let a_wake_after_60_sec_fut = system_time_source.wake_after(Duration::seconds(60));
+    let b_wake_after_90_sec_fut = system_time_source.wake_after(Duration::seconds(90));
+
+    let main_inner = async {
+        // 2. Simulate +90 seconds:
+        //    - a) +01:30/01:00 - 30-second overrun
+        //    - b) +01:30/01:30 - done
+        let dt1 = Duration::seconds(90);
+        let t1 = t0 + dt1;
+
+        let woke_up_callers_ids = system_time_source.simulate_time_passage(dt1);
+
+        assert_eq!(woke_up_callers_ids, [1, 2]);
+        assert_eq!(system_time_source.now(), t1);
+    };
+
+    let main = futures::future::join3(a_wake_after_60_sec_fut, b_wake_after_90_sec_fut, main_inner);
+    let main_result_or_timeout = timeout(StdDuration::from_millis(50), main).await;
+
+    assert_matches!(main_result_or_timeout, Ok(_));
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+fn point_in_time_in_a_parallel_universe() -> DateTime<Utc> {
+    Utc.with_ymd_and_hms(2050, 1, 2, 12, 0, 0).unwrap()
+}
