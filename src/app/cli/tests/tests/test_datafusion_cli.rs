69ebd6f08a48860db235b7a142e7d6573bdbc9de
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-28 20:13:57 -0700

    DataFusion-based SQL console #228: added missing integration test for datafusion-cli.

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -1,3 +1,14 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 use assert_cmd::Command;
 
 #[test]
@@ -11,7 +22,8 @@ fn test_datafusion_cli() {
 
     let output = assert.get_output();
 
-    //not using assert.stdout("expected text") as time of query execution may differ
+    //not using assert.stdout("expected text") as time of query execution may
+    // differ
     let string_result = String::from_utf8_lossy(&output.stdout);
 
     assert!(
