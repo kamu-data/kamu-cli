00eaf13dc6977302da8c8e36c51c1e63f9d0ca96
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-28 21:33:43 -0700

    DataFusion-based SQL console #228: added missing integration test for datafusion-cli

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -13,7 +13,22 @@ use assert_cmd::Command;
 
 #[test]
 fn test_datafusion_cli() {
+    let expected_output =
+        "+----------+\n| Int64(1) |\n+----------+\n| 1        |\n+----------+\n1 row in set.";
+
     let mut cmd = Command::cargo_bin("kamu-cli").unwrap();
 
-    cmd.arg("-- sql").assert().success();
+    let assert = cmd.arg("sql").write_stdin("select 1;").assert();
+
+    let output = assert.get_output();
+
+    //not using assert.stdout("expected text") as time of query execution may
+    // differ
+    let string_result = String::from_utf8_lossy(&output.stdout);
+
+    assert!(
+        string_result.contains(expected_output),
+        "Output does not contain expected text"
+    );
+    assert.success();
 }
