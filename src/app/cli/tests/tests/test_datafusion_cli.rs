5f5cc2e621987981922921d8228d57e676fe2da0
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-29 21:42:47 -0700

    DataFusion-based SQL console #228: changes after code review

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -10,25 +10,40 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 use assert_cmd::Command;
+use regex::Regex;
 
-#[test]
-fn test_datafusion_cli() {
-    let expected_output =
-        "+----------+\n| Int64(1) |\n+----------+\n| 1        |\n+----------+\n1 row in set.";
-
+#[test_group::group(engine, datafusion)]
+#[test_log::test(tokio::test)]
+async fn test_datafusion_cli() {
     let mut cmd = Command::cargo_bin("kamu-cli").unwrap();
 
     let assert = cmd.arg("sql").write_stdin("select 1;").assert();
 
     let output = assert.get_output();
 
-    //not using assert.stdout("expected text") as time of query execution may
-    // differ
     let string_result = String::from_utf8_lossy(&output.stdout);
 
-    assert!(
-        string_result.contains(expected_output),
-        "Output does not contain expected text"
+    let seconds = Regex::new(r"took (.*?) seconds.")
+        .unwrap()
+        .captures(&*string_result)
+        .and_then(|caps| caps.get(1))
+        .map(|m| m.as_str())
+        .unwrap_or("Seconds not found.");
+
+    let expected_output = format!(
+        indoc::indoc! {r#"
+            +----------+
+            | Int64(1) |
+            +----------+
+            | 1        |
+            +----------+
+            1 row in set. Query took {:.5} seconds.
+
+            \q
+        "#},
+        seconds
     );
+
+    assert_eq!(string_result.trim(), expected_output.trim());
     assert.success();
 }
