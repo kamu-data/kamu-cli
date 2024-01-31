a35363e910390171a9063a15b4d3ee641311d513
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-30 20:22:10 -0700

    DataFusion-based SQL console #228: debugging failing test by changing command

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -18,5 +18,13 @@ async fn test_datafusion_cli() {
 
     let assert = cmd.arg("sql").write_stdin("select 1;").assert();
 
-    assert.success().code(0);
+    let output = assert.get_output();
+
+    let string_result = String::from_utf8_lossy(&output.stdout);
+    let string_error = String::from_utf8_lossy(&output.stderr);
+    let string_status = output.status.to_string();
+
+    assert_eq!(string_status, "exit status: 0");
+    assert_eq!(string_error, "Kamu + DataFusion SQL shell. Type \\? for help.\n");
+    assert_eq!(string_result, "+----------+\n| Int64(1) |\n+----------+\n| 1        |\n+----------+\n1 row in set. Query took 0.011 seconds.\n\n\\q\n");
 }
