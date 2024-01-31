ff5b061e0c57ba565e2cf371cf505292c94e4857
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-30 23:15:53 -0700

    DataFusion-based SQL console #228: debugging failing test by changing command

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -8,14 +8,48 @@
 // by the Apache License, Version 2.0.
 
 /////////////////////////////////////////////////////////////////////////////////////////
-
 use assert_cmd::Command;
+use predicates::prelude::*;
+use regex::Regex;
 
 #[test_group::group(engine, datafusion)]
 #[test_log::test(tokio::test)]
 async fn test_datafusion_cli() {
     let mut cmd = Command::cargo_bin("kamu-cli").unwrap();
 
-    let assert = cmd.arg("sql").write_stdin("select 1;").assert();
-    assert.failure().stderr("");
+    let result = cmd.arg("sql").write_stdin("select 1;").assert();
+
+    match result.try_success() {
+        Ok(assert) => {
+            let output = assert.get_output();
+
+            let string_result = String::from_utf8_lossy(&output.stdout);
+
+            let seconds = Regex::new(r"took (.*?) seconds.")
+                .unwrap()
+                .captures(&string_result)
+                .and_then(|caps| caps.get(1))
+                .map_or("Seconds not found.", |m| m.as_str());
+
+            let expected_output = format!(
+                indoc::indoc! {r#"
+            +----------+
+            | Int64(1) |
+            +----------+
+            | 1        |
+            +----------+
+            1 row in set. Query took {:.5} seconds.
+
+            \q
+        "#},
+                seconds
+            );
+
+            assert_eq!(string_result.trim(), expected_output.trim());
+        }
+        Err(err) => {
+            err.assert()
+                .stdout(predicate::eq(b"Debug error\n" as &[u8]));
+        }
+    }
 }
