006fa26948b5a4e4324a34f9f7368d0ef6918b9c
wizzy-wooz <elena.vodzyanova@gmail.com>
2024-01-31 17:03:01 -0700

    DataFusion-based SQL console #228: checking escargot

--- src/app/cli/tests/tests/test_datafusion_cli.rs
+++ src/app/cli/tests/tests/test_datafusion_cli.rs
@@ -8,46 +8,23 @@
 // by the Apache License, Version 2.0.
 
 /////////////////////////////////////////////////////////////////////////////////////////
-use assert_cmd::Command;
-use regex::Regex;
+extern crate escargot;
+extern crate assert_fs;
+use std::process::{Stdio};
 
-#[test_group::group(engine, datafusion)]
 #[test_log::test(tokio::test)]
 async fn test_datafusion_cli() {
-    let mut cmd = Command::cargo_bin("kamu-cli").unwrap();
+    let run = escargot::CargoBuild::new()
+        .bin("kamu-cli")
+        .current_release()
+        .current_target()
+        .run()
+        .unwrap()
+        .command()
+        .arg("sql")
+        .stdin(Stdio::piped())
+        .status()
+        .unwrap();
 
-    let result = cmd.arg("sql").write_stdin("select 1;").assert();
-
-    match result.try_success() {
-        Ok(assert) => {
-            let output = assert.get_output();
-
-            let string_result = String::from_utf8_lossy(&output.stdout);
-
-            let seconds = Regex::new(r"took (.*?) seconds.")
-                .unwrap()
-                .captures(&string_result)
-                .and_then(|caps| caps.get(1))
-                .map_or("Seconds not found.", |m| m.as_str());
-
-            let expected_output = format!(
-                indoc::indoc! {r#"
-            +----------+
-            | Int64(1) |
-            +----------+
-            | 1        |
-            +----------+
-            1 row in set. Query took {:.5} seconds.
-
-            \q
-        "#},
-                seconds
-            );
-
-            assert_eq!(string_result.trim(), expected_output.trim());
-        }
-        Err(err) => {
-            err.assert().stdout("");
-        }
-    }
+    assert_eq!(run.success(), true);
 }
