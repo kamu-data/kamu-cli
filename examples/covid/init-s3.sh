a8e7b01f8fcd7f5f2c0363a1986a12c295488fac
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2024-02-27 11:13:41 -0800

    Rename covid example and demo datasets

--- examples/covid/init-s3.sh
+++ examples/covid/init-s3.sh
@@ -6,9 +6,9 @@ S3_BASE_URL="https://s3.us-west-2.amazonaws.com/datasets.kamu.dev/odf/v2/example
 
 kamu init || true
 
-kamu pull "${S3_BASE_URL}alberta.case-details"
-kamu pull "${S3_BASE_URL}british-columbia.case-details"
-kamu pull "${S3_BASE_URL}ontario.case-details"
-kamu pull "${S3_BASE_URL}quebec.case-details"
+kamu pull "${S3_BASE_URL}covid19.alberta.case-details"
+kamu pull "${S3_BASE_URL}covid19.british-columbia.case-details"
+kamu pull "${S3_BASE_URL}covid19.ontario.case-details"
+kamu pull "${S3_BASE_URL}covid19.quebec.case-details"
 
 kamu add -r .
