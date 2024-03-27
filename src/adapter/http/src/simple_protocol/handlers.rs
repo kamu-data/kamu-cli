1908d0af7c98e1a976cb326d916b8d77e3844e0b
Dima Pristupa <dima@pristupa.dev>
2024-03-27 18:34:46 +0200

    Smart Transfer Protocol: respect KAMU_BASE_URL

--- src/adapter/http/src/simple_protocol/handlers.rs
+++ src/adapter/http/src/simple_protocol/handlers.rs
@@ -279,8 +279,15 @@ fn get_base_dataset_url(
 /////////////////////////////////////////////////////////////////////////////////
 
 fn get_api_server_url(host: &str) -> Url {
-    let scheme = std::env::var("KAMU_PROTOCOL_SCHEME").unwrap_or_else(|_| String::from("http"));
-    Url::parse(&format!("{scheme}://{host}")).unwrap()
+    // TODO: Use value from config not envvar
+    //       https://github.com/kamu-data/kamu-node/issues/45
+    let raw_base_url = std::env::var("KAMU_BASE_URL").unwrap_or_else(|_| {
+        let scheme = std::env::var("KAMU_PROTOCOL_SCHEME").unwrap_or_else(|_| String::from("http"));
+
+        format!("{scheme}://{host}")
+    });
+
+    Url::parse(&raw_base_url).unwrap()
 }
 
 /////////////////////////////////////////////////////////////////////////////////
