4441cc1d5189a71494eb7b17d0a960e723c68be2
Dima Pristupa <dima@pristupa.dev>
2024-03-29 12:44:26 +0200

    Data access panel: GraphQL initial API (#570)
    * GraphQL: Dataset->Endpoints initial implementation
    * OData: respect KAMU_BASE_URL
    * Smart Transfer Protocol: respect KAMU_BASE_URL

---
+++ src/adapter/graphql/src/scalars/dataset_endpoints.rs
@@ -0,0 +1,63 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use crate::prelude::*;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(SimpleObject, Debug)]
+pub struct LinkProtocolDesc {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct CliProtocolDesc {
+    pub pull_command: String,
+    pub push_command: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct RestProtocolDesc {
+    pub tail_url: String,
+    pub query_url: String,
+    pub push_url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct FlightSqlDesc {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct JdbcDesc {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct PostgreSqlDesl {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct KafkaProtocolDesc {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct WebSocketProtocolDesc {
+    pub url: String,
+}
+
+#[derive(SimpleObject, Debug)]
+pub struct OdataProtocolDesc {
+    pub service_url: String,
+    pub collection_url: String,
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
