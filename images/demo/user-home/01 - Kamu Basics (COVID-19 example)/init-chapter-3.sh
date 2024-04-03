01ee8ab3f24174c1522cb1300453a8a3b90909c2
Dima Pristupa <dima@pristupa.dev>
2024-04-03 14:44:16 +0300

    Trivial shell scripts refactor

--- images/demo/user-home/01 - Kamu Basics (COVID-19 example)/init-chapter-3.sh
+++ images/demo/user-home/01 - Kamu Basics (COVID-19 example)/init-chapter-3.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf .kamu
 kamu init
 
-kamu repo add kamu-node ${KAMU_NODE_URL}
+kamu repo add kamu-node "${KAMU_NODE_URL}"
 kamu pull kamu-node/kamu/covid19.british-columbia.case-details --no-alias
 kamu pull kamu-node/kamu/covid19.ontario.case-details
 kamu add datasets/canada*.yaml
