f775cde988402a1fc451820f64537f0e2a94a83f
Dima Pristupa <dima@pristupa.dev>
2024-04-18 17:51:26 +0300

    Fix formatting issue

--- src/infra/flow-system/inmem/tests/tests/test_flow_service_inmem.rs
+++ src/infra/flow-system/inmem/tests/tests/test_flow_service_inmem.rs
@@ -18,12 +18,12 @@ use kamu::*;
 use kamu_core::*;
 use kamu_flow_system::*;
 use kamu_flow_system_inmem::*;
+use kamu_flow_system_services::{FlowConfigurationServiceImpl, FlowServiceInMemory};
 use kamu_task_system::*;
 use kamu_task_system_inmem::TaskSystemEventStoreInMemory;
 use kamu_task_system_services::TaskSchedulerImpl;
 use opendatafabric::*;
 use tokio::task::yield_now;
-use kamu_flow_system_services::{FlowConfigurationServiceImpl, FlowServiceInMemory};
 
 use super::{
     FlowSystemTestListener,
