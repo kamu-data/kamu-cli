e12e7a68d9f9ac4fe30391debf56eb2119aa893b
Dima Pristupa <dima@pristupa.dev>
2024-04-24 17:52:59 +0300

    kamu-flow-system-sqlite: introduce

---
+++ migrations/sqlite/20240422100311_flow_configuration_events.sql
@@ -0,0 +1,26 @@
+CREATE TABLE flow_configuration_event
+(
+    event_id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    created_time timestamptz                       NOT NULL
+);
+
+CREATE TABLE system_flow_configuration_events
+(
+    event_id         INTEGER PRIMARY KEY                              NOT NULL,
+    system_flow_type VARCHAR(10) CHECK ( system_flow_type IN ('gc') ) NOT NULL,
+    event_type       VARCHAR(50)                                      NOT NULL,
+    event_time       TIMESTAMPTZ                                      NOT NULL,
+    event_payload    JSONB                                            NOT NULL
+);
+
+CREATE TABLE dataset_flow_configuration_events
+(
+    event_id          INTEGER PRIMARY KEY                                                                           NOT NULL,
+    dataset_id        VARCHAR(100)                                                                                  NOT NULL,
+    dataset_flow_type VARCHAR(20) CHECK ( dataset_flow_type IN ('ingest', 'execute_transform', 'hard_compacting') ) NOT NULL,
+    event_type        VARCHAR(50)                                                                                   NOT NULL,
+    event_time        TIMESTAMPTZ                                                                                   NOT NULL,
+    event_payload     JSONB                                                                                         NOT NULL
+);
+
+CREATE INDEX dataset_flow_configuration_events_dataset_id_idx ON dataset_flow_configuration_events (dataset_id, dataset_flow_type);
---
+++ src/infra/flow-system/sqlite/tests/tests/test_flow_configuration_event_store_sqlite.rs
@@ -0,0 +1,81 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use database_common::{run_transactional, SqliteTransactionManager};
+use dill::{Catalog, CatalogBuilder};
+use kamu_flow_system_sqlite::FlowSystemEventStoreSqlite;
+use sqlx::SqlitePool;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#[test_group::group(database, sqlite)]
+#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
+async fn test_event_store_empty(sqlite_pool: SqlitePool) {
+    let harness = SqliteAccountRepositoryHarness::new(sqlite_pool);
+
+    run_transactional(&harness.catalog, |catalog: Catalog| async move {
+        kamu_flow_system_repo_tests::test_event_store_empty(&catalog).await;
+        Ok(())
+    })
+    .await
+    .unwrap();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#[test_group::group(database, sqlite)]
+#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
+async fn test_event_store_get_streams(sqlite_pool: SqlitePool) {
+    let harness = SqliteAccountRepositoryHarness::new(sqlite_pool);
+
+    run_transactional(&harness.catalog, |catalog: Catalog| async move {
+        kamu_flow_system_repo_tests::test_event_store_get_streams(&catalog).await;
+        Ok(())
+    })
+    .await
+    .unwrap();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#[test_group::group(database, sqlite)]
+#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
+async fn test_event_store_get_events_with_windowing(sqlite_pool: SqlitePool) {
+    let harness = SqliteAccountRepositoryHarness::new(sqlite_pool);
+
+    run_transactional(&harness.catalog, |catalog: Catalog| async move {
+        kamu_flow_system_repo_tests::test_event_store_get_events_with_windowing(&catalog).await;
+        Ok(())
+    })
+    .await
+    .unwrap();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Harness
+/////////////////////////////////////////////////////////////////////////////////////////
+
+struct SqliteAccountRepositoryHarness {
+    catalog: Catalog,
+}
+
+impl SqliteAccountRepositoryHarness {
+    pub fn new(sqlite_pool: SqlitePool) -> Self {
+        // Initialize catalog with predefined Postgres pool
+        let mut catalog_builder = CatalogBuilder::new();
+        catalog_builder.add_value(sqlite_pool);
+        catalog_builder.add::<SqliteTransactionManager>();
+        catalog_builder.add::<FlowSystemEventStoreSqlite>();
+
+        Self {
+            catalog: catalog_builder.build(),
+        }
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////
