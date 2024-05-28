9503eae9eb77f3248c4d5b5f6b5c2027360e7da5
Dima Pristupa <dima@pristupa.dev>
2024-05-28 17:01:46 +0300

    TaskExecutorImpl: split transactions

--- src/domain/task-system/services/src/task_executor_impl.rs
+++ src/domain/task-system/services/src/task_executor_impl.rs
@@ -82,7 +82,7 @@ impl TaskExecutor for TaskExecutorImpl {
     // TODO: Error and panic handling strategy
     async fn run(&self) -> Result<(), InternalError> {
         loop {
-            DatabaseTransactionRunner::run_transactional(
+            let mut task = DatabaseTransactionRunner::run_transactional(
                 &self.catalog,
                 |updated_catalog| async move {
                     let event_store = updated_catalog
@@ -90,108 +90,116 @@ impl TaskExecutor for TaskExecutorImpl {
                         .int_err()?;
 
                     let task_id = self.task_sched.take().await.int_err()?;
-                    let mut task = Task::load(task_id, event_store.as_ref()).await.int_err()?;
 
-                    tracing::info!(
-                        %task_id,
-                        logical_plan = ?task.logical_plan,
-                        "Executing task",
-                    );
-
-                    self.publish_task_running(task.task_id).await?;
-
-                    let outcome = match &task.logical_plan {
-                        LogicalPlan::UpdateDataset(upd) => {
-                            let pull_svc =
-                                updated_catalog.get_one::<dyn PullService>().int_err()?;
-                            let maybe_pull_result = pull_svc
-                                .pull(&upd.dataset_id.as_any_ref(), PullOptions::default(), None)
-                                .await;
-
-                            match maybe_pull_result {
-                                Ok(pull_result) => TaskOutcome::Success(
-                                    TaskResult::UpdateDatasetResult(pull_result.into()),
-                                ),
-                                Err(_) => TaskOutcome::Failed(TaskError::Empty),
-                            }
-                        }
-                        LogicalPlan::Probe(Probe {
-                            busy_time,
-                            end_with_outcome,
-                            ..
-                        }) => {
-                            if let Some(busy_time) = busy_time {
-                                tokio::time::sleep(*busy_time).await;
-                            }
-                            end_with_outcome
-                                .clone()
-                                .unwrap_or(TaskOutcome::Success(TaskResult::Empty))
-                        }
-                        LogicalPlan::HardCompactingDataset(HardCompactingDataset {
-                            dataset_id,
-                            max_slice_size,
-                            max_slice_records,
-                            keep_metadata_only,
-                        }) => {
-                            let compacting_svc = updated_catalog
-                                .get_one::<dyn CompactingService>()
-                                .int_err()?;
-                            let dataset_repo = updated_catalog
-                                .get_one::<dyn DatasetRepository>()
-                                .int_err()?;
-                            let dataset_handle = dataset_repo
-                                .resolve_dataset_ref(&dataset_id.as_local_ref())
-                                .await
-                                .int_err()?;
-
-                            let compacting_result = compacting_svc
-                                .compact_dataset(
-                                    &dataset_handle,
-                                    CompactingOptions {
-                                        max_slice_size: *max_slice_size,
-                                        max_slice_records: *max_slice_records,
-                                        keep_metadata_only: *keep_metadata_only,
-                                    },
-                                    None,
-                                )
-                                .await;
-
-                            match compacting_result {
-                                Ok(result) => TaskOutcome::Success(
-                                    TaskResult::CompactingDatasetResult(result.into()),
-                                ),
-                                Err(err) => {
-                                    tracing::info!(
-                                        %task_id,
-                                        logical_plan = ?task.logical_plan,
-                                        err = ?err,
-                                        "Task failed",
-                                    );
-                                    TaskOutcome::Failed(TaskError::Empty)
-                                }
-                            }
-                        }
-                    };
-
-                    tracing::info!(
-                        %task_id,
-                        logical_plan = ?task.logical_plan,
-                        ?outcome,
-                        "Task finished",
-                    );
-
-                    // Refresh the task in case it was updated concurrently (e.g. late cancellation)
-                    task.update(event_store.as_ref()).await.int_err()?;
-                    task.finish(self.time_source.now(), outcome.clone())
-                        .int_err()?;
-                    task.save(event_store.as_ref()).await.int_err()?;
-
-                    self.publish_task_finished(task.task_id, outcome).await?;
-
-                    Ok(())
+                    Task::load(task_id, event_store.as_ref()).await.int_err()
                 },
             )
             .await?;
+
+            tracing::info!(
+                %task.task_id,
+                logical_plan = ?task.logical_plan,
+                "Executing task",
+            );
+
+            self.publish_task_running(task.task_id).await?;
+
+            let outcome = match &task.logical_plan {
+                LogicalPlan::UpdateDataset(upd) => {
+                    let pull_svc = self.catalog.get_one::<dyn PullService>().int_err()?;
+                    let maybe_pull_result = pull_svc
+                        .pull(&upd.dataset_id.as_any_ref(), PullOptions::default(), None)
+                        .await;
+
+                    match maybe_pull_result {
+                        Ok(pull_result) => TaskOutcome::Success(TaskResult::UpdateDatasetResult(
+                            pull_result.into(),
+                        )),
+                        Err(_) => TaskOutcome::Failed(TaskError::Empty),
+                    }
+                }
+                LogicalPlan::Probe(Probe {
+                    busy_time,
+                    end_with_outcome,
+                    ..
+                }) => {
+                    if let Some(busy_time) = busy_time {
+                        tokio::time::sleep(*busy_time).await;
+                    }
+                    end_with_outcome
+                        .clone()
+                        .unwrap_or(TaskOutcome::Success(TaskResult::Empty))
+                }
+                LogicalPlan::HardCompactingDataset(HardCompactingDataset {
+                    dataset_id,
+                    max_slice_size,
+                    max_slice_records,
+                    keep_metadata_only,
+                }) => {
+                    let compacting_svc =
+                        self.catalog.get_one::<dyn CompactingService>().int_err()?;
+                    let dataset_repo = self.catalog.get_one::<dyn DatasetRepository>().int_err()?;
+                    let dataset_handle = dataset_repo
+                        .resolve_dataset_ref(&dataset_id.as_local_ref())
+                        .await
+                        .int_err()?;
+
+                    let compacting_result = compacting_svc
+                        .compact_dataset(
+                            &dataset_handle,
+                            CompactingOptions {
+                                max_slice_size: *max_slice_size,
+                                max_slice_records: *max_slice_records,
+                                keep_metadata_only: *keep_metadata_only,
+                            },
+                            None,
+                        )
+                        .await;
+
+                    match compacting_result {
+                        Ok(result) => {
+                            TaskOutcome::Success(TaskResult::CompactingDatasetResult(result.into()))
+                        }
+                        Err(err) => {
+                            tracing::info!(
+                                %task.task_id,
+                                logical_plan = ?task.logical_plan,
+                                err = ?err,
+                                "Task failed",
+                            );
+                            TaskOutcome::Failed(TaskError::Empty)
+                        }
+                    }
+                }
+            };
+
+            tracing::info!(
+                %task.task_id,
+                logical_plan = ?task.logical_plan,
+                ?outcome,
+                "Task finished",
+            );
+
+            let outcome_clone = outcome.clone();
+            let task = DatabaseTransactionRunner::run_transactional(
+                &self.catalog,
+                |updated_catalog| async move {
+                    let event_store = updated_catalog
+                        .get_one::<dyn TaskSystemEventStore>()
+                        .int_err()?;
+
+                    // Refresh the task in case it was updated concurrently (e.g. late cancellation)
+                    task.update(event_store.as_ref()).await.int_err()?;
+                    task.finish(self.time_source.now(), outcome_clone)
+                        .int_err()?;
+                    task.save(event_store.as_ref()).await.int_err()?;
+
+                    Ok(task)
+                },
+            )
+            .await?;
+
+            self.publish_task_finished(task.task_id, outcome).await?;
         }
     }
 }
