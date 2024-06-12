9b480f534cd44114496e110dd91d5af3cfb937fb
Dima Pristupa <dima@pristupa.dev>
2024-06-12 15:38:09 +0300

    kamu-cli-e2e-sqlite: introduce

--- src/e2e/app/cli/common/src/e2e_test.rs
+++ src/e2e/app/cli/common/src/e2e_test.rs
@@ -13,8 +13,9 @@ use std::net::{Ipv4Addr, SocketAddrV4};
 
 use internal_error::InternalError;
 use kamu_cli_wrapper::Kamu;
+use regex::Regex;
 use reqwest::Url;
-use sqlx::{MySqlPool, PgPool};
+use sqlx::{MySqlPool, PgPool, SqlitePool};
 
 use crate::KamuApiServerClient;
 
@@ -45,6 +46,8 @@ pub async fn e2e_test<ServerRunFut, Fixture, FixtureFut>(
     assert_matches!(tokio::try_join!(server_run_fut, test_fut), Ok(_));
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Database-related E2E bootstrapping functions
 ////////////////////////////////////////////////////////////////////////////////
 
 pub async fn postgres_kamu_cli_e2e_test<F, Fut>(pg_pool: PgPool, fixture: F)
@@ -125,6 +128,51 @@ where
 
 ////////////////////////////////////////////////////////////////////////////////
 
+pub async fn sqlite_kamu_cli_e2e_test<F, Fut>(sqlite_pool: SqlitePool, fixture: F)
+where
+    F: FnOnce(KamuApiServerClient) -> Fut,
+    Fut: Future<Output = ()>,
+{
+    // Ugly way to get the path as the settings have a not-so-good signature:
+    // SqliteConnectOptions::get_filename(self) -> Cow<'static, Path>
+    //                                    ^^^^
+    // Arc<T> + consuming = bad combo
+    let database_path = {
+        let re = Regex::new(r#"filename: "(.*)""#).unwrap();
+        let connect_options = format!("{:#?}", sqlite_pool.connect_options());
+        let re_groups = re.captures(connect_options.as_str()).unwrap();
+
+        re_groups[1].to_string()
+    };
+    let kamu = Kamu::new_workspace_tmp_with_kamu_config(
+        format!(
+            indoc::indoc!(
+                r#"
+                kind: CLIConfig
+                version: 1
+                content:
+                    database:
+                        provider: sqlite
+                        databasePath: {path}
+                "#
+            ),
+            path = database_path
+        )
+        .as_str(),
+    )
+    .await;
+
+    // TODO: Random port support -- this unlocks parallel running
+    let server_addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 4000);
+    let server_run_fut = kamu.start_api_server(server_addr);
+
+    e2e_test(server_addr, server_run_fut, fixture).await;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Macros
+////////////////////////////////////////////////////////////////////////////////
+
 #[macro_export]
 macro_rules! kamu_cli_e2e_test {
     (postgres_kamu_cli_e2e_test, $test_package: expr, $test_name: expr) => {
@@ -147,6 +195,19 @@ macro_rules! kamu_cli_e2e_test {
             async fn [<$test_name>] (mysql_pool: sqlx::MySqlPool) {
                 mysql_kamu_cli_e2e_test (
                     mysql_pool,
+                    $test_package::$test_name,
+                )
+                .await;
+            }
+        }
+    };
+    (sqlite_kamu_cli_e2e_test, $test_package: expr, $test_name: expr) => {
+        paste::paste! {
+            #[test_group::group(e2e, database, sqlite)]
+            #[test_log::test(sqlx::test(migrations = "../../../../../migrations/sqlite"))]
+            async fn [<$test_name>] (sqlite_pool: sqlx::SqlitePool) {
+                sqlite_kamu_cli_e2e_test (
+                    sqlite_pool,
                     $test_package::$test_name,
                 )
                 .await;
