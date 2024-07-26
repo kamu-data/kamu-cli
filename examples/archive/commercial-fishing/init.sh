78e1b3c4c85b16cf24c48d03f18c05cfa336d002
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2024-07-26 11:38:22 -0700

    Incorporate europort demos

---
+++ examples/archive/commercial-fishing/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+KAMU_NODE_URL="odf+https://node.demo.kamu.dev/"
+
+kamu init --multi-tenant --exists-ok
+
+kamu --account acme.fishing.co pull "${KAMU_NODE_URL}acme.fishing.co/vessels.gps"
+kamu --account acme.fishing.co pull "${KAMU_NODE_URL}acme.fishing.co/vessels.trawl"
+kamu --account acme.fishing.co pull "${KAMU_NODE_URL}acme.fishing.co/vessels.fuel"
+kamu --account acme.fishing.co pull "${KAMU_NODE_URL}acme.fishing.co/vessels.location-annotated"
+kamu --account globalfishingwatch.org pull "${KAMU_NODE_URL}globalfishingwatch.org/protected-areas"
---
+++ examples/archive/water-management/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+KAMU_NODE_URL="odf+https://node.demo.kamu.dev/"
+
+kamu init --multi-tenant --exists-ok
+
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/stations"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.boven-rijn"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.ijssel"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.lek"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.nederrijn"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.pannerdensch-kanaal"
+kamu --account rijkswaterstaat.nl pull "${KAMU_NODE_URL}rijkswaterstaat.nl/measurements.waal"
+kamu --account deltares.nl pull "${KAMU_NODE_URL}deltares.nl/rhine-basin.netherlands"
+kamu --account deltares.nl pull "${KAMU_NODE_URL}deltares.nl/rhine-basin.netherlands.sim"
