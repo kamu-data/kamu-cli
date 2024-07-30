3360707617f53903a28f784b0301262e2113250e
Dima Pristupa <dima@pristupa.dev>
2024-07-30 19:31:02 +0300

    E2E, test_datafusion_cli: add "engine, datafusion" test groups

--- src/e2e/app/cli/inmem/tests/tests/commands/test_sql_command.rs
+++ src/e2e/app/cli/inmem/tests/tests/commands/test_sql_command.rs
@@ -13,7 +13,8 @@ use kamu_cli_e2e_common::prelude::*;
 
 kamu_cli_execute_command_e2e_test!(
     storage = inmem,
-    fixture = kamu_cli_e2e_repo_tests::test_datafusion_cli
+    fixture = kamu_cli_e2e_repo_tests::test_datafusion_cli,
+    extra_test_groups = "engine, datafusion"
 );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
