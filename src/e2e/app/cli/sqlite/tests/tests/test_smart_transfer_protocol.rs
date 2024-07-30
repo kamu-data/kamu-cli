e175b7b432340c3769363d3e5f55f113fa142720
Dima Pristupa <dima@pristupa.dev>
2024-07-30 20:03:56 +0300

    E2E, test_smart_push_pull_sequence(): add "engine, ingest, datafusion" test groups

--- src/e2e/app/cli/sqlite/tests/tests/test_smart_transfer_protocol.rs
+++ src/e2e/app/cli/sqlite/tests/tests/test_smart_transfer_protocol.rs
@@ -19,7 +19,8 @@ kamu_cli_run_api_server_e2e_test!(
     // use the projected date (the current day) as a workaround.
     options = Options::default()
         .with_multi_tenant()
-        .with_today_as_frozen_system_time()
+        .with_today_as_frozen_system_time(),
+    extra_test_groups = "engine, ingest, datafusion"
 );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
