ff447ab1a54e9fd14109ae7fc1c068998c9b1399
Dima Pristupa <dima@pristupa.dev>
2024-08-14 15:56:44 +0300

    kamu-datasets-repo-tests: use database_transactional_test!()

--- src/infra/datasets/inmem/tests/repos/test_inmem_dataset_entry_repository.rs
+++ src/infra/datasets/inmem/tests/repos/test_inmem_dataset_entry_repository.rs
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use database_common_macros::database_transactional_test;
 use dill::{Catalog, CatalogBuilder};
 use kamu_accounts_inmem::InMemoryAccountRepository;
 use kamu_datasets_inmem::InMemoryDatasetEntryRepository;
@@ -14,78 +15,67 @@ use kamu_datasets_repo_tests::dataset_entry_repo;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_log::test(tokio::test)]
-async fn test_get_dataset_entry() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_get_dataset_entry(&harness.catalog).await;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#[test_log::test(tokio::test)]
-async fn test_get_dataset_entry_by_name() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_get_dataset_entry_by_name(&harness.catalog).await;
-}
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_get_dataset_entry,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_log::test(tokio::test)]
-async fn test_get_dataset_entries_by_owner_id() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_get_dataset_entries_by_owner_id(&harness.catalog).await;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#[test_log::test(tokio::test)]
-async fn test_try_save_duplicate_dataset_entry() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_try_save_duplicate_dataset_entry(&harness.catalog).await;
-}
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_get_dataset_entry_by_name,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_log::test(tokio::test)]
-async fn test_try_save_dataset_entry_with_name_collision() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_try_save_dataset_entry_with_name_collision(&harness.catalog).await;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#[test_log::test(tokio::test)]
-async fn test_try_set_same_dataset_name_for_another_owned_dataset_entry() {
-    let harness = InmemDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_try_set_same_dataset_name_for_another_owned_dataset_entry(
-        &harness.catalog,
-    )
-    .await;
-}
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_get_dataset_entries_by_owner_id,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_log::test(tokio::test)]
-async fn test_update_dataset_entry_name() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
-
-    dataset_entry_repo::test_update_dataset_entry_name(&harness.catalog).await;
-}
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_try_save_duplicate_dataset_entry,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_log::test(tokio::test)]
-async fn test_delete_dataset_entry() {
-    let harness = InMemoryDatasetEntryRepositoryHarness::new();
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_try_save_dataset_entry_with_name_collision,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
-    dataset_entry_repo::test_delete_dataset_entry(&harness.catalog).await;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_try_set_same_dataset_name_for_another_owned_dataset_entry,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_update_dataset_entry_name,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+database_transactional_test!(
+    storage = inmem,
+    fixture = dataset_entry_repo::test_delete_dataset_entry,
+    harness = InMemoryDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
--- src/infra/datasets/sqlite/tests/repos/test_sqlite_dataset_entry_repository.rs
+++ src/infra/datasets/sqlite/tests/repos/test_sqlite_dataset_entry_repository.rs
@@ -7,9 +7,9 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use database_common::{DatabaseTransactionRunner, SqliteTransactionManager};
+use database_common::SqliteTransactionManager;
+use database_common_macros::database_transactional_test;
 use dill::{Catalog, CatalogBuilder};
-use internal_error::InternalError;
 use kamu_accounts_sqlite::SqliteAccountRepository;
 use kamu_datasets_repo_tests::dataset_entry_repo;
 use kamu_datasets_sqlite::SqliteDatasetEntryRepository;
@@ -17,134 +17,67 @@ use sqlx::SqlitePool;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_get_dataset_entry(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_get_dataset_entry(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_get_dataset_entry,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_get_dataset_entry_by_name(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_get_dataset_entry_by_name(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_get_dataset_entry_by_name,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_get_dataset_entries_by_owner_id(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_get_dataset_entries_by_owner_id(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_get_dataset_entries_by_owner_id,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_try_save_duplicate_dataset_entry(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_try_save_duplicate_dataset_entry(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_try_save_duplicate_dataset_entry,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_try_save_dataset_entry_with_name_collision(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_try_save_dataset_entry_with_name_collision(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_try_save_dataset_entry_with_name_collision,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_try_set_same_dataset_name_for_another_owned_dataset_entry(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_try_set_same_dataset_name_for_another_owned_dataset_entry(
-                &catalog,
-            )
-            .await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_try_set_same_dataset_name_for_another_owned_dataset_entry,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_update_dataset_entry_name(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_update_dataset_entry_name(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_update_dataset_entry_name,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_delete_dataset_entry(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEntryRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_entry_repo::test_delete_dataset_entry(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_entry_repo::test_delete_dataset_entry,
+    harness = SqliteDatasetEntryRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
