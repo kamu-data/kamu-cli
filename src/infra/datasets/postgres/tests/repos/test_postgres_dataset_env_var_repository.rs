226a391ccd69e4e5ad3c8bbf0a68cd2d7e982850
Dima Pristupa <dima@pristupa.dev>
2024-08-23 18:15:03 +0300

    kamu-datasets-postgres: use database_transactional_test!()

--- src/infra/datasets/postgres/tests/repos/test_postgres_dataset_env_var_repository.rs
+++ src/infra/datasets/postgres/tests/repos/test_postgres_dataset_env_var_repository.rs
@@ -7,92 +7,52 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use database_common::{DatabaseTransactionRunner, PostgresTransactionManager};
+use database_common::PostgresTransactionManager;
+use database_common_macros::database_transactional_test;
 use dill::{Catalog, CatalogBuilder};
-use internal_error::InternalError;
 use kamu_datasets_postgres::PostgresDatasetEnvVarRepository;
 use kamu_datasets_repo_tests::dataset_env_var_repo;
 use sqlx::PgPool;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, postgres)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/postgres"))]
-async fn test_missing_dataset_env_var_not_found(pg_pool: PgPool) {
-    let harness = PostgresDatasetEnvVarRepositoryHarness::new(pg_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_missing_dataset_env_var_not_found(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = postgres,
+    fixture = dataset_env_var_repo::test_missing_dataset_env_var_not_found,
+    harness = PostgresDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, postgres)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/postgres"))]
-async fn test_insert_and_get_dataset_env_var(pg_pool: PgPool) {
-    let harness = PostgresDatasetEnvVarRepositoryHarness::new(pg_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_insert_and_get_dataset_env_var(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = postgres,
+    fixture = dataset_env_var_repo::test_insert_and_get_dataset_env_var,
+    harness = PostgresDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, postgres)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/postgres"))]
-async fn test_insert_and_get_multiple_dataset_env_vars(pg_pool: PgPool) {
-    let harness = PostgresDatasetEnvVarRepositoryHarness::new(pg_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_insert_and_get_multiple_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = postgres,
+    fixture = dataset_env_var_repo::test_insert_and_get_multiple_dataset_env_vars,
+    harness = PostgresDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, postgres)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/postgres"))]
-async fn test_delete_dataset_env_vars(pg_pool: PgPool) {
-    let harness = PostgresDatasetEnvVarRepositoryHarness::new(pg_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_delete_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = postgres,
+    fixture = dataset_env_var_repo::test_delete_dataset_env_vars,
+    harness = PostgresDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, postgres)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/postgres"))]
-async fn test_modify_dataset_env_vars(pg_pool: PgPool) {
-    let harness = PostgresDatasetEnvVarRepositoryHarness::new(pg_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_modify_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = postgres,
+    fixture = dataset_env_var_repo::test_modify_dataset_env_vars,
+    harness = PostgresDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -113,3 +73,5 @@ impl PostgresDatasetEnvVarRepositoryHarness {
         }
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
