279f93dfbdfc6981c08446a4360c5fb4943ce957
Dima Pristupa <dima@pristupa.dev>
2024-08-23 18:17:00 +0300

    kamu-datasets-sqlite: use database_transactional_test!()

--- src/infra/datasets/sqlite/tests/repos/test_sqlite_dataset_env_var_repository.rs
+++ src/infra/datasets/sqlite/tests/repos/test_sqlite_dataset_env_var_repository.rs
@@ -7,92 +7,52 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use database_common::{DatabaseTransactionRunner, SqliteTransactionManager};
+use database_common::SqliteTransactionManager;
+use database_common_macros::database_transactional_test;
 use dill::{Catalog, CatalogBuilder};
-use internal_error::InternalError;
 use kamu_datasets_repo_tests::dataset_env_var_repo;
 use kamu_datasets_sqlite::SqliteDatasetEnvVarRepository;
 use sqlx::SqlitePool;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_missing_dataset_env_var_not_found(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEnvVarRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_missing_dataset_env_var_not_found(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_env_var_repo::test_missing_dataset_env_var_not_found,
+    harness = SqliteDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_insert_and_get_dataset_env_var(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEnvVarRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_insert_and_get_dataset_env_var(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_env_var_repo::test_insert_and_get_dataset_env_var,
+    harness = SqliteDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_insert_and_get_multiple_dataset_env_vars(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEnvVarRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_insert_and_get_multiple_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_env_var_repo::test_insert_and_get_multiple_dataset_env_vars,
+    harness = SqliteDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_delete_dataset_env_vars(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEnvVarRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_delete_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_env_var_repo::test_delete_dataset_env_vars,
+    harness = SqliteDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[test_group::group(database, sqlite)]
-#[test_log::test(sqlx::test(migrations = "../../../../migrations/sqlite"))]
-async fn test_modify_dataset_env_vars(sqlite_pool: SqlitePool) {
-    let harness = SqliteDatasetEnvVarRepositoryHarness::new(sqlite_pool);
-
-    DatabaseTransactionRunner::new(harness.catalog)
-        .transactional(|catalog| async move {
-            dataset_env_var_repo::test_modify_dataset_env_vars(&catalog).await;
-            Ok::<_, InternalError>(())
-        })
-        .await
-        .unwrap();
-}
+database_transactional_test!(
+    storage = sqlite,
+    fixture = dataset_env_var_repo::test_modify_dataset_env_vars,
+    harness = SqliteDatasetEnvVarRepositoryHarness
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -113,3 +73,5 @@ impl SqliteDatasetEnvVarRepositoryHarness {
         }
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
