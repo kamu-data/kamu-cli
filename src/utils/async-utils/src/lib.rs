a6fdcca2c656bead97bc2d789ebb97d78357ceb7
Dima Pristupa <dima@pristupa.dev>
2024-09-02 16:07:59 +0300

    async-utils: introduce

---
+++ src/utils/async-utils/src/lib.rs
@@ -0,0 +1,34 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[async_trait::async_trait(?Send)]
+pub trait ResultAsync<T, E> {
+    async fn and_then_async<F, FFut, U>(self, op: F) -> Result<U, E>
+    where
+        F: FnOnce(T) -> FFut,
+        FFut: std::future::Future<Output = Result<U, E>>;
+}
+
+#[async_trait::async_trait(?Send)]
+impl<T, E> ResultAsync<T, E> for Result<T, E> {
+    async fn and_then_async<F, FFut, U>(self, op: F) -> Result<U, E>
+    where
+        F: FnOnce(T) -> FFut,
+        FFut: std::future::Future<Output = Result<U, E>>,
+    {
+        match self {
+            Ok(val) => op(val).await,
+            Err(e) => Err(e),
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
