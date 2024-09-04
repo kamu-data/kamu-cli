5ab1ef224cbaa72f6ecaab267810ecc5f9b7c4e7
Dima Pristupa <dima@pristupa.dev>
2024-09-04 16:07:11 +0300

    CI: use FEATURE_TAG without branch-name

--- images/scripts/get-feature-image-tag.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# Firstly, check the tag, because it may be missing
-VERSION=$(git describe --tags --exact-match | grep -E "^v[0-9]+$")
-FEATURE=$(git rev-parse --abbrev-ref HEAD | sed 's/\//-/g')
-
-echo "${FEATURE}-${VERSION}"
