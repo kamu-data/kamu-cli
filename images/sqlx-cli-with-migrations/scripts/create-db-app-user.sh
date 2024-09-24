068a970225a799550adcef5f2458b2b1e0bb4087
Sergei Zaychenko <szaychenko@kamu.dev>
2024-09-24 02:56:03 -0700

    Moved majority of role creation code to a migration function

--- images/sqlx-cli-with-migrations/scripts/create-db-app-user.sh
+++ images/sqlx-cli-with-migrations/scripts/create-db-app-user.sh
@@ -22,4 +22,4 @@ if [ $# -ne 0 ]; then
   exit 1
 fi
 
-psql --set=APP_DB_USERNAME='$APP_DB_USERNAME' --set=APP_DB_PASSWORD='$APP_DB_PASSWORD' $DB_CONNECTION_STRING --file /scripts/create-db-app-user.sql
+psql $DB_CONNECTION_STRING -c "SELECT create_app_user_role_if_not_exists(current_database(), '${APP_DB_USERNAME}', '${APP_DB_PASSWORD}')"
