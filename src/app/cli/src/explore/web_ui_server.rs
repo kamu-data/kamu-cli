73c9b2643f8d734bd6c6a4642c910f1cb95edeb0
Dima Pristupa <dima@pristupa.dev>
2024-09-25 19:53:17 +0300

    CI: add web-ui build test

--- src/app/cli/src/explore/web_ui_server.rs
+++ src/app/cli/src/explore/web_ui_server.rs
@@ -61,7 +61,8 @@ struct WebUILoginInstructions {
 #[serde(rename_all = "camelCase")]
 struct WebUIFeatureFlags {
     enable_logout: bool,
-    enable_scheduling: bool,
+    // TODO: just for test
+    // enable_scheduling: bool,
     // TODO: Correct a typo in `WebUIFeatureFlags`
     //       (content of `assets/runtime-config.json`)
     //       https://github.com/kamu-data/kamu-cli/issues/841
