d602f792a261e4a4b6c7c53c264caa75a4bfc533
Dima Pristupa <dima@pristupa.dev>
2024-10-07 19:55:38 +0300

    E2E, test_delete_dataset(): add

--- src/e2e/app/cli/repo-tests/src/commands/mod.rs
+++ src/e2e/app/cli/repo-tests/src/commands/mod.rs
@@ -10,6 +10,7 @@
 mod test_add_command;
 mod test_complete_command;
 mod test_config_command;
+mod test_delete_command;
 mod test_ingest_command;
 mod test_init_command;
 mod test_repo_alias_command;
@@ -20,6 +21,7 @@ mod test_system_generate_token_command;
 pub use test_add_command::*;
 pub use test_complete_command::*;
 pub use test_config_command::*;
+pub use test_delete_command::*;
 pub use test_ingest_command::*;
 pub use test_init_command::*;
 pub use test_repo_alias_command::*;
