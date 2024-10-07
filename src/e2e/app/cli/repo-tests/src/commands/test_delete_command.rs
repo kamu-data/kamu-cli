614f3169c7340f4767e888deca88b4ac92a6a9f6
Dima Pristupa <dima@pristupa.dev>
2024-10-07 20:16:50 +0300

    E2E, test_delete_dataset_all(): add

--- src/e2e/app/cli/repo-tests/src/commands/test_delete_command.rs
+++ src/e2e/app/cli/repo-tests/src/commands/test_delete_command.rs
@@ -122,3 +122,65 @@ pub async fn test_delete_dataset_recursive(kamu: KamuCliPuppet) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub async fn test_delete_dataset_all(kamu: KamuCliPuppet) {
+    // 1. Root
+    kamu.execute_with_input(["add", "--stdin"], DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR)
+        .await
+        .success();
+
+    // 2. Derivative (from 1.)
+    kamu.execute_with_input(
+        ["add", "--stdin"],
+        DATASET_DERIVATIVE_LEADERBOARD_SNAPSHOT_STR,
+    )
+    .await
+    .success();
+
+    // 3. One more root
+    kamu.execute_with_input(
+        ["add", "--stdin", "--name", "another-root"],
+        DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR,
+    )
+    .await
+    .success();
+
+    {
+        let dataset_names = kamu
+            .list_datasets()
+            .await
+            .into_iter()
+            .map(|dataset| dataset.name)
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            dataset_names,
+            ["another-root", "leaderboard", "player-scores"]
+        );
+    }
+    {
+        let assert = kamu.execute(["--yes", "delete", "--all"]).await.success();
+
+        let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
+
+        assert!(
+            stderr.contains("Deleted 3 dataset(s)"),
+            "Unexpected output:\n{stderr}",
+        );
+    }
+    {
+        let dataset_names = kamu
+            .list_datasets()
+            .await
+            .into_iter()
+            .map(|dataset| dataset.name)
+            .collect::<Vec<_>>();
+
+        assert!(
+            dataset_names.is_empty(),
+            "Unexpected dataset names: {dataset_names:?}"
+        );
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
