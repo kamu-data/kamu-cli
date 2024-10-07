ecad9436d2c4dde75b0ee117f56fa4d98117b03f
Dima Pristupa <dima@pristupa.dev>
2024-10-07 15:33:26 +0300

    E2E, test_config_set_value(): respect potential CI agent podman configuration

--- src/e2e/app/cli/repo-tests/src/commands/test_config_command.rs
+++ src/e2e/app/cli/repo-tests/src/commands/test_config_command.rs
@@ -12,21 +12,8 @@ use kamu_cli_puppet::KamuCliPuppet;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub async fn test_config_set_value(kamu: KamuCliPuppet) {
-    // 1. Set flow for the "engine.runtime" key
-    {
-        let assert = kamu.execute(["config", "list"]).await.success();
-        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
-
-        pretty_assertions::assert_eq!(
-            stdout,
-            indoc::indoc!(
-                r#"
-                {}
-
-                "#
-            )
-        );
-    }
+    // 0. CI sets container runtime to podman for some targets, so we simulate this
+    //    behavior for all others.
     {
         let assert = kamu
             .execute(["config", "set", "engine.runtime", "podman"])
@@ -40,9 +27,39 @@ pub async fn test_config_set_value(kamu: KamuCliPuppet) {
             "Unexpected output:\n{stderr}",
         );
     }
+
+    // 1. Set flow for the "engine.networkNs" key
+    {
+        let assert = kamu.execute(["config", "list"]).await.success();
+        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
+
+        pretty_assertions::assert_eq!(
+            stdout,
+            indoc::indoc!(
+                r#"
+                engine:
+                  runtime: podman
+
+                "#
+            )
+        );
+    }
     {
         let assert = kamu
-            .execute(["config", "get", "engine.runtime"])
+            .execute(["config", "set", "engine.networkNs", "host"])
+            .await
+            .success();
+
+        let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
+
+        assert!(
+            stderr.contains("Set engine.networkNs to host in workspace scope"),
+            "Unexpected output:\n{stderr}",
+        );
+    }
+    {
+        let assert = kamu
+            .execute(["config", "get", "engine.networkNs"])
             .await
             .success();
 
@@ -52,7 +69,7 @@ pub async fn test_config_set_value(kamu: KamuCliPuppet) {
             stdout,
             indoc::indoc!(
                 r#"
-                podman
+                host
 
                 "#
             )
@@ -68,6 +85,7 @@ pub async fn test_config_set_value(kamu: KamuCliPuppet) {
                 r#"
                 engine:
                   runtime: podman
+                  networkNs: host
 
                 "#
             )
@@ -115,6 +133,7 @@ pub async fn test_config_set_value(kamu: KamuCliPuppet) {
                 r#"
                 engine:
                   runtime: podman
+                  networkNs: host
                 uploads:
                   maxFileSizeInMb: 42
 
@@ -129,40 +148,40 @@ pub async fn test_config_set_value(kamu: KamuCliPuppet) {
 pub async fn test_config_reset_key(kamu: KamuCliPuppet) {
     {
         let assert = kamu
-            .execute(["config", "set", "engine.runtime", "podman"])
+            .execute(["config", "set", "engine.networkNs", "host"])
             .await
             .success();
 
         let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
 
         assert!(
-            stderr.contains("Set engine.runtime to podman in workspace scope"),
+            stderr.contains("Set engine.networkNs to host in workspace scope"),
             "Unexpected output:\n{stderr}",
         );
     }
     {
         let assert = kamu
-            .execute(["config", "set", "engine.runtime"])
+            .execute(["config", "set", "engine.networkNs"])
             .await
             .success();
 
         let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
 
         assert!(
-            stderr.contains("Removed engine.runtime from workspace scope"),
+            stderr.contains("Removed engine.networkNs from workspace scope"),
             "Unexpected output:\n{stderr}",
         );
     }
     {
         let assert = kamu
-            .execute(["config", "get", "engine.runtime"])
+            .execute(["config", "get", "engine.networkNs"])
             .await
             .failure();
 
         let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
 
         assert!(
-            stderr.contains("Error: Key engine.runtime not found"),
+            stderr.contains("Error: Key engine.networkNs not found"),
             "Unexpected output:\n{stderr}",
         );
     }
