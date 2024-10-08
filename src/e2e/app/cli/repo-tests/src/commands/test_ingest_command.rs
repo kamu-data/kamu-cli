729312d410baff60bd2a914f4b767798ed19a240
Dima Pristupa <dima@pristupa.dev>
2024-10-08 17:18:58 +0300

    E2E, test_ingest_from_stdin(): also check data via the tail command

--- src/e2e/app/cli/repo-tests/src/commands/test_ingest_command.rs
+++ src/e2e/app/cli/repo-tests/src/commands/test_ingest_command.rs
@@ -186,7 +186,9 @@ pub async fn test_push_ingest_from_file_snapshot_with_event_time(mut kamu: KamuC
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub async fn test_ingest_from_stdin(kamu: KamuCliPuppet) {
+pub async fn test_ingest_from_stdin(mut kamu: KamuCliPuppet) {
+    kamu.set_system_time(Some(Utc.with_ymd_and_hms(2050, 2, 3, 4, 5, 6).unwrap()));
+
     kamu.execute_with_input(["add", "--stdin"], DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR)
         .await
         .success();
@@ -208,6 +210,33 @@ pub async fn test_ingest_from_stdin(kamu: KamuCliPuppet) {
         DATASET_ROOT_PLAYER_SCORES_INGEST_DATA_NDJSON_CHUNK_3,
     )
     .await;
+
+    {
+        let assert = kamu
+            .execute(["tail", "player-scores", "--output-format", "table"])
+            .await
+            .success();
+
+        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
+
+        pretty_assertions::assert_eq!(
+            stdout,
+            indoc::indoc!(
+                r#"
+                ┌────────┬────┬──────────────────────┬──────────────────────┬──────────┬───────────┬───────┐
+                │ offset │ op │     system_time      │      match_time      │ match_id │ player_id │ score │
+                ├────────┼────┼──────────────────────┼──────────────────────┼──────────┼───────────┼───────┤
+                │      0 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │     Alice │   100 │
+                │      1 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │       Bob │    80 │
+                │      2 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-02T00:00:00Z │        2 │   Charlie │    90 │
+                │      3 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-02T00:00:00Z │        2 │     Alice │    70 │
+                │      4 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-03T00:00:00Z │        3 │       Bob │    60 │
+                │      5 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-03T00:00:00Z │        3 │   Charlie │   110 │
+                └────────┴────┴──────────────────────┴──────────────────────┴──────────┴───────────┴───────┘
+                "#
+            )
+        );
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
