81ceb097483cac314734c262eb24c27a3b6f98fc
Dima Pristupa <dima@pristupa.dev>
2024-10-08 15:40:56 +0300

    Images, kamu-base-with-data-mt: init-workspace.py use .kamuconfig

--- images/kamu-base-with-data-mt/init-workspace.py
+++ images/kamu-base-with-data-mt/init-workspace.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import os
-import sys
+import shutil
 import subprocess
+from pathlib import Path
 
 ###############################################################################
 
+CURRENT_PATH = Path(__file__).resolve().parent
 S3_REPO_URL = "s3://datasets.kamu.dev/odf/v2/example-mt/"
+
 
 ###############################################################################
 
@@ -14,35 +16,40 @@ def s3_listdir(url):
     return [
         line.strip().split(' ')[1]
         for line in subprocess.run(
-            f"aws s3 ls {url}", 
-            shell=True, 
+            f"aws s3 ls {url}",
+            shell=True,
             text=True,
             check=True,
             capture_output=True,
         ).stdout.splitlines()
     ]
 
+
 def s3_cat(url):
     return subprocess.run(
-        f"aws s3 cp {url} -", 
-        shell=True, 
+        f"aws s3 cp {url} -",
+        shell=True,
         text=True,
         check=True,
         capture_output=True,
     ).stdout.strip()
 
+
 ###############################################################################
 
 subprocess.run(
-    "kamu init --multi-tenant --exists-ok", 
+    "kamu init --multi-tenant --exists-ok",
     shell=True,
     check=True,
 )
+
+shutil.copy(CURRENT_PATH / "extra/.kamuconfig", ".kamuconfig")
 
 for did in s3_listdir(S3_REPO_URL):
     url = S3_REPO_URL + did
     alias = s3_cat(f"{S3_REPO_URL}{did}info/alias")
     account, name = alias.split('/', 1)
+
     subprocess.run(
         f"kamu --account {account} pull --no-alias {url} --as {name}",
         shell=True,
