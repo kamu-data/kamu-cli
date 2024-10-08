0fed0dc1c2d54fe9963a54c2f12639b4ea48108b
Dima Pristupa <dima@pristupa.dev>
2024-10-08 12:56:06 +0300

    E2E: add (or expand) `config`/`init`/`add`/`rename` command tests (#878)
    * KamuCliApiServerHarnessOptions::with_kamu_config(): introduce
    
    * E2E: cover "kamu config" command
    
    * E2E, test_init_in_an_existing_workspace(): add
    
    * E2E, test_config_set_value(): respect potential CI agent podman configuration
    
    * Updated yanked crate: futures-channel
    
    * E2E, test_config_get_with_default(): respect potential CI agent podman configuration
    
    * kamu-cli-e2e-common: extract "player-scores" & "leaderboard" datasets into statics
    
    * assert_cmd: add patch
    
    * E2E, test_add_dataset_from_stdin(): add
    
    * E2E, test_add_dataset_from_stdin(): add
    
    * AddCommand::validate_args(): fix "name" arg processing
    
    * E2E, test_add_dataset_with_name(): mix add commands
    
    * E2E, test_add_dataset_with_replace(): add
    
    * Interact: fix if is_tty
    
    * CHANGELOG.md: update
    
    * E2E, test_delete_dataset(): add
    
    * E2E, test_delete_dataset_recursive(): add
    
    * E2E, test_delete_dataset_all(): add
    
    * E2E, test_rename_dataset(): add
    
    * assert_cmd: use patch (now from the kamu-data org)
    
    * kamu-cli-puppet: remove unused dep
    
    * Extracted to the separate PR

---
+++ src/e2e/app/cli/repo-tests/src/commands/test_rename_command.rs
@@ -0,0 +1,59 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use kamu_cli_e2e_common::DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR;
+use kamu_cli_puppet::extensions::KamuCliPuppetExt;
+use kamu_cli_puppet::KamuCliPuppet;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub async fn test_rename_dataset(kamu: KamuCliPuppet) {
+    {
+        let assert = kamu
+            .execute(["rename", "player-scores", "top-player-scores"])
+            .await
+            .failure();
+
+        let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
+
+        assert!(
+            stderr.contains("Error: Dataset not found: player-scores"),
+            "Unexpected output:\n{stderr}",
+        );
+    }
+
+    kamu.execute_with_input(["add", "--stdin"], DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR)
+        .await
+        .success();
+
+    {
+        let assert = kamu
+            .execute(["rename", "player-scores", "top-player-scores"])
+            .await
+            .success();
+
+        let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
+
+        assert!(
+            stderr.contains("Dataset renamed"),
+            "Unexpected output:\n{stderr}",
+        );
+    }
+
+    let dataset_names = kamu
+        .list_datasets()
+        .await
+        .into_iter()
+        .map(|dataset| dataset.name)
+        .collect::<Vec<_>>();
+
+    assert_eq!(dataset_names, ["top-player-scores"]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
