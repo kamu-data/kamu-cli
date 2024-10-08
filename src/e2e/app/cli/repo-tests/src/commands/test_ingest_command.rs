570e97ae9c33d1626b4e46c5f1c4c7611925b089
Dima Pristupa <dima@pristupa.dev>
2024-10-08 20:10:50 +0300

    E2E, ingest_data_to_player_scores_from_stdio(): check data for all ingest steps

--- src/e2e/app/cli/repo-tests/src/commands/test_ingest_command.rs
+++ src/e2e/app/cli/repo-tests/src/commands/test_ingest_command.rs
@@ -199,11 +199,59 @@ pub async fn test_ingest_from_stdin(mut kamu: KamuCliPuppet) {
     )
     .await;
 
+    {
+        let assert = kamu
+            .execute(["tail", "player-scores", "--output-format", "table"])
+            .await
+            .success();
+
+        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
+
+        pretty_assertions::assert_eq!(
+            stdout,
+            indoc::indoc!(
+                r#"
+                ┌────────┬────┬──────────────────────┬──────────────────────┬──────────┬───────────┬───────┐
+                │ offset │ op │     system_time      │      match_time      │ match_id │ player_id │ score │
+                ├────────┼────┼──────────────────────┼──────────────────────┼──────────┼───────────┼───────┤
+                │      0 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │     Alice │   100 │
+                │      1 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │       Bob │    80 │
+                └────────┴────┴──────────────────────┴──────────────────────┴──────────┴───────────┴───────┘
+                "#
+            )
+        );
+    }
+
     ingest_data_to_player_scores_from_stdio(
         &kamu,
         DATASET_ROOT_PLAYER_SCORES_INGEST_DATA_NDJSON_CHUNK_2,
     )
     .await;
+
+    {
+        let assert = kamu
+            .execute(["tail", "player-scores", "--output-format", "table"])
+            .await
+            .success();
+
+        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
+
+        pretty_assertions::assert_eq!(
+            stdout,
+            indoc::indoc!(
+                r#"
+                ┌────────┬────┬──────────────────────┬──────────────────────┬──────────┬───────────┬───────┐
+                │ offset │ op │     system_time      │      match_time      │ match_id │ player_id │ score │
+                ├────────┼────┼──────────────────────┼──────────────────────┼──────────┼───────────┼───────┤
+                │      0 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │     Alice │   100 │
+                │      1 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-01T00:00:00Z │        1 │       Bob │    80 │
+                │      2 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-02T00:00:00Z │        2 │   Charlie │    90 │
+                │      3 │ +A │ 2050-02-03T04:05:06Z │ 2000-01-02T00:00:00Z │        2 │     Alice │    70 │
+                └────────┴────┴──────────────────────┴──────────────────────┴──────────┴───────────┴───────┘
+                "#
+            )
+        );
+    }
 
     ingest_data_to_player_scores_from_stdio(
         &kamu,
