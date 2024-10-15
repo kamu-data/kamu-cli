116b0052d0bd45aed3363a74a16fb651cdf08e0c
Dima Pristupa <dima@pristupa.dev>
2024-10-15 22:54:25 +0300

    E2E, test_sql_command: use new methods

--- src/e2e/app/cli/repo-tests/src/commands/test_sql_command.rs
+++ src/e2e/app/cli/repo-tests/src/commands/test_sql_command.rs
@@ -7,11 +7,11 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use indoc::indoc;
 use kamu_cli_e2e_common::{
     DATASET_ROOT_PLAYER_SCORES_INGEST_DATA_NDJSON_CHUNK_1,
     DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR,
 };
+use kamu_cli_puppet::extensions::KamuCliPuppetExt;
 use kamu_cli_puppet::KamuCliPuppet;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,7 +26,7 @@ pub async fn test_datafusion_cli(kamu: KamuCliPuppet) {
 
     assert!(
         stdout.contains(
-            indoc!(
+            indoc::indoc!(
                 r#"
                 +----------+
                 | Int64(1) |
@@ -50,80 +50,62 @@ pub async fn test_datafusion_cli_not_launched_in_root_ws(kamu: KamuCliPuppet) {
     // so there is no problem that the process working directory is one of the
     // subdirectories (kamu-cli/src/e2e/app/cli/inmem)
 
-    {
-        let assert = kamu.execute(["list"]).await.failure();
-
-        let stderr = std::str::from_utf8(&assert.get_output().stderr).unwrap();
-
-        assert!(
-            stderr.contains("Error: Directory is not a kamu workspace"),
-            "Unexpected output:\n{stderr}",
-        );
-    }
+    kamu.assert_failure_command_execution(
+        ["list"],
+        None,
+        Some("Error: Directory is not a kamu workspace"),
+    )
+    .await;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub async fn test_sql_command(kamu: KamuCliPuppet) {
-    {
-        let assert = kamu
-            .execute([
-                "sql",
-                "--command",
-                "SELECT 42 as answer;",
-                "--output-format",
-                "table",
-            ])
-            .await
-            .success();
-
-        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
-
-        pretty_assertions::assert_eq!(
-            indoc::indoc!(
-                r#"
-                ┌────────┐
-                │ answer │
-                ├────────┤
-                │     42 │
-                └────────┘
-                "#
-            ),
-            stdout
-        );
-    }
+    kamu.assert_success_command_execution(
+        [
+            "sql",
+            "--command",
+            "SELECT 42 as answer;",
+            "--output-format",
+            "table",
+        ],
+        Some(indoc::indoc!(
+            r#"
+            ┌────────┐
+            │ answer │
+            ├────────┤
+            │     42 │
+            └────────┘
+            "#
+        )),
+        None,
+    )
+    .await;
 
     kamu.execute_with_input(["add", "--stdin"], DATASET_ROOT_PLAYER_SCORES_SNAPSHOT_STR)
         .await
         .success();
 
-    {
-        let assert = kamu
-            .execute([
-                "sql",
-                "--command",
-                "SELECT * FROM \"player-scores\";",
-                "--output-format",
-                "table",
-            ])
-            .await
-            .success();
-
-        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
-
-        pretty_assertions::assert_eq!(
-            indoc::indoc!(
-                r#"
-                ┌┐
-                ││
-                ├┤
-                ││
-                └┘
-                "#
-            ),
-            stdout
-        );
-    }
+    kamu.assert_success_command_execution(
+        [
+            "sql",
+            "--command",
+            "SELECT * FROM \"player-scores\";",
+            "--output-format",
+            "table",
+        ],
+        Some(indoc::indoc!(
+            r#"
+            ┌┐
+            ││
+            ├┤
+            ││
+            └┘
+            "#
+        )),
+        None,
+    )
+    .await;
 
     kamu.execute_with_input(
         ["ingest", "player-scores", "--stdin"],
@@ -132,34 +114,27 @@ pub async fn test_sql_command(kamu: KamuCliPuppet) {
     .await
     .success();
 
-    {
-        let assert = kamu
-            .execute([
-                "sql",
-                "--command",
-                "SELECT * FROM \"player-scores\" ORDER BY offset;",
-                "--output-format",
-                "table",
-            ])
-            .await
-            .success();
-
-        let stdout = std::str::from_utf8(&assert.get_output().stdout).unwrap();
-
-        pretty_assertions::assert_eq!(
-            indoc::indoc!(
-                r#"
-                ┌────────┬────┬──────────────────────┬──────────────────────┬──────────┬───────────┬───────┐
-                │ offset │ op │     system_time      │      match_time      │ match_id │ player_id │ score │
-                ├────────┼────┼──────────────────────┼──────────────────────┼──────────┼───────────┼───────┤
-                │      0 │  0 │ 2050-01-02T03:04:05Z │ 2000-01-01T00:00:00Z │        1 │     Alice │   100 │
-                │      1 │  0 │ 2050-01-02T03:04:05Z │ 2000-01-01T00:00:00Z │        1 │       Bob │    80 │
-                └────────┴────┴──────────────────────┴──────────────────────┴──────────┴───────────┴───────┘
-                "#
-            ),
-            stdout
-        );
-    }
+    kamu.assert_success_command_execution(
+        [
+            "sql",
+            "--command",
+            "SELECT * FROM \"player-scores\" ORDER BY offset;",
+            "--output-format",
+            "table",
+        ],
+        Some(indoc::indoc!(
+            r#"
+            ┌────────┬────┬──────────────────────┬──────────────────────┬──────────┬───────────┬───────┐
+            │ offset │ op │     system_time      │      match_time      │ match_id │ player_id │ score │
+            ├────────┼────┼──────────────────────┼──────────────────────┼──────────┼───────────┼───────┤
+            │      0 │  0 │ 2050-01-02T03:04:05Z │ 2000-01-01T00:00:00Z │        1 │     Alice │   100 │
+            │      1 │  0 │ 2050-01-02T03:04:05Z │ 2000-01-01T00:00:00Z │        1 │       Bob │    80 │
+            └────────┴────┴──────────────────────┴──────────────────────┴──────────┴───────────┴───────┘
+            "#
+        )),
+        None,
+    )
+    .await;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
