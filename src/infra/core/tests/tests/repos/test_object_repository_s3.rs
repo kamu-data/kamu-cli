eb206db2902cd68f7d51f58189630351b514458e
Dima Pristupa <dima@pristupa.dev>
2024-10-17 18:59:56 +0300

    Tests: ObjectRepository::{get_external_download_url(),get_external_upload_url()}

--- src/infra/core/tests/tests/repos/test_object_repository_s3.rs
+++ src/infra/core/tests/tests/repos/test_object_repository_s3.rs
@@ -11,12 +11,14 @@ use std::assert_matches::assert_matches;
 use std::convert::TryFrom;
 
 use kamu::domain::*;
-use kamu::testing::LocalS3Server;
+use kamu::testing::{LocalS3Server, TEST_BUCKET_NAME};
 use kamu::utils::s3_context::S3Context;
 use kamu::*;
 use opendatafabric::*;
+use url::Url;
 
 use super::test_object_repository_shared;
+use super::test_object_repository_shared::ExternalUrlTestOptions;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -188,6 +190,68 @@ async fn test_insert_expect() {
     let repo = ObjectRepositoryS3Sha3::new(S3Context::from_url(&s3.url).await, None);
 
     test_object_repository_shared::test_insert_expect(&repo).await;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[test_group::group(containerized)]
+#[test_log::test(tokio::test)]
+async fn test_external_urls() {
+    let s3 = LocalS3Server::new().await;
+    let repo = ObjectRepositoryS3Sha3::new(S3Context::from_url(&s3.url).await, None);
+
+    let hash_foo = Multihash::from_digest_sha3_256(b"foo");
+
+    let expected_external_download_url = {
+        let mut url =
+            Url::parse(&format!("http://127.0.0.1/{TEST_BUCKET_NAME}/{hash_foo}")).unwrap();
+        url.set_port(s3.url.port()).unwrap();
+        url
+    };
+    let expected_external_upload_url_result = expected_external_download_url.clone();
+
+    test_object_repository_shared::test_external_urls(
+        &repo,
+        &hash_foo,
+        Ok(expected_external_download_url),
+        Ok(expected_external_upload_url_result),
+        ExternalUrlTestOptions {
+            cut_query_params: true,
+        },
+    )
+    .await;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[test_group::group(containerized)]
+#[test_log::test(tokio::test)]
+async fn test_external_urls_with_override() {
+    let s3 = LocalS3Server::new().await;
+    let external_address_override = Url::parse("https://example.com:8443").unwrap();
+    let repo = ObjectRepositoryS3Sha3::new(
+        S3Context::from_url(&s3.url).await,
+        Some(external_address_override),
+    );
+
+    let hash_foo = Multihash::from_digest_sha3_256(b"foo");
+
+    let expected_external_download_url = Url::parse(&format!(
+        "https://example.com:8443/{TEST_BUCKET_NAME}/{hash_foo}"
+    ))
+    .unwrap();
+    let expected_external_upload_url_result = expected_external_download_url.clone();
+
+    test_object_repository_shared::test_external_urls(
+        &repo,
+        &hash_foo,
+        Ok(expected_external_download_url),
+        Ok(expected_external_upload_url_result),
+        ExternalUrlTestOptions {
+            cut_query_params: true,
+        },
+    )
+    .await;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
