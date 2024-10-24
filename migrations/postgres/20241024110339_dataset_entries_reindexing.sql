e8c19d21eaabd48f0c33bafd73d0032dce871a50
Dima Pristupa <dima@pristupa.dev>
2024-10-24 14:43:52 +0300

    Migrations: re-index dataset_entries table

---
+++ migrations/postgres/20241024110339_dataset_entries_reindexing.sql
@@ -0,0 +1,19 @@
+-- Inserting if there is no row.
+INSERT INTO outbox_message_consumptions (consumer_name, producer_name, last_consumed_message_id)
+SELECT 'dev.kamu.domain.datasets.DatasetEntryIndexer', 'dev.kamu.domain.core.services.DatasetService', 0
+WHERE NOT EXISTS (SELECT *
+                  FROM outbox_message_consumptions
+                  WHERE consumer_name = 'dev.kamu.domain.datasets.DatasetEntryIndexer');
+
+-- Skip previous `DatasetLifecycleMessage`'s because the structure has changed.
+-- This does not cause problems, because `DatasetEntryIndexer` will fill with rows based on current datasets.
+UPDATE outbox_message_consumptions
+SET last_consumed_message_id = (SELECT last_consumed_message_id
+                                FROM outbox_message_consumptions
+                                WHERE producer_name = 'dev.kamu.domain.core.services.DatasetService'
+                                LIMIT 1)
+WHERE consumer_name = 'dev.kamu.domain.datasets.DatasetEntryIndexer';
+
+-- Start re-indexing.
+DELETE
+FROM dataset_entries;
