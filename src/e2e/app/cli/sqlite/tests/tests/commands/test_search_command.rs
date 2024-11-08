5f8be378cd40289c8332c44a1b34828f9e40f501
Dima Pristupa <dima@pristupa.dev>
2024-11-08 18:39:20 +0200

    E2E, test_search_multi_user: add for sqlite & postgres

--- src/e2e/app/cli/sqlite/tests/tests/commands/test_search_command.rs
+++ src/e2e/app/cli/sqlite/tests/tests/commands/test_search_command.rs
@@ -13,6 +13,32 @@ use kamu_cli_e2e_common::prelude::*;
 
 kamu_cli_run_api_server_e2e_test!(
     storage = sqlite,
+    fixture = kamu_cli_e2e_repo_tests::test_search_multi_user
+    // We need synthetic time for the tests, but the third-party JWT code
+    // uses the current time. Assuming that the token lifetime is 24 hours, we will
+    // use the projected date (the current day) as a workaround.
+    options = Options::default()
+        .with_multi_tenant()
+        .with_today_as_frozen_system_time()
+        .with_kamu_config(
+            indoc::indoc!(
+                r#"
+                kind: CLIConfig
+                version: 1
+                content:
+                  users:
+                    predefined:
+                      - accountName: kamu
+                "#
+            )
+        ),
+    extra_test_groups = "engine, ingest, datafusion"
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+kamu_cli_run_api_server_e2e_test!(
+    storage = sqlite,
     fixture = kamu_cli_e2e_repo_tests::test_search_by_name
     // We need synthetic time for the tests, but the third-party JWT code
     // uses the current time. Assuming that the token lifetime is 24 hours, we will
