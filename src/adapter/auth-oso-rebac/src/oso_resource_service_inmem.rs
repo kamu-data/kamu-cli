a282a101afaee22f64dffe74cb4e4d24ca7120a0
Dima Pristupa <dima@pristupa.dev>
2024-12-04 20:00:52 +0200

    OsoResourceServiceInMem: rewrite to use streamed pages

--- src/adapter/auth-oso-rebac/src/oso_resource_service_inmem.rs
+++ src/adapter/auth-oso-rebac/src/oso_resource_service_inmem.rs
@@ -8,30 +8,19 @@
 // by the Apache License, Version 2.0.
 
 use std::collections::HashMap;
+use std::sync::Arc;
 
+use database_common::{EntityListing, EntityStreamer};
 use dill::*;
-use internal_error::InternalError;
-use kamu_core::{
-    DatasetLifecycleMessage,
-    DatasetLifecycleMessageCreated,
-    DatasetLifecycleMessageDeleted,
-    DatasetVisibility,
-    MESSAGE_PRODUCER_KAMU_CORE_DATASET_SERVICE,
-};
-use messaging_outbox::{
-    MessageConsumer,
-    MessageConsumerMeta,
-    MessageConsumerT,
-    MessageDeliveryMechanism,
-};
+use internal_error::{ErrorIntoInternal, InternalError, ResultIntoInternal};
+use kamu_accounts::{AccountRepository, GetAccountByIdError};
+use kamu_auth_rebac::RebacService;
+use kamu_datasets::{DatasetEntriesResolution, DatasetEntryRepository, GetDatasetEntryError};
 use opendatafabric as odf;
-use tokio::sync::{RwLock, RwLockReadGuard};
+use thiserror::Error;
+use tokio::sync::RwLock;
 
-use crate::{
-    DatasetResource,
-    UserActor,
-    MESSAGE_CONSUMER_KAMU_AUTH_OSO_REBAC_OSO_RESOURCE_SERVICE,
-};
+use crate::{DatasetResource, UserActor};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -39,184 +28,218 @@ type EntityId = String;
 
 #[derive(Debug, Default)]
 struct State {
-    user_actor_map: HashMap<EntityId, UserActor>,
-    dataset_resource_map: HashMap<EntityId, DatasetResource>,
+    user_actor_cache_map: HashMap<EntityId, UserActor>,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: Private Datasets: add Service trait
 pub struct OsoResourceServiceInMem {
     state: RwLock<State>,
+    dataset_entry_repo: Arc<dyn DatasetEntryRepository>,
+    rebac_service: Arc<dyn RebacService>,
+    account_repo: Arc<dyn AccountRepository>,
 }
 
 // TODO: Private Datasets: Add ReBAC-specific messages to update properties at
 //       runtime
 #[component(pub)]
 #[scope(Singleton)]
-#[interface(dyn MessageConsumer)]
-#[interface(dyn MessageConsumerT<DatasetLifecycleMessage>)]
-#[meta(MessageConsumerMeta {
-    consumer_name: MESSAGE_CONSUMER_KAMU_AUTH_OSO_REBAC_OSO_RESOURCE_SERVICE,
-    feeding_producers: &[MESSAGE_PRODUCER_KAMU_CORE_DATASET_SERVICE],
-    // This is a workaround, since we need the result of processing immediately, not after some time
-    delivery: MessageDeliveryMechanism::Immediate,
- })]
 impl OsoResourceServiceInMem {
-    pub fn new() -> Self {
+    pub fn new(
+        dataset_entry_repo: Arc<dyn DatasetEntryRepository>,
+        rebac_service: Arc<dyn RebacService>,
+        account_repo: Arc<dyn AccountRepository>,
+    ) -> Self {
         Self {
             state: RwLock::new(State::default()),
+            dataset_entry_repo,
+            rebac_service,
+            account_repo,
         }
     }
 
-    pub async fn initialize(
+    // TODO: Private Datasets: concrete the error (absorb option to NotFound error?)
+    pub async fn user_dataset_pair(
         &self,
-        dataset_resources: impl IntoIterator<Item = (EntityId, DatasetResource)>,
-        user_actors: impl IntoIterator<Item = (EntityId, UserActor)>,
-    ) {
+        maybe_account_id: Option<&odf::AccountID>,
+        dataset_id: &odf::DatasetID,
+    ) -> Result<(Option<UserActor>, Option<DatasetResource>), InternalError> {
+        let user_actor = self.user_actor(maybe_account_id).await?;
+        let dataset_resource = self.dataset_resource(dataset_id).await?;
+
+        Ok((user_actor, dataset_resource))
+    }
+
+    // TODO: Private Datasets: concrete the error (absorb option to NotFound error?)
+    pub async fn user_actor(
+        &self,
+        maybe_account_id: Option<&odf::AccountID>,
+    ) -> Result<Option<UserActor>, InternalError> {
+        let Some(account_id) = maybe_account_id else {
+            return Ok(Some(UserActor::anonymous()));
+        };
+
+        // First, an attempt to get from the cache
+        {
+            let readable_state = self.state.read().await;
+
+            let account_id_stack = account_id.as_did_str().to_stack_string();
+            let maybe_cached_user_actor = readable_state
+                .user_actor_cache_map
+                .get(account_id_stack.as_str())
+                .cloned();
+
+            if maybe_cached_user_actor.is_some() {
+                return Ok(maybe_cached_user_actor);
+            }
+        }
+
+        // The second attempt is from the database
+        let user_actor = {
+            let account = match self.account_repo.get_account_by_id(account_id).await {
+                Ok(found_account) => found_account,
+                Err(get_err) => {
+                    return match get_err {
+                        GetAccountByIdError::NotFound(_) => Ok(None),
+                        e => Err(e.int_err()),
+                    }
+                }
+            };
+
+            let account_properties = self
+                .rebac_service
+                .get_account_properties(&account.id)
+                .await
+                .int_err()?;
+
+            UserActor::logged(&account.id, account_properties.is_admin)
+        };
+
+        // Lastly, caching
         let mut writable_state = self.state.write().await;
 
         writable_state
-            .dataset_resource_map
-            .extend(dataset_resources);
+            .user_actor_cache_map
+            .insert(user_actor.account_id.clone(), user_actor.clone());
 
-        writable_state.user_actor_map.extend(user_actors);
+        Ok(Some(user_actor))
     }
 
-    pub async fn user_dataset_pair(
+    // TODO: Private Datasets: concrete the error (absorb option to NotFound error?)
+    pub async fn dataset_resource(
         &self,
         dataset_id: &odf::DatasetID,
-        maybe_account_id: Option<&odf::AccountID>,
-    ) -> (Option<UserActor>, Option<DatasetResource>) {
-        let readable_state = self.state.read().await;
+    ) -> Result<Option<DatasetResource>, InternalError> {
+        let dataset_entry = match self.dataset_entry_repo.get_dataset_entry(dataset_id).await {
+            Ok(found_dataset_entry) => found_dataset_entry,
+            Err(get_err) => {
+                return match get_err {
+                    GetDatasetEntryError::NotFound(_) => Ok(None),
+                    e => Err(e.int_err()),
+                }
+            }
+        };
+        let dataset_properties = self
+            .rebac_service
+            .get_dataset_properties(&dataset_entry.id)
+            .await
+            .int_err()?;
 
-        let user_actor = self.user_actor_impl(&readable_state, maybe_account_id);
-        let dataset_resource = self.dataset_resource_impl(&readable_state, dataset_id);
-
-        (user_actor, dataset_resource)
-    }
-
-    pub async fn user_actor(&self, maybe_account_id: Option<&odf::AccountID>) -> Option<UserActor> {
-        let readable_state = self.state.read().await;
-
-        self.user_actor_impl(&readable_state, maybe_account_id)
-    }
-
-    pub async fn dataset_resource(&self, dataset_id: &odf::DatasetID) -> Option<DatasetResource> {
-        let readable_state = self.state.read().await;
-
-        self.dataset_resource_impl(&readable_state, dataset_id)
-    }
-
-    fn user_actor_impl(
-        &self,
-        readable_state: &RwLockReadGuard<'_, State>,
-        maybe_account_id: Option<&odf::AccountID>,
-    ) -> Option<UserActor> {
-        if let Some(account_id) = maybe_account_id {
-            let account_entity_id = account_id.as_did_str().to_stack_string();
-
-            readable_state
-                .user_actor_map
-                .get(account_entity_id.as_str())
-                .cloned()
-        } else {
-            Some(UserActor::anonymous())
-        }
-    }
-
-    fn dataset_resource_impl(
-        &self,
-        readable_state: &RwLockReadGuard<'_, State>,
-        dataset_id: &odf::DatasetID,
-    ) -> Option<DatasetResource> {
-        let entity_id = dataset_id.as_did_str().to_stack_string();
-
-        readable_state
-            .dataset_resource_map
-            .get(entity_id.as_str())
-            .cloned()
-    }
-
-    async fn handle_dataset_lifecycle_created_message(
-        &self,
-        message: &DatasetLifecycleMessageCreated,
-    ) -> Result<(), InternalError> {
-        let mut writable_state = self.state.write().await;
-
-        let entity_id = message.dataset_id.to_string();
         let dataset_resource = DatasetResource::new(
-            &message.owner_account_id,
-            message.dataset_visibility == DatasetVisibility::Public,
+            &dataset_entry.owner_id,
+            dataset_properties.allows_public_read,
         );
 
-        let had_duplicate = writable_state
-            .dataset_resource_map
-            .insert(entity_id, dataset_resource)
-            .is_some();
-
-        if had_duplicate {
-            tracing::warn!("There was a duplicate dataset resource");
-        }
-
-        Ok(())
+        Ok(Some(dataset_resource))
     }
 
-    async fn handle_dataset_lifecycle_deleted_message(
+    pub async fn get_multiple_dataset_resources(
         &self,
-        message: &DatasetLifecycleMessageDeleted,
-    ) -> Result<(), InternalError> {
-        let mut writable_state = self.state.write().await;
+        dataset_ids: &[odf::DatasetID],
+    ) -> Result<DatasetResourcesResolution, GetMultipleDatasetResourcesError> {
+        let DatasetEntriesResolution {
+            resolved_entries,
+            unresolved_entries,
+        } = self
+            .dataset_entry_repo
+            .get_multiple_dataset_entries(dataset_ids)
+            .await
+            .int_err()?;
 
-        let entity_id = message.dataset_id.to_string();
+        let dataset_resources_stream = EntityStreamer::default().into_stream(
+            || async { Ok(Arc::new(resolved_entries)) },
+            |dataset_entries, pagination| {
+                let dataset_entries_page = dataset_entries
+                    .iter()
+                    .skip(pagination.offset)
+                    .take(pagination.safe_limit(dataset_entries.len()))
+                    .collect::<Vec<_>>();
 
-        let was_deletion = writable_state
-            .dataset_resource_map
-            .remove(&entity_id)
-            .is_some();
+                let dataset_id_owner_id_mapping =
+                    dataset_entries_page
+                        .iter()
+                        .fold(HashMap::new(), |mut acc, dataset_entry| {
+                            acc.insert(dataset_entry.id.clone(), dataset_entry.owner_id.clone());
+                            acc
+                        });
+                let dataset_ids = dataset_entries_page
+                    .iter()
+                    .map(|dataset_entry| dataset_entry.id.clone())
+                    .collect::<Vec<_>>();
 
-        if was_deletion {
-            tracing::warn!("Dataset resource not found");
-        }
+                async move {
+                    let dataset_properties_map = self
+                        .rebac_service
+                        .get_dataset_properties_by_ids(&dataset_ids)
+                        .await
+                        .int_err()?;
 
-        Ok(())
+                    let mut dataset_resources = Vec::with_capacity(dataset_properties_map.len());
+
+                    for (dataset_id, dataset_properties) in dataset_properties_map {
+                        let owner_id = dataset_id_owner_id_mapping
+                            .get(&dataset_id)
+                            .ok_or_else(|| format!("owner_id not found: {dataset_id}").int_err())?;
+
+                        let dataset_resource =
+                            DatasetResource::new(owner_id, dataset_properties.allows_public_read);
+
+                        dataset_resources.push((dataset_id, dataset_resource));
+                    }
+
+                    Ok(EntityListing {
+                        list: dataset_resources,
+                        total_count: dataset_entries.len(),
+                    })
+                }
+            },
+        );
+
+        use futures::TryStreamExt;
+
+        Ok(DatasetResourcesResolution {
+            resolved_resources: dataset_resources_stream.try_collect().await?,
+            unresolved_resources: unresolved_entries,
+        })
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-impl MessageConsumer for OsoResourceServiceInMem {}
+
+pub struct DatasetResourcesResolution {
+    pub resolved_resources: Vec<(odf::DatasetID, DatasetResource)>,
+    pub unresolved_resources: Vec<odf::DatasetID>,
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Errors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[async_trait::async_trait]
-impl MessageConsumerT<DatasetLifecycleMessage> for OsoResourceServiceInMem {
-    #[tracing::instrument(
-        level = "debug",
-        skip_all,
-        name = "OsoResourceServiceInMem[DatasetLifecycleMessage]"
-    )]
-    async fn consume_message(
-        &self,
-        _: &Catalog,
-        message: &DatasetLifecycleMessage,
-    ) -> Result<(), InternalError> {
-        tracing::debug!(received_message = ?message, "Received dataset lifecycle message");
-
-        match message {
-            DatasetLifecycleMessage::Created(message) => {
-                self.handle_dataset_lifecycle_created_message(message).await
-            }
-
-            DatasetLifecycleMessage::Deleted(message) => {
-                self.handle_dataset_lifecycle_deleted_message(message).await
-            }
-
-            DatasetLifecycleMessage::DependenciesUpdated(_)
-            | DatasetLifecycleMessage::Renamed(_) => {
-                // No action required
-                Ok(())
-            }
-        }
-    }
+#[derive(Error, Debug)]
+pub enum GetMultipleDatasetResourcesError {
+    #[error(transparent)]
+    Internal(#[from] InternalError),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
