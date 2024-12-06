27fb97a67cec66a5824a8d406d06c2ac22f86f6e
Dima Pristupa <dima@pristupa.dev>
2024-12-06 21:12:19 +0200

    sqlite_generate_placeholders_list: extract & use

--- src/utils/database-common/src/lib.rs
+++ src/utils/database-common/src/lib.rs
@@ -10,6 +10,7 @@
 #![feature(lint_reasons)]
 #![expect(incomplete_features)]
 #![feature(inherent_associated_types)]
+#![feature(iter_intersperse)]
 
 mod db_connection_settings;
 mod db_credentials;
@@ -17,6 +18,7 @@ mod db_error;
 mod db_provider;
 mod entities;
 
+mod helpers;
 mod password;
 mod plugins;
 mod transactions;
@@ -26,6 +28,7 @@ pub use db_credentials::*;
 pub use db_error::*;
 pub use db_provider::*;
 pub use entities::*;
+pub use helpers::*;
 pub use password::*;
 pub use plugins::*;
 pub use transactions::*;
