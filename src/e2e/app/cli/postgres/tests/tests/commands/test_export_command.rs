8befca7c59ef9be46fab0ecee2b5b7952ac553d6
Andrii Demus <andrii@demus.dev>
2024-12-19 00:37:08 +0100

    More tests

--- src/e2e/app/cli/postgres/tests/tests/commands/test_export_command.rs
+++ src/e2e/app/cli/postgres/tests/tests/commands/test_export_command.rs
@@ -13,55 +13,66 @@ use kamu_cli_e2e_common::prelude::*;
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_file_partitioning_ignored_for_single_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_file_partitioning_ignored_for_single_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_files
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_csv_files,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_file_partitioning_ignored_for_single_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_file_partitioning_ignored_for_single_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_files
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_json_files,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_file_partitioning_ignored_for_single_file
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_file_partitioning_ignored_for_single_file,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_files
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_parquet_files,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_unsupported_format
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_to_unsupported_format,
+    extra_test_groups = "engine, datafusion"
 );
 
 kamu_cli_execute_command_e2e_test!(
     storage = postgres,
-    fixture = kamu_cli_e2e_repo_tests::commands::test_export_non_existent_dataset
+    fixture = kamu_cli_e2e_repo_tests::commands::test_export_non_existent_dataset,
+    extra_test_groups = "engine, datafusion"
 );
