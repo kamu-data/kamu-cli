314564bae12d6673101734fd21187f332453e6b8
rmn-boiko <roman.bv20@gmail.com>
2024-12-20 10:57:37 +0100

    Extend clean up migration

--- migrations/postgres/20241204155207_cleanup_flow_configurations.sql
+++ migrations/postgres/20241204155207_cleanup_flow_configurations.sql
@@ -1,3 +1,6 @@
 delete from outbox_message_consumptions where producer_name='dev.kamu.domain.flow-system.FlowConfigurationService';
 
 TRUNCATE TABLE flow_configuration_events RESTART IDENTITY;
+TRUNCATE TABLE flow_events CASCADE;
+ALTER SEQUENCE flow_event_id_seq RESTART WITH 1;
+ALTER SEQUENCE flow_id_seq RESTART WITH 1;
