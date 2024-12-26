34a9579a256cddee5d714f1fe59c318447b6bf86
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2024-12-26 12:27:41 -0800

    Update python examples

--- examples/flight-sql/python/client_flightsql_dbapi2.py
+++ examples/flight-sql/python/client_flightsql_dbapi2.py
@@ -1,35 +1,45 @@
 from flightsql import connect, FlightSQLClient
 
+# Secure remote connection
+client = FlightSQLClient(
+    host="node.demo.kamu.dev",
+    port=50050,
+    # Anonymous users have to authenticate using basic auth so they could be assigned a session token
+    user="anonymous",
+    password="anonymous",
+    # Registered users can provide a bearer token
+    # token="<kamu-token>",
+)
+
 # No-TLS local connection
 #
 # To test with local server use:
 #   cd examples/reth-vs-snp500
-#   kamu -vv sql server --flight-sql --port 50050 --address 0.0.0.0
+#   kamu -vv sql server --port 50050 --address 0.0.0.0
 #
-client = FlightSQLClient(
-    host='localhost',
-    port=50050,
-    user='kamu',
-    password='kamu',
-    insecure=True,
-)
-
-# Secure remote connection
 # client = FlightSQLClient(
-#     host='node.demo.kamu.dev',
+#     host="localhost",
 #     port=50050,
-#     user='kamu',
-#     password='kamu',
+#     insecure=True,
+#     # Anonymous users have to authenticate using basic auth so they could be assigned a session token
+#     user="anonymous",
+#     password="anonymous",
+#     # Registered users can provide a bearer token
+#     # token="<kamu-token>",
 # )
 
 con = connect(client)
 cursor = con.cursor()
 
+cursor.execute("select 1 as value")
+print("columns:", cursor.description)
+print("rows:", [r for r in cursor])
+
 cursor.execute("show tables")
 print("columns:", cursor.description)
 print("rows:", [r for r in cursor])
 
-cursor.execute("select * from 'co.alphavantage.tickers.daily.spy' limit 10")
+cursor.execute("select * from 'kamu/co.alphavantage.tickers.daily.spy' limit 10")
 print("columns:", cursor.description)
 print("rows:", [r for r in cursor])
 
@@ -40,5 +50,5 @@ import pandas
 df = pandas.read_sql("show tables", con)
 print(df)
 
-df = pandas.read_sql("select * from 'co.alphavantage.tickers.daily.spy' limit 10", con)
+df = pandas.read_sql("select * from 'kamu/co.alphavantage.tickers.daily.spy' limit 10", con)
 print(df)
--- examples/flight-sql/python/client_flightsql_sqlalchemy.py
+++ examples/flight-sql/python/client_flightsql_sqlalchemy.py
@@ -2,24 +2,33 @@ import flightsql.sqlalchemy
 import sqlalchemy
 import pandas as pd
 
+# Secure remote connection
+engine = sqlalchemy.create_engine(
+    # Anonymous users have to authenticate using basic auth so they could be assigned a session token
+    "datafusion+flightsql://anonymous:anonymous@node.demo.kamu.dev:50050"
+    # Registered users can provide a bearer token directy
+    # "datafusion+flightsql://node.demo.kamu.dev:50050?token=kamu-token"
+)
+
 # No-TLS local connection
 #
 # To test with local server use:
 #   cd examples/reth-vs-snp500
-#   kamu -vv sql server --flight-sql --port 50050 --address 0.0.0.0
+#   kamu -vv sql server --port 50050 --address 0.0.0.0
 #
-engine = sqlalchemy.create_engine(
-    "datafusion+flightsql://kamu:kamu@localhost:50050?insecure=True"
-)
-
-# Secure remote connection
 # engine = sqlalchemy.create_engine(
-#     "datafusion+flightsql://kamu:kamu@node.demo.kamu.dev:50050"
+#     # Anonymous users have to authenticate using basic auth so they could be assigned a session token
+#     "datafusion+flightsql://anonymous:anonymous@localhost:50050?insecure=True"
+#     # Registered users can provide a bearer token directy
+#     # "datafusion+flightsql://localhost:50050?insecure=True&token=kamu-token"
 # )
 
 with engine.connect() as con:
+    df = pd.read_sql(sql="select 1 as value", con=con.connection)
+    print(df)
+
     df = pd.read_sql(sql="show tables", con=con.connection)
     print(df)
 
-    df = pd.read_sql(sql="select * from 'co.alphavantage.tickers.daily.spy' limit 10", con=con.connection)
+    df = pd.read_sql(sql="select * from 'kamu/co.alphavantage.tickers.daily.spy' limit 10", con=con.connection)
     print(df)
