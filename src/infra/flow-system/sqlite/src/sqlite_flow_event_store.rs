66ee4294d5ccd17ba913a3b1023d24ffc4f16c42
Andrii Demus <andrii@demus.dev>
2025-01-08 13:13:34 +0100

    Simplify sqlite queries

--- src/infra/flow-system/sqlite/src/sqlite_flow_event_store.rs
+++ src/infra/flow-system/sqlite/src/sqlite_flow_event_store.rs
@@ -646,24 +646,15 @@ impl FlowEventStore for SqliteFlowEventStore {
 
             let query_str = format!(
                 r#"
-                WITH unsorted_flows AS
-                    (SELECT
-                        flow_id,
-                        flow_status,
-                        last_event_id,
-                        (CASE
-                            WHEN flow_status = 'waiting' THEN 1
-                            WHEN flow_status = 'running' THEN 2
-                            ELSE 3
-                        END) AS ord_status
-                    FROM flows
-                    WHERE
-                        dataset_id = $1
-                        AND (cast($2 as dataset_flow_type) IS NULL OR dataset_flow_type = $2)
-                        AND (cast($3 as flow_status_type) IS NULL OR flow_status = $3)
-                        AND ($4 = 0 OR initiator in ({})))
-                SELECT flow_id FROM unsorted_flows
-                ORDER BY ord_status, last_event_id DESC
+                SELECT
+                    flow_id
+                FROM flows
+                WHERE
+                    dataset_id = $1
+                    AND (cast($2 as dataset_flow_type) IS NULL OR dataset_flow_type = $2)
+                    AND (cast($3 as flow_status_type) IS NULL OR flow_status = $3)
+                    AND ($4 = 0 OR initiator in ({}))
+                ORDER BY flow_status DESC, last_event_id DESC
                 LIMIT $5 OFFSET $6
                 "#,
                 maybe_initiators
@@ -769,24 +760,15 @@ impl FlowEventStore for SqliteFlowEventStore {
 
             let query_str = format!(
                 r#"
-                WITH unsorted_flows AS
-                (SELECT
-                    flow_id,
-                    flow_status,
-                    last_event_id,
-                    (CASE
-                        WHEN flow_status = 'waiting' THEN 1
-                        WHEN flow_status = 'running' THEN 2
-                        ELSE 3
-                    END) AS ord_status
+                SELECT
+                    flow_id
                 FROM flows
                 WHERE
                     dataset_id in ({})
                     AND (cast($1 as dataset_flow_type) IS NULL OR dataset_flow_type = $1)
                     AND (cast($2 as flow_status_type) IS NULL OR flow_status = $2)
-                    AND ($3 = 0 OR initiator in ({})))
-                SELECT flow_id FROM unsorted_flows
-                ORDER BY ord_status, last_event_id DESC
+                    AND ($3 = 0 OR initiator in ({}))
+                ORDER BY flow_status DESC, last_event_id DESC
                 LIMIT $4 OFFSET $5
                 "#,
                 sqlite_generate_placeholders_list(dataset_ids.len(), 6),
