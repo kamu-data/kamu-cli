747d60bb2f3b67177407ef3d554635c44b621f79
Dima Pristupa <dima@pristupa.dev>
2025-01-14 19:36:27 +0200

    RebacIndexer: respect predefined treat_datasets_as_public

--- src/domain/auth-rebac/services/src/rebac_indexer.rs
+++ src/domain/auth-rebac/services/src/rebac_indexer.rs
@@ -7,17 +7,28 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use dill::{component, interface, meta};
 use init_on_startup::{InitOnStartup, InitOnStartupMeta};
 use internal_error::{InternalError, ResultIntoInternal};
-use kamu_accounts::{AccountService, JOB_KAMU_ACCOUNTS_PREDEFINED_ACCOUNTS_REGISTRATOR};
+use kamu_accounts::{
+    AccountService,
+    PredefinedAccountsConfig,
+    JOB_KAMU_ACCOUNTS_PREDEFINED_ACCOUNTS_REGISTRATOR,
+};
 use kamu_auth_rebac::{AccountPropertyName, DatasetPropertyName, RebacRepository, RebacService};
+use kamu_core::DatasetVisibility;
 use kamu_datasets::DatasetEntryService;
 use kamu_datasets_services::JOB_KAMU_DATASETS_DATASET_ENTRY_INDEXER;
+use opendatafabric as odf;
 
 use crate::JOB_KAMU_REBAC_INDEXER;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type PredefinedAccountIdDatasetVisibilityMapping = HashMap<odf::AccountID, DatasetVisibility>;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -26,6 +37,7 @@ pub struct RebacIndexer {
     rebac_service: Arc<dyn RebacService>,
     dataset_entry_service: Arc<dyn DatasetEntryService>,
     account_service: Arc<dyn AccountService>,
+    predefined_accounts_config: Arc<PredefinedAccountsConfig>,
 }
 
 #[component(pub)]
@@ -44,12 +56,14 @@ impl RebacIndexer {
         rebac_service: Arc<dyn RebacService>,
         dataset_entry_service: Arc<dyn DatasetEntryService>,
         account_service: Arc<dyn AccountService>,
+        predefined_accounts_config: Arc<PredefinedAccountsConfig>,
     ) -> Self {
         Self {
             rebac_repo,
             rebac_service,
             dataset_entry_service,
             account_service,
+            predefined_accounts_config,
         }
     }
 
@@ -60,22 +74,32 @@ impl RebacIndexer {
     }
 
     async fn index_entities(&self) -> Result<(), InternalError> {
-        self.index_dataset_entries().await?;
-        self.index_accounts().await?;
+        let visibility_map = self.index_accounts().await?;
+        self.index_dataset_entries(visibility_map).await?;
 
         Ok(())
     }
 
     #[tracing::instrument(level = "debug", skip_all)]
-    async fn index_dataset_entries(&self) -> Result<(), InternalError> {
+    async fn index_dataset_entries(
+        &self,
+        visibility_map: PredefinedAccountIdDatasetVisibilityMapping,
+    ) -> Result<(), InternalError> {
         use futures::TryStreamExt;
 
         let mut dataset_entry_stream = self.dataset_entry_service.all_entries();
 
         while let Some(dataset_entry) = dataset_entry_stream.try_next().await? {
+            let is_public_dataset =
+                if let Some(visibility) = visibility_map.get(&dataset_entry.owner_id) {
+                    visibility.is_public()
+                } else {
+                    false
+                };
+
             for (name, value) in [
-                DatasetPropertyName::allows_public_read(false),
-                DatasetPropertyName::allows_anonymous_read(false),
+                DatasetPropertyName::allows_public_read(is_public_dataset),
+                DatasetPropertyName::allows_anonymous_read(is_public_dataset),
             ] {
                 self.rebac_service
                     .set_dataset_property(&dataset_entry.id, name, &value)
@@ -88,10 +112,24 @@ impl RebacIndexer {
     }
 
     #[tracing::instrument(level = "debug", skip_all)]
-    async fn index_accounts(&self) -> Result<(), InternalError> {
+    async fn index_accounts(
+        &self,
+    ) -> Result<PredefinedAccountIdDatasetVisibilityMapping, InternalError> {
         use futures::TryStreamExt;
 
         let mut accounts_stream = self.account_service.all_accounts();
+
+        let predefined_accounts_map = self.predefined_accounts_config.predefined.iter().fold(
+            HashMap::new(),
+            |mut acc, account| {
+                acc.insert(
+                    account.account_name.clone(),
+                    account.treat_datasets_as_public,
+                );
+                acc
+            },
+        );
+        let mut visibility_map = HashMap::new();
 
         while let Some(account) = accounts_stream.try_next().await? {
             for (name, value) in [AccountPropertyName::is_admin(account.is_admin)] {
@@ -100,9 +138,21 @@ impl RebacIndexer {
                     .await
                     .int_err()?;
             }
+
+            if let Some(treat_datasets_as_public) =
+                predefined_accounts_map.get(&account.account_name)
+            {
+                let visibility = if *treat_datasets_as_public {
+                    DatasetVisibility::Public
+                } else {
+                    DatasetVisibility::Private
+                };
+
+                visibility_map.insert(account.id, visibility);
+            }
         }
 
-        Ok(())
+        Ok(visibility_map)
     }
 }
 
