5a1a92f09c478984d7b0116d11486c7b74171d78
Dima Pristupa <dima@pristupa.dev>
2025-02-19 15:18:00 +0200

    odf, storage-s3: tracing instrument

--- src/odf/storage-s3/src/repos/object_repository_s3.rs
+++ src/odf/storage-s3/src/repos/object_repository_s3.rs
@@ -74,6 +74,7 @@ where
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#[common_macros::method_names_consts]
 #[async_trait]
 impl<D, const C: u32> ObjectRepository for ObjectRepositoryS3<D, C>
 where
@@ -84,7 +85,7 @@ where
         ObjectRepositoryProtocol::S3
     }
 
-    #[tracing::instrument(level = "debug", skip_all, fields(%hash))]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_contains, skip_all, fields(%hash))]
     async fn contains(&self, hash: &Multihash) -> Result<bool, ContainsError> {
         let key = self.get_key(hash);
 
@@ -100,7 +101,7 @@ where
         }
     }
 
-    #[tracing::instrument(level = "debug", skip_all, fields(%hash))]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_get_size, skip_all, fields(%hash))]
     async fn get_size(&self, hash: &Multihash) -> Result<u64, GetError> {
         let key = self.get_key(hash);
 
@@ -123,7 +124,7 @@ where
         }
     }
 
-    #[tracing::instrument(level = "debug", skip_all, fields(%hash))]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_get_bytes, skip_all, fields(%hash))]
     async fn get_bytes(&self, hash: &Multihash) -> Result<Bytes, GetError> {
         use tokio::io::AsyncReadExt;
         let mut stream = self.get_stream(hash).await?;
@@ -134,7 +135,7 @@ where
         Ok(Bytes::from(data))
     }
 
-    #[tracing::instrument(level = "debug", skip_all, fields(%hash))]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_get_stream, skip_all, fields(%hash))]
     async fn get_stream(&self, hash: &Multihash) -> Result<Box<AsyncReadObj>, GetError> {
         let key = self.get_key(hash);
 
@@ -235,7 +236,7 @@ where
         })
     }
 
-    #[tracing::instrument(level = "debug", skip_all)]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_insert_bytes, skip_all)]
     async fn insert_bytes<'a>(
         &'a self,
         data: &'a [u8],
@@ -269,7 +270,7 @@ where
         Ok(InsertResult { hash })
     }
 
-    #[tracing::instrument(level = "debug", skip_all)]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_insert_stream, skip_all)]
     async fn insert_stream<'a>(
         &'a self,
         src: Box<AsyncReadObj>,
@@ -301,7 +302,7 @@ where
         Ok(InsertResult { hash })
     }
 
-    #[tracing::instrument(level = "debug", skip_all)]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_insert_file_move, skip_all)]
     async fn insert_file_move<'a>(
         &'a self,
         src: &Path,
@@ -313,7 +314,7 @@ where
         Ok(insert_result)
     }
 
-    #[tracing::instrument(level = "debug", skip_all, fields(%hash))]
+    #[tracing::instrument(level = "debug", name = ObjectRepositoryS3_delete, skip_all, fields(%hash))]
     async fn delete(&self, hash: &Multihash) -> Result<(), DeleteError> {
         let key = self.get_key(hash);
 
@@ -328,3 +329,5 @@ where
         Ok(())
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
