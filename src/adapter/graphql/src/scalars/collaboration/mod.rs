ed32afd845377bc45cbbc87fbb24a544d879a8a4
Dima Pristupa <dima@pristupa.dev>
2025-03-04 13:41:27 +0200

    GQL: Dataset::collaboration() + DatasetState

---
+++ src/adapter/graphql/src/scalars/collaboration/mod.rs
@@ -0,0 +1,14 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+mod account;
+mod dataset_access_role;
+
+pub(crate) use account::*;
+pub(crate) use dataset_access_role::*;
