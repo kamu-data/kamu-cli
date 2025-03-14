1490c97452b19d5e128a3cdd626b21b8d2d6066e
Dima Pristupa <dima@pristupa.dev>
2025-03-14 14:04:27 +0200

    Tests: update use cases

--- src/infra/core/src/testing/mock_dataset_action_authorizer.rs
+++ src/infra/core/src/testing/mock_dataset_action_authorizer.rs
@@ -110,6 +110,36 @@ impl MockDatasetActionAuthorizer {
         )
     }
 
+    pub fn expect_check_maintain_dataset(
+        self,
+        expected_dataset_id: &odf::DatasetID,
+        times: usize,
+        success: bool,
+    ) -> Self {
+        let expected_dataset_id = expected_dataset_id.clone();
+        self.expect_check_action_allowed_internal(
+            function(move |dataset_id| *dataset_id == expected_dataset_id),
+            DatasetAction::Maintain,
+            times,
+            success,
+        )
+    }
+
+    pub fn expect_check_own_dataset(
+        self,
+        expected_dataset_id: &odf::DatasetID,
+        times: usize,
+        success: bool,
+    ) -> Self {
+        let expected_dataset_id = expected_dataset_id.clone();
+        self.expect_check_action_allowed_internal(
+            function(move |dataset_id| *dataset_id == expected_dataset_id),
+            DatasetAction::Own,
+            times,
+            success,
+        )
+    }
+
     pub fn expect_check_read_a_dataset(self, times: usize, success: bool) -> Self {
         self.expect_check_action_allowed_internal(always(), DatasetAction::Read, times, success)
     }
