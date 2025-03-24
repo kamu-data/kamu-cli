7c25ab52346ffceb395b01284abea75c945545a3
Dima Pristupa <dima@pristupa.dev>
2025-03-24 14:30:25 +0200

    RebacDatasetRegistryFacade: introduce

---
+++ src/domain/auth-rebac/domain/src/services/rebac_dataset_registry_facade.rs
@@ -0,0 +1,96 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::collections::HashMap;
+
+use internal_error::InternalError;
+use kamu_core::{auth, ResolvedDataset};
+use thiserror::Error;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[async_trait::async_trait]
+pub trait RebacDatasetRegistryFacade: Send + Sync {
+    async fn resolve_dataset_handle_for_action_by_ref(
+        &self,
+        dataset_ref: &odf::DatasetRef,
+        action: auth::DatasetAction,
+    ) -> Result<odf::DatasetHandle, RebacDatasetRefUnresolvedError>;
+
+    async fn resolve_dataset_for_action_by_ref(
+        &self,
+        dataset_ref: &odf::DatasetRef,
+        action: auth::DatasetAction,
+    ) -> Result<ResolvedDataset, RebacDatasetRefUnresolvedError>;
+
+    async fn access_multi_dataset_refs(
+        &self,
+        dataset_refs: Vec<odf::DatasetRef>,
+        action: auth::DatasetAction,
+    ) -> Result<AccessMultiDatasetRefsResponse, InternalError>;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct AccessMultiDatasetRefsResponse {
+    pub accessible_resolved_refs: Vec<(odf::DatasetRef, odf::DatasetHandle)>,
+    pub inaccessible_refs: Vec<(odf::DatasetRef, RebacDatasetRefUnresolvedError)>,
+}
+
+impl AccessMultiDatasetRefsResponse {
+    pub fn into_inaccessible_input_datasets_message(
+        self,
+        dataset_ref_alias_map: &HashMap<&odf::DatasetRef, &String>,
+    ) -> String {
+        use itertools::Itertools;
+
+        let inaccessible_dataset_refs_it = self
+            .inaccessible_refs
+            .into_iter()
+            .map(|(dataset_ref, _)| dataset_ref);
+        let joined_inaccessible_datasets = inaccessible_dataset_refs_it
+            .map(|dataset_ref| {
+                if let Some(alias) = dataset_ref_alias_map.get(&dataset_ref) {
+                    format!("'{alias}'")
+                } else {
+                    format!("'{dataset_ref}'")
+                }
+            })
+            .join(", ");
+
+        format!("Some input dataset(s) are inaccessible: {joined_inaccessible_datasets}")
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Errors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Error, Debug)]
+pub enum RebacDatasetRefUnresolvedError {
+    #[error(transparent)]
+    NotFound(#[from] odf::DatasetNotFoundError),
+
+    #[error(transparent)]
+    Access(
+        #[from]
+        #[backtrace]
+        odf::AccessError,
+    ),
+
+    #[error(transparent)]
+    Internal(
+        #[from]
+        #[backtrace]
+        InternalError,
+    ),
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
