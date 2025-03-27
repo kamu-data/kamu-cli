5a9fd5e80bd1a9668cdfaf40f7b441d732dbebbb
Dima Pristupa <dima@pristupa.dev>
2025-03-27 16:44:37 +0200

    E2E: extract to utils

--- src/e2e/app/cli/repo-tests/src/test_private_datasets.rs
+++ src/e2e/app/cli/repo-tests/src/test_private_datasets.rs
@@ -29,6 +29,8 @@ use kamu_cli_puppet::extensions::{AddDatasetOptions, KamuCliPuppetExt};
 use kamu_cli_puppet::KamuCliPuppet;
 use odf::metadata::testing::MetadataFactory;
 
+use crate::utils::make_logged_clients;
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub const PRIVATE_DATESET_WORKSPACE_KAMU_CONFIG: &str = indoc::indoc!(
@@ -1633,37 +1635,6 @@ pub async fn test_granted_role_is_correctly_revoked(anonymous: KamuApiServerClie
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Helpers
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-async fn make_logged_client(
-    anonymous: &KamuApiServerClient,
-    account_name: &str,
-) -> KamuApiServerClient {
-    let mut new_client = anonymous.clone();
-    let password_same_as_account_name = account_name;
-
-    new_client
-        .auth()
-        .login_with_password(account_name, password_same_as_account_name)
-        .await;
-
-    new_client
-}
-
-async fn make_logged_clients<const N: usize>(
-    anonymous: &KamuApiServerClient,
-    account_names: [&str; N],
-) -> [KamuApiServerClient; N] {
-    use std::mem::MaybeUninit;
-
-    let mut logged_clients: [MaybeUninit<KamuApiServerClient>; N] = MaybeUninit::uninit_array();
-
-    for (i, account_name) in account_names.iter().enumerate() {
-        let logged_client = make_logged_client(anonymous, account_name).await;
-        logged_clients[i].write(logged_client);
-    }
-
-    unsafe { logged_clients.map(|item| item.assume_init()) }
-}
 
 async fn authorize_users_by_roles(
     owner: &KamuApiServerClient,
