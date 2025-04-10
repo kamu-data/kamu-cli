33420792b68af5e62ebb093a9708039c1a9e14b0
Dima Pristupa <dima@pristupa.dev>
2025-04-10 18:23:09 +0300

    platform_token_device_handler(): scaffolding

--- src/adapter/http/src/http_server_dataset_router.rs
+++ src/adapter/http/src/http_server_dataset_router.rs
@@ -7,7 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use axum::{Extension, Json};
+use axum::{Extension, Form, Json};
 use database_common_macros::transactional_handler;
 use dill::Catalog;
 use http_common::{ApiError, ApiErrorResponse, IntoApiError, ResultIntoApiError};
@@ -150,10 +150,142 @@ pub async fn platform_login_handler(
         ("api_key" = []),
     )
 )]
-#[allow(clippy::unused_async)]
+#[expect(clippy::unused_async)]
 pub async fn platform_token_validate_handler(catalog: Extension<Catalog>) -> Result<(), ApiError> {
     ensure_authenticated_account(&catalog).api_err()?;
     Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// <https://www.oauth.com/oauth2-servers/device-flow/authorization-request/>
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub struct StartDeviceFlowRequest {
+    /// Reserved: not used
+    pub client_id: String,
+}
+
+/// <https://www.oauth.com/oauth2-servers/device-flow/token-request/>
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub struct PollingDeviceTokenRequest {
+    /// Reserved: not used
+    pub grant_type: String,
+    /// Reserved: not used
+    pub client_id: String,
+    pub device_code: String,
+}
+
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub enum TokenDeviceRequest {
+    StartDeviceFlow(StartDeviceFlowRequest),
+    PollingTokenDevice(PollingDeviceTokenRequest),
+}
+
+/// <https://www.oauth.com/oauth2-servers/device-flow/authorization-request/>
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub struct StartDeviceFlowResponse {
+    pub device_code: String,
+    /// Reserved: not used
+    pub user_code: String,
+    /// Reserved: not used
+    pub verification_uri: String,
+    #[schema(minimum = 1, example = 5)]
+    pub interval: u64,
+    #[schema(minimum = 1, example = 300)]
+    pub expires_in: u64,
+}
+
+/// <https://www.oauth.com/oauth2-servers/device-flow/token-request/>
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub struct FinishDeviceFlowResponse {
+    pub access_token: String,
+    /// Reserved: not used
+    pub refresh_token: String,
+    pub token_type: String,
+    /// Reserved: not used
+    #[schema(minimum = 1, example = 3600)]
+    pub expires_in: u64,
+    #[schema(minimum = 1, example = 5)]
+    /// Reserved: not used
+    pub score: String,
+}
+
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub enum TokenDeviceResponse {
+    StartDeviceFlow(StartDeviceFlowResponse),
+    FinishDeviceFlow(FinishDeviceFlowResponse),
+}
+
+#[derive(Serialize, Deserialize, utoipa::ToSchema, strum::Display)]
+#[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
+pub enum TokenDeviceErrorStatus {
+    /// The device is polling too frequently
+    SlowDown,
+    /// The user has not either allowed or denied the request yet
+    AuthorizationPending,
+    /// The user denies the request
+    AccessDenied,
+    /// The device code has expired
+    ExpiredToken,
+}
+
+#[derive(Serialize, Deserialize, utoipa::ToSchema)]
+pub struct TokenDeviceError {
+    pub message: TokenDeviceErrorStatus,
+}
+
+impl IntoApiError for TokenDeviceError {
+    fn api_err(self) -> ApiError {
+        ApiError::bad_request_with_message(self.message.to_string().as_str())
+    }
+}
+
+/// Generating and receiving a token according to [OAuth 2.0 Device Authorization Grant](https://oauth.net/2/device-flow/)
+#[utoipa::path(
+    post,
+    path = "/platform/token/device",
+    request_body(
+        content = TokenDeviceRequest,
+        content_type = "application/x-www-form-urlencoded"
+    ),
+    responses(
+        (status = OK, body = TokenDeviceResponse),
+        (status = BAD_REQUEST, body = TokenDeviceError)
+    ),
+    tag = "kamu",
+    security(
+        ("api_key" = []),
+    )
+)]
+#[expect(clippy::unused_async)]
+pub async fn platform_token_device_handler(
+    _catalog: Extension<Catalog>,
+    Form(request): Form<TokenDeviceRequest>,
+) -> Result<Json<TokenDeviceResponse>, ApiError> {
+    match request {
+        TokenDeviceRequest::StartDeviceFlow(_) => {
+            Ok(Json(TokenDeviceResponse::StartDeviceFlow(
+                StartDeviceFlowResponse {
+                    // TODO: Device Flow: generate device_code
+                    device_code: String::new(),
+                    // Reserved
+                    user_code: String::new(),
+                    // Reserved
+                    verification_uri: String::new(),
+                    interval: 5,
+                    expires_in: 300,
+                },
+            )))
+        }
+        TokenDeviceRequest::PollingTokenDevice(_request) => {
+            // TODO: Device Flow: implementation
+            Err(TokenDeviceError {
+                message: TokenDeviceErrorStatus::AccessDenied,
+            }
+            .api_err())
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
