df26e6754b4d4b66970894a9dce69b42d2340f36
rmn-boiko <roman.bv20@gmail.com>
2025-04-30 15:46:39 +0200

    Add migration cleanup step

--- migrations/postgres/20250425151059_remove_is_admin_from_account.sql
+++ migrations/postgres/20250425151059_remove_is_admin_from_account.sql
@@ -1,8 +1,11 @@
+DELETE FROM auth_rebac_properties
+WHERE entity_type = 'account';
+
 INSERT INTO auth_rebac_properties (entity_type, entity_id, property_name, property_value)
 SELECT 
   'account' AS entity_type,
   id::VARCHAR AS entity_id,
-  'is_admin' AS property_name,
+  'account/is_admin' AS property_name,
   is_admin::TEXT AS property_value
 FROM accounts;
 
--- migrations/sqlite/20250425151059_remove_is_admin_from_account.sql
+++ migrations/sqlite/20250425151059_remove_is_admin_from_account.sql
@@ -1,10 +1,13 @@
 PRAGMA foreign_keys = OFF;
 
+DELETE FROM auth_rebac_properties
+WHERE entity_type = 'account';
+
 INSERT INTO auth_rebac_properties (entity_type, entity_id, property_name, property_value)
 SELECT 
   'account' AS entity_type,
   id AS entity_id,
-  'is_admin' AS property_name,
+  'account/is_admin' AS property_name,
   CASE is_admin
     WHEN 1 THEN 'true'
     ELSE 'false'
