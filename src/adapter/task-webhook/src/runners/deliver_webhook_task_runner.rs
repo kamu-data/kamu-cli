4650f2c4da029c4ab7a70e6961ae617456a157f9
Sergei Zaychenko <szaychenko@kamu.dev>
2025-07-16 10:53:43 +0300

    Fixed crash: webhook delivery runner now needs a transaction

--- src/adapter/task-webhook/src/runners/deliver_webhook_task_runner.rs
+++ src/adapter/task-webhook/src/runners/deliver_webhook_task_runner.rs
@@ -10,9 +10,16 @@
 use std::sync::Arc;
 
 use chrono::Utc;
+use database_common_macros::transactional_method1;
 use internal_error::InternalError;
 use kamu_task_system::*;
-use kamu_webhooks::{WebhookDeliveryWorker, WebhookEvent, WebhookEventID, WebhookSubscriptionID};
+use kamu_webhooks::{
+    WebhookDeliveryWorker,
+    WebhookEvent,
+    WebhookEventID,
+    WebhookEventRepository,
+    WebhookSubscriptionID,
+};
 
 use crate::TaskDefinitionWebhookDeliver;
 
@@ -24,16 +31,16 @@ use crate::TaskDefinitionWebhookDeliver;
     task_type: TaskDefinitionWebhookDeliver::TASK_TYPE,
 })]
 pub struct DeliverWebhookTaskRunner {
-    webhook_event_repo: Arc<dyn kamu_webhooks::WebhookEventRepository>,
+    catalog: dill::Catalog,
     webhook_delivery_worker: Arc<dyn WebhookDeliveryWorker>,
 }
 
 impl DeliverWebhookTaskRunner {
-    #[tracing::instrument(level = "debug", skip_all, fields(?task_webhook))]
-    async fn run_deliver_webhook(
+    #[transactional_method1(webhook_event_repo: Arc<dyn WebhookEventRepository>)]
+    async fn create_webhook_event(
         &self,
         task_webhook: TaskDefinitionWebhookDeliver,
-    ) -> Result<TaskOutcome, InternalError> {
+    ) -> Result<WebhookEvent, InternalError> {
         // Create and save webhook event
         let webhook_event_id = WebhookEventID::new(uuid::Uuid::new_v4());
         let webhook_event = WebhookEvent::new(
@@ -42,18 +49,28 @@ impl DeliverWebhookTaskRunner {
             task_webhook.webhook_payload,
             Utc::now(),
         );
-        self.webhook_event_repo
+        webhook_event_repo
             .create_event(&webhook_event)
             .await
             .int_err()?;
+        Ok(webhook_event)
+    }
+
+    #[tracing::instrument(level = "debug", skip_all, fields(?task_webhook))]
+    async fn run_deliver_webhook(
+        &self,
+        task_webhook: TaskDefinitionWebhookDeliver,
+    ) -> Result<TaskOutcome, InternalError> {
+        let task_id = task_webhook.task_id;
+
+        let webhook_subscription_id =
+            WebhookSubscriptionID::new(task_webhook.webhook_subscription_id);
+
+        let webhook_event = self.create_webhook_event(task_webhook).await?;
 
         match self
             .webhook_delivery_worker
-            .deliver_webhook(
-                task_webhook.task_id,
-                WebhookSubscriptionID::new(task_webhook.webhook_subscription_id),
-                webhook_event_id,
-            )
+            .deliver_webhook(task_id, webhook_subscription_id, webhook_event.id)
             .await
         {
             Ok(_) => Ok(TaskOutcome::Success(TaskResult::empty())),
