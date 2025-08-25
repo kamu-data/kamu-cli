9b30825d67749fc02916234b80206275c5dae936
Sergei Zaychenko <szaychenko@kamu.dev>
2025-08-25 11:48:55 +0300

    Marked 2 e2e tests as flaky

--- src/e2e/app/cli/postgres/tests/tests/test_smart_transfer_protocol.rs
+++ src/e2e/app/cli/postgres/tests/tests/test_smart_transfer_protocol.rs
@@ -404,6 +404,7 @@ kamu_cli_run_api_server_e2e_test!(
 kamu_cli_run_api_server_e2e_test!(
     storage = postgres,
     fixture = kamu_cli_e2e_repo_tests::test_smart_push_trigger_dependent_dataset_update_st,
+    extra_test_groups = "flaky",
     options = Options::default()
         .with_multi_tenant()
         .with_kamu_config(indoc::indoc!(
@@ -427,6 +428,7 @@ kamu_cli_run_api_server_e2e_test!(
 kamu_cli_run_api_server_e2e_test!(
     storage = postgres,
     fixture = kamu_cli_e2e_repo_tests::test_smart_push_trigger_dependent_dataset_update_mt,
+    extra_test_groups = "flaky",
     options = Options::default()
         .with_multi_tenant()
         .with_kamu_config(indoc::indoc!(
