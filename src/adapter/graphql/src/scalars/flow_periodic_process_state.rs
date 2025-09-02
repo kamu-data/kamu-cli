64666bc4485cc07ebd78ee320ebc2d4c65ab30a9
Sergei Zaychenko <szaychenko@kamu.dev>
2025-09-03 00:12:24 +0300

    Early draft of flow status badge supporting API

---
+++ src/adapter/graphql/src/scalars/flow_periodic_process_state.rs
@@ -0,0 +1,36 @@
+// Copyright Kamu Data, Inc. and contributors. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use chrono::{DateTime, Utc};
+
+use crate::prelude::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(SimpleObject, PartialEq, Eq)]
+pub struct FlowPeriodicProcessState {
+    pub effective_status: FlowPeriodicProcessStatus,
+    pub consecutive_failures: u32,
+    pub last_success_at: Option<DateTime<Utc>>,
+    pub last_attempt_at: Option<DateTime<Utc>>,
+    pub last_failure_at: Option<DateTime<Utc>>,
+    pub next_planned_at: Option<DateTime<Utc>>,
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Enum, Debug, Copy, Clone, Eq, PartialEq)]
+pub enum FlowPeriodicProcessStatus {
+    Active,
+    Failing,
+    PausedByUser,
+    StoppedAutomatically,
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
