673b4dd3a7a87cc854ba9e84ea0304d9d100ee33
Sergei Zaychenko <szaychenko@kamu.dev>
2025-09-04 18:51:30 +0300

    Sketched empty repository components

--- src/infra/flow-system/postgres/src/lib.rs
+++ src/infra/flow-system/postgres/src/lib.rs
@@ -13,8 +13,12 @@ pub use kamu_flow_system as domain;
 mod helpers;
 mod postgres_flow_configuration_event_store;
 mod postgres_flow_event_store;
+mod postgres_flow_process_state_query;
+mod postgres_flow_process_state_repository;
 mod postgres_flow_trigger_event_store;
 
 pub use postgres_flow_configuration_event_store::*;
 pub use postgres_flow_event_store::*;
+pub use postgres_flow_process_state_query::*;
+pub use postgres_flow_process_state_repository::*;
 pub use postgres_flow_trigger_event_store::*;
