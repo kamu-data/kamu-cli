0de41db69beabb35779395d3181d7c3a566da16f
Sergei Zaychenko <szaychenko@kamu.dev>
2025-09-10 20:20:18 +0300

    Simplified updating trigger state in process state

--- src/infra/flow-system/postgres/src/process_state/postgres_flow_process_state_repository.rs
+++ src/infra/flow-system/postgres/src/process_state/postgres_flow_process_state_repository.rs
@@ -188,8 +188,8 @@ impl FlowProcessStateRepository for PostgresFlowProcessStateRepository {
     async fn update_trigger_state(
         &self,
         flow_binding: &FlowBinding,
-        paused_manual: Option<bool>,
-        stop_policy: Option<FlowTriggerStopPolicy>,
+        paused_manual: bool,
+        stop_policy: FlowTriggerStopPolicy,
         trigger_event_id: EventID,
     ) -> Result<(), FlowProcessUpdateError> {
         // Load current state
