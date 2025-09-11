632da2bf9a23397711570235591855d9f05ae230
Dima Pristupa <dima@pristupa.dev>
2025-09-11 19:10:47 +0300

    AccountsMut::by_names(): vectorization

--- src/adapter/graphql/src/mutations/accounts_mut.rs
+++ src/adapter/graphql/src/mutations/accounts_mut.rs
@@ -124,22 +124,19 @@ impl AccountsMut {
     ) -> Result<Vec<AccountMut>> {
         let account_service = from_catalog_n!(ctx, dyn kamu_accounts::AccountService);
 
-        let mut res = Vec::new();
+        let domain_account_names = account_names.iter().map(AsRef::as_ref).collect::<Vec<_>>();
+        let lookup = account_service
+            .get_accounts_by_names(&domain_account_names)
+            .await?;
 
-        for account_name in account_names {
-            let account_name = account_name.into();
-
-            // TODO: PERF: Vectorize resolution
-            match account_service.account_by_name(&account_name).await? {
-                Some(account) => res.push(AccountMut::new(account)),
-                None if skip_missing => (),
-                None => {
-                    return Err(GqlError::gql(format!("Unresolved account: {account_name}")));
-                }
-            }
+        if !skip_missing && !lookup.not_found.is_empty() {
+            return Err(GqlError::gql(format!(
+                "Unresolved accounts: {}",
+                itertools::join(lookup.not_found.iter().map(|(name, _)| name), ",")
+            )));
         }
 
-        Ok(res)
+        Ok(lookup.found.into_iter().map(AccountMut::new).collect())
     }
 
     /// Create a new account
