d9cdcf7826c7f8680f38cbae26973ce9ad590bc9
Sergei Zaychenko <szaychenko@kamu.dev>
2025-09-16 12:30:22 +0300

    Sketched flow process state indexer

--- src/utils/event-sourcing/tests/test_aggregate.rs
+++ src/utils/event-sourcing/tests/test_aggregate.rs
@@ -51,6 +51,15 @@ impl CalcEventStore {
 
 #[async_trait::async_trait]
 impl EventStore<CalcState> for CalcEventStore {
+    fn get_all_events(&self, _opts: GetEventsOpts) -> EventStream<CalcEvents> {
+        use futures::StreamExt;
+        Box::pin(
+            tokio_stream::iter(self.0.lock().unwrap().clone())
+                .enumerate()
+                .map(|(i, e)| Ok((EventID::new(i64::try_from(i).unwrap()), e))),
+        )
+    }
+
     fn get_events(&self, _query: &(), _opts: GetEventsOpts) -> EventStream<CalcEvents> {
         use futures::StreamExt;
         Box::pin(
