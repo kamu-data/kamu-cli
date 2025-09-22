c79947bf4155b6ddec6448b34e46f3d8c9263be1
Dima Pristupa <dima@pristupa.dev>
2025-09-22 15:24:52 +0300

    InspectQueryCommand: use visitors

--- src/app/cli/src/commands/inspect_query_command.rs
+++ src/app/cli/src/commands/inspect_query_command.rs
@@ -10,9 +10,9 @@
 use std::io::Write;
 use std::sync::Arc;
 
-use chrono::SecondsFormat;
+use chrono::{DateTime, SecondsFormat, Utc};
 use console::style;
-use futures::TryStreamExt;
+use internal_error::ResultIntoInternal;
 use kamu::domain::*;
 use kamu_auth_rebac::RebacDatasetRegistryFacade;
 
@@ -37,37 +37,40 @@ impl InspectQueryCommand {
     ) -> Result<(), CLIError> {
         use odf::dataset::MetadataChainExt;
 
-        let mut blocks = resolved_dataset.as_metadata_chain().iter_blocks();
-        while let Some((block_hash, block)) = blocks.try_next().await? {
-            match &block.event {
-                odf::MetadataEvent::SetTransform(odf::metadata::SetTransform {
-                    inputs,
-                    transform,
-                }) => {
-                    self.render_transform(
-                        output,
-                        resolved_dataset.get_handle(),
-                        &block_hash,
-                        &block,
-                        inputs,
-                        transform,
-                    )?;
-                }
-                odf::MetadataEvent::SetPollingSource(odf::metadata::SetPollingSource {
-                    preprocess: Some(transform),
-                    ..
-                }) => {
-                    self.render_transform(
-                        output,
-                        resolved_dataset.get_handle(),
-                        &block_hash,
-                        &block,
-                        &Vec::new(),
-                        transform,
-                    )?;
-                }
-                _ => (),
-            }
+        let dataset_kind = resolved_dataset.get_kind();
+
+        let mut set_transform_visitor = odf::dataset::SearchSetTransformVisitor::new(dataset_kind);
+        let mut active_polling_source =
+            odf::dataset::SearchActivePollingSourceVisitor::new(dataset_kind);
+
+        resolved_dataset
+            .as_metadata_chain()
+            .accept(&mut [&mut set_transform_visitor, &mut active_polling_source])
+            .await
+            .int_err()?;
+
+        if let Some((block_hash, block)) = set_transform_visitor.into_inner().into_hashed_block() {
+            self.render_transform(
+                output,
+                resolved_dataset.get_handle(),
+                &block_hash,
+                block.system_time,
+                &block.event.inputs,
+                &block.event.transform,
+            )?;
+        }
+
+        if let Some((block_hash, block)) = active_polling_source.into_hashed_block()
+            && let Some(transform) = block.event.preprocess
+        {
+            self.render_transform(
+                output,
+                resolved_dataset.get_handle(),
+                &block_hash,
+                block.system_time,
+                &Vec::new(),
+                &transform,
+            )?;
         }
 
         Ok(())
@@ -78,7 +81,7 @@ impl InspectQueryCommand {
         output: &mut impl Write,
         dataset_handle: &odf::DatasetHandle,
         block_hash: &odf::Multihash,
-        block: &odf::MetadataBlock,
+        block_system_time: DateTime<Utc>,
         inputs: &Vec<odf::metadata::TransformInput>,
         transform: &odf::metadata::Transform,
     ) -> Result<(), std::io::Error> {
@@ -93,9 +96,7 @@ impl InspectQueryCommand {
             output,
             "{} {}",
             style("As Of:").dim(),
-            block
-                .system_time
-                .to_rfc3339_opts(SecondsFormat::AutoSi, true)
+            block_system_time.to_rfc3339_opts(SecondsFormat::AutoSi, true)
         )?;
 
         writeln!(output, "{}", style("Inputs:").dim())?;
