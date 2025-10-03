e09df231e0b7a03d31964fd7be9d5a996abdd3cc
Sergei Zaychenko <szaychenko@kamu.dev>
2025-10-03 16:03:42 +0300

    Garbage out

--- src/domain/flow.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Serialize, Deserialize)]
-enum FlowProcessEffectiveState {
-    // ... existing variants ...
-}
-
-#[derive(Serialize, Deserialize)]
-enum FlowProcessAutoStopReason {
-    // ... existing variants ...
-}
