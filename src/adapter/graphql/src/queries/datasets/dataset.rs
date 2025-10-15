b51c4b7d8813df46ead7c79b54f90bf147c2e71f
Dima Pristupa <dima@pristupa.dev>
2025-10-15 19:37:42 +0300

    Fix typos

--- src/adapter/graphql/src/queries/datasets/dataset.rs
+++ src/adapter/graphql/src/queries/datasets/dataset.rs
@@ -253,7 +253,7 @@ impl Dataset {
         DatasetCollaboration::new_with_access_check(ctx, &self.dataset_request_state).await
     }
 
-    /// Access to the dataset's webhooks management functionality
+    /// Access to the dataset's webhook management functionality
     async fn webhooks(&self) -> DatasetWebhooks {
         DatasetWebhooks::new(&self.dataset_request_state)
     }
