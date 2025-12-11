2b18cb8eb1603fe15c0391926270be83d30a3856
Dima Pristupa <dima@pristupa.dev>
2025-12-11 16:41:35 +0200

    datafusion: register array functions

--- src/domain/datasets/services/src/utils/data_frame_extra_data_fields_filter_applier.rs
+++ src/domain/datasets/services/src/utils/data_frame_extra_data_fields_filter_applier.rs
@@ -53,18 +53,49 @@ impl DataFrameExtraDataFieldsFilterApplier {
         df: DataFrameExt,
         filter: ExtraDataFieldsFilter,
     ) -> Result<DataFrameExt, DataFrameExtraDataFieldsFilterApplyError> {
+        use datafusion::logical_expr::expr::ScalarFunction;
         use datafusion::logical_expr::{Expr, col, lit};
+        use datafusion::scalar::ScalarValue;
 
         Self::validate_requested_extra_data_fields(&df, &filter)?;
 
         let filter_expr = filter
             .into_iter()
-            .map(|ExtraDataFieldFilter { field_name, values }| {
-                let values_as_lits = values.into_iter().map(lit).collect();
-                // field1 in [1, 2, 3]
-                col(field_name).in_list(values_as_lits, false)
-            })
-            // ((field1 in [1, 2, 3] AND field2 in [4, 5, 6]) AND field3 in [7, 8, 9])
+            .map(
+                |ExtraDataFieldFilter {
+                     field_name,
+                     values,
+                     is_array,
+                 }| {
+                    if is_array {
+                        let values_as_scalar = {
+                            let scalars_iter = values
+                                .into_iter()
+                                // TODO: PERF: new_utf8view()?
+                                .map(ScalarValue::new_utf8)
+                                .collect::<Vec<_>>();
+                            let list_array = ScalarValue::new_list_from_iter(
+                                scalars_iter.into_iter(),
+                                &datafusion::arrow::datatypes::DataType::Utf8,
+                                false,
+                            );
+
+                            ScalarValue::List(list_array)
+                        };
+
+                        // array_has_all(field1, [1, 2, 3]
+                        Expr::ScalarFunction(ScalarFunction::new_udf(
+                            (*ARRAY_HAS_ALL_UDF).clone(),
+                            vec![col(field_name), lit(values_as_scalar)],
+                        ))
+                    } else {
+                        let values_as_lits = values.into_iter().map(lit).collect();
+                        // field2 in [1, 2, 3]
+                        col(field_name).in_list(values_as_lits, false)
+                    }
+                },
+            )
+            // ((array_has_all(field1, [1, 2, 3]) AND field2 in [4, 5, 6]) AND field3 in [7, 8, 9])
             .reduce(Expr::and)
             // Safety: we use the NonEmpty<T>, so we will always have elements.
             .unwrap();
