a7eeedde8ba72da7b602428542a3b88ee2c7a467
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2025-12-23 18:06:18 -0800

    Upgrade prost and tonic

--- src/odf/metadata/src/engine/grpc_generated/engine/engine.tonic.rs
+++ src/odf/metadata/src/engine/grpc_generated/engine/engine.tonic.rs
@@ -10,7 +10,13 @@
 // @generated
 /// Generated client implementations.
 pub mod engine_client {
-    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    #![allow(
+        unused_variables,
+        dead_code,
+        missing_docs,
+        clippy::wildcard_imports,
+        clippy::let_unit_value
+    )]
     use tonic::codegen::http::Uri;
     use tonic::codegen::*;
     #[derive(Debug, Clone)]
@@ -30,10 +36,10 @@ pub mod engine_client {
     }
     impl<T> EngineClient<T>
     where
-        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T: tonic::client::GrpcService<tonic::body::Body>,
         T::Error: Into<StdError>,
-        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
-        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+        T::ResponseBody: Body<Data = Bytes> + std::marker::Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + std::marker::Send,
     {
         pub fn new(inner: T) -> Self {
             let inner = tonic::client::Grpc::new(inner);
@@ -51,13 +57,13 @@ pub mod engine_client {
             F: tonic::service::Interceptor,
             T::ResponseBody: Default,
             T: tonic::codegen::Service<
-                    http::Request<tonic::body::BoxBody>,
+                    http::Request<tonic::body::Body>,
                     Response = http::Response<
-                        <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                        <T as tonic::client::GrpcService<tonic::body::Body>>::ResponseBody,
                     >,
                 >,
-            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
-                Into<StdError> + Send + Sync,
+            <T as tonic::codegen::Service<http::Request<tonic::body::Body>>>::Error:
+                Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             EngineClient::new(InterceptedService::new(inner, interceptor))
         }
@@ -100,12 +106,9 @@ pub mod engine_client {
             tonic::Status,
         > {
             self.inner.ready().await.map_err(|e| {
-                tonic::Status::new(
-                    tonic::Code::Unknown,
-                    format!("Service was not ready: {}", e.into()),
-                )
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
             })?;
-            let codec = tonic::codec::ProstCodec::default();
+            let codec = tonic_prost::ProstCodec::default();
             let path = http::uri::PathAndQuery::from_static("/engine.Engine/ExecuteRawQuery");
             let mut req = request.into_request();
             req.extensions_mut()
@@ -120,12 +123,9 @@ pub mod engine_client {
             tonic::Status,
         > {
             self.inner.ready().await.map_err(|e| {
-                tonic::Status::new(
-                    tonic::Code::Unknown,
-                    format!("Service was not ready: {}", e.into()),
-                )
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
             })?;
-            let codec = tonic::codec::ProstCodec::default();
+            let codec = tonic_prost::ProstCodec::default();
             let path = http::uri::PathAndQuery::from_static("/engine.Engine/ExecuteTransform");
             let mut req = request.into_request();
             req.extensions_mut()
@@ -136,16 +136,22 @@ pub mod engine_client {
 }
 /// Generated server implementations.
 pub mod engine_server {
-    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    #![allow(
+        unused_variables,
+        dead_code,
+        missing_docs,
+        clippy::wildcard_imports,
+        clippy::let_unit_value
+    )]
     use tonic::codegen::*;
     /// Generated trait containing gRPC methods that should be implemented for
     /// use with EngineServer.
     #[async_trait]
-    pub trait Engine: Send + Sync + 'static {
+    pub trait Engine: std::marker::Send + std::marker::Sync + 'static {
         /// Server streaming response type for the ExecuteRawQuery method.
         type ExecuteRawQueryStream: tonic::codegen::tokio_stream::Stream<
                 Item = std::result::Result<super::RawQueryResponse, tonic::Status>,
-            > + Send
+            > + std::marker::Send
             + 'static;
         async fn execute_raw_query(
             &self,
@@ -154,7 +160,7 @@ pub mod engine_server {
         /// Server streaming response type for the ExecuteTransform method.
         type ExecuteTransformStream: tonic::codegen::tokio_stream::Stream<
                 Item = std::result::Result<super::TransformResponse, tonic::Status>,
-            > + Send
+            > + std::marker::Send
             + 'static;
         async fn execute_transform(
             &self,
@@ -162,14 +168,14 @@ pub mod engine_server {
         ) -> std::result::Result<tonic::Response<Self::ExecuteTransformStream>, tonic::Status>;
     }
     #[derive(Debug)]
-    pub struct EngineServer<T: Engine> {
+    pub struct EngineServer<T> {
         inner: Arc<T>,
         accept_compression_encodings: EnabledCompressionEncodings,
         send_compression_encodings: EnabledCompressionEncodings,
         max_decoding_message_size: Option<usize>,
         max_encoding_message_size: Option<usize>,
     }
-    impl<T: Engine> EngineServer<T> {
+    impl<T> EngineServer<T> {
         pub fn new(inner: T) -> Self {
             Self::from_arc(Arc::new(inner))
         }
@@ -221,10 +227,10 @@ pub mod engine_server {
     impl<T, B> tonic::codegen::Service<http::Request<B>> for EngineServer<T>
     where
         T: Engine,
-        B: Body + Send + 'static,
-        B::Error: Into<StdError> + Send + 'static,
+        B: Body + std::marker::Send + 'static,
+        B::Error: Into<StdError> + std::marker::Send + 'static,
     {
-        type Response = http::Response<tonic::body::BoxBody>;
+        type Response = http::Response<tonic::body::Body>;
         type Error = std::convert::Infallible;
         type Future = BoxFuture<Self::Response, Self::Error>;
         fn poll_ready(
@@ -263,7 +269,7 @@ pub mod engine_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = ExecuteRawQuerySvc(inner);
-                        let codec = tonic::codec::ProstCodec::default();
+                        let codec = tonic_prost::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
                                 accept_compression_encodings,
@@ -306,7 +312,7 @@ pub mod engine_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = ExecuteTransformSvc(inner);
-                        let codec = tonic::codec::ProstCodec::default();
+                        let codec = tonic_prost::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
                                 accept_compression_encodings,
@@ -322,20 +328,22 @@ pub mod engine_server {
                     Box::pin(fut)
                 }
                 _ => Box::pin(async move {
-                    Ok(http::Response::builder()
-                        .status(200)
-                        .header("grpc-status", tonic::Code::Unimplemented as i32)
-                        .header(
-                            http::header::CONTENT_TYPE,
-                            tonic::metadata::GRPC_CONTENT_TYPE,
-                        )
-                        .body(empty_body())
-                        .unwrap())
+                    let mut response = http::Response::new(tonic::body::Body::default());
+                    let headers = response.headers_mut();
+                    headers.insert(
+                        tonic::Status::GRPC_STATUS,
+                        (tonic::Code::Unimplemented as i32).into(),
+                    );
+                    headers.insert(
+                        http::header::CONTENT_TYPE,
+                        tonic::metadata::GRPC_CONTENT_TYPE,
+                    );
+                    Ok(response)
                 }),
             }
         }
     }
-    impl<T: Engine> Clone for EngineServer<T> {
+    impl<T> Clone for EngineServer<T> {
         fn clone(&self) -> Self {
             let inner = self.inner.clone();
             Self {
@@ -347,7 +355,9 @@ pub mod engine_server {
             }
         }
     }
-    impl<T: Engine> tonic::server::NamedService for EngineServer<T> {
-        const NAME: &'static str = "engine.Engine";
+    /// Generated gRPC service name
+    pub const SERVICE_NAME: &str = "engine.Engine";
+    impl<T> tonic::server::NamedService for EngineServer<T> {
+        const NAME: &'static str = SERVICE_NAME;
     }
 }
