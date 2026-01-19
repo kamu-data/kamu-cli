4ff201473642e0d1814ba8d7280770f686aaff2e
Dima Pristupa <dima@pristupa.dev>
2026-01-19 18:11:41 +0200

    MoleculeActivityMessage::WriteRequested: added

--- src/sku/molecule/domain/src/messages/molecule_activity_message.rs
+++ src/sku/molecule/domain/src/messages/molecule_activity_message.rs
@@ -21,12 +21,25 @@ const MOLECULE_ACTIVITY_MESSAGE_OUTBOX_VERSION: u32 = 1;
 /// Represents messages related to the announcements published in Molecule
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum MoleculeActivityMessage {
+    WriteRequested(MoleculeActivityMessageWriteRequested),
     Published(MoleculeActivityMessagePublished),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 impl MoleculeActivityMessage {
+    pub fn write_requested(
+        molecule_subject_account_name: odf::AccountName,
+        source_event_time: Option<DateTime<Utc>>,
+        activity_record: MoleculeDataRoomActivityPayloadRecord,
+    ) -> Self {
+        Self::WriteRequested(MoleculeActivityMessageWriteRequested {
+            molecule_subject_account_name,
+            source_event_time,
+            activity_record,
+        })
+    }
+
     pub fn published(
         event_time: DateTime<Utc>,
         molecule_account_id: odf::AccountID,
@@ -48,6 +61,15 @@ impl Message for MoleculeActivityMessage {
     fn version() -> u32 {
         MOLECULE_ACTIVITY_MESSAGE_OUTBOX_VERSION
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct MoleculeActivityMessageWriteRequested {
+    pub molecule_subject_account_name: odf::AccountName,
+    pub source_event_time: Option<DateTime<Utc>>,
+    pub activity_record: MoleculeDataRoomActivityPayloadRecord,
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
