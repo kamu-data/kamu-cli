cd380afa2391db062fc7345ef6fbcbee2b99b4cb
Dima Pristupa <dima@pristupa.dev>
2026-02-02 19:07:40 +0200

    MoleculeAppendGlobalDataRoomActivityUseCaseImpl: only request not write

--- src/sku/molecule/services/src/use_cases/activities/molecule_append_data_room_activity_use_case_impl.rs
+++ src/sku/molecule/services/src/use_cases/activities/molecule_append_data_room_activity_use_case_impl.rs
@@ -10,8 +10,7 @@
 use std::sync::Arc;
 
 use chrono::{DateTime, Utc};
-use internal_error::{InternalError, ResultIntoInternal};
-use kamu_core::PushIngestResult;
+use internal_error::ResultIntoInternal;
 use kamu_molecule_domain::*;
 use messaging_outbox::{Outbox, OutboxExt};
 
@@ -24,37 +23,6 @@ use crate::MoleculeGlobalDataRoomActivitiesService;
 pub struct MoleculeAppendGlobalDataRoomActivityUseCaseImpl {
     global_data_room_activities_service: Arc<dyn MoleculeGlobalDataRoomActivitiesService>,
     outbox: Arc<dyn Outbox>,
-}
-
-impl MoleculeAppendGlobalDataRoomActivityUseCaseImpl {
-    async fn get_offset_after_append(
-        &self,
-        global_activities_dataset: &dyn odf::Dataset,
-        new_head: &odf::Multihash,
-    ) -> Result<u64, InternalError> {
-        // Extract the event from the new head
-        let new_odf_event = global_activities_dataset
-            .as_metadata_chain()
-            .get_block(new_head)
-            .await
-            .int_err()?
-            .event;
-
-        // We expect it to be AddData
-        let odf::metadata::MetadataEvent::AddData(add_data) = new_odf_event else {
-            unreachable!("We just appended data, the latest event must be AddData")
-        };
-
-        // We expect just 1 record to be appended
-        let offset = if let Some(new_data) = add_data.new_data {
-            assert_eq!(new_data.num_records(), 1);
-            new_data.offset_interval.end
-        } else {
-            unreachable!("We just appended data, new_data must be Some")
-        };
-
-        Ok(offset)
-    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -75,58 +43,25 @@ impl MoleculeAppendGlobalDataRoomActivityUseCase
         source_event_time: Option<DateTime<Utc>>,
         activity_record: MoleculeDataRoomActivityPayloadRecord,
     ) -> Result<(), MoleculeAppendDataRoomActivityError> {
-        // Gain write access to global activities dataset
-        let global_data_room_activities_writer = self
+        // Check dataset access before sending the message
+        let _ = self
             .global_data_room_activities_service
             .writer(&molecule_subject.account_name, true) // TODO: try to create once as start-up job?
             .await
             .map_err(MoleculeDatasetErrorExt::adapt::<MoleculeAppendDataRoomActivityError>)?;
 
-        // Append new activity record
-        let new_changelog_record = MoleculeDataRoomActivityChangelogInsertionRecord {
-            op: odf::metadata::OperationType::Append,
-            payload: activity_record,
-        };
-
-        let push_res = global_data_room_activities_writer
-            .push_ndjson_data(new_changelog_record.to_bytes(), source_event_time)
-            .await?;
-
-        // Check commit status
-        match push_res {
-            PushIngestResult::UpToDate => {
-                unreachable!("We just created a new announcement, it cannot be up-to-date")
-            }
-            PushIngestResult::Updated {
-                system_time: insertion_system_time,
-                new_head,
-                ..
-            } => {
-                // We need to know offset for message
-                let offset = self
-                    .get_offset_after_append(
-                        global_data_room_activities_writer
-                            .get_write_checked_dataset()
-                            .as_ref(),
-                        &new_head,
-                    )
-                    .await?;
-
-                // Notify external listeners
-                self.outbox
-                    .post_message(
-                        MESSAGE_PRODUCER_MOLECULE_ACTIVITY_SERVICE,
-                        MoleculeActivityMessage::published(
-                            insertion_system_time,
-                            molecule_subject.account_id.clone(),
-                            offset,
-                            new_changelog_record.payload,
-                        ),
-                    )
-                    .await
-                    .int_err()?;
-            }
-        }
+        // Request asynchronous write
+        self.outbox
+            .post_message(
+                MESSAGE_PRODUCER_MOLECULE_ACTIVITY_SERVICE,
+                MoleculeActivityMessage::write_requested(
+                    molecule_subject.account_name.clone(),
+                    source_event_time,
+                    activity_record,
+                ),
+            )
+            .await
+            .int_err()?;
 
         Ok(())
     }
