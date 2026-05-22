4c7667c55be8d2cc499216707a950519e39dae79
Dima Pristupa <dima@pristupa.dev>
2026-05-22 19:32:33 +0300

    alloy dep breakdown

--- src/utils/crypto-eip712-utils/src/eip712_typed_data.rs
+++ src/utils/crypto-eip712-utils/src/eip712_typed_data.rs
@@ -7,8 +7,8 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use alloy::dyn_abi::TypedData as AlloyEip712TypedData;
-use alloy::primitives::B256;
+use alloy_core::dyn_abi::TypedData as AlloyEip712TypedData;
+use alloy_core::primitives::B256;
 use internal_error::{InternalError, ResultIntoInternal};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
