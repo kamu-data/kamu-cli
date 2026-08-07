152672c1ec6bef9620c4f4e061edc9702c63b1d1
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2026-08-07 12:13:32 -0700

    Respect DatasetName length limit in name generator

--- src/domain/datasets/services/tests/tests/utils/test_dataset_name_generator.rs
+++ src/domain/datasets/services/tests/tests/utils/test_dataset_name_generator.rs
@@ -14,32 +14,47 @@ use pretty_assertions::assert_eq;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[rstest::rstest]
-#[case("/path/to/file", "00000000-1111-2222-3333-444444444444-path-to-file")]
-#[case(
-    "/path/to/%20/file",
-    "00000000-1111-2222-3333-444444444444-path-to-file"
-)]
-#[case(
-    "/path/to/file.txt",
-    "00000000-1111-2222-3333-444444444444-path-to-file-txt"
-)]
-#[case(
-    "path/to/file.txt",
-    "00000000-1111-2222-3333-444444444444-path-to-file-txt"
-)]
+#[case("/file.txt", "00000000-1111-2222-3333-444444444444-file-txt")]
 #[case("file.txt", "00000000-1111-2222-3333-444444444444-file-txt")]
-#[case("file.tar.gz", "00000000-1111-2222-3333-444444444444-file-tar-gz")]
+#[case("/path/to/file", "00000000-1111-2222-3333-444444444444-file")]
+#[case("/path/to/file.txt", "00000000-1111-2222-3333-444444444444-file-txt")]
+#[case("path/to/file.txt", "00000000-1111-2222-3333-444444444444-file-txt")]
+#[case("/path/to/%20/file", "00000000-1111-2222-3333-444444444444-file")]
 #[case(
-    "-name--with---dashes----",
+    "/-name--with---dashes----",
     "00000000-1111-2222-3333-444444444444-name-with-dashes"
 )]
 #[case(
-    "%20file%20with%20encoded%20spaces.txt",
+    "/name%20with%20spaces",
+    "00000000-1111-2222-3333-444444444444-name-with-spaces"
+)]
+#[case(
+    "/name%20with%20mixed%20%D1%8E%D0%BD%D0%B8%D0%BA%D0%BE%D0%B4.txt",
+    "00000000-1111-2222-3333-444444444444-name-with-mixed-txt"
+)]
+#[case(
+    "/%20file%20with%20encoded%20spaces.txt",
     "00000000-1111-2222-3333-444444444444-file-with-encoded-spaces-txt"
 )]
 #[case(
-    "%2Ffile%2Fwith%2Fencoded%2Fslashes.txt",
+    "/%2Ffile%2Fwith%2Fencoded%2Fslashes.txt",
     "00000000-1111-2222-3333-444444444444-file-with-encoded-slashes-txt"
+)]
+#[case(
+    "/a/very/very/very/very/very/very/very/very/very/very/very/very/very/very/very/very/very/long/\
+     path/to/a/file.txt",
+    "00000000-1111-2222-3333-444444444444-file-txt"
+)]
+// Should be truncated to `DatasetName::MAX_LEN` characters
+#[case(
+    "/a-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-\
+     file-name.txt",
+    "00000000-1111-2222-3333-444444444444-a-very-very-very-very-very-very-very-very-very-very-very-very-v"
+)]
+// "/юникод.txt"
+#[case(
+    "/%D1%8E%D0%BD%D0%B8%D0%BA%D0%BE%D0%B4.txt",
+    "00000000-1111-2222-3333-444444444444-txt"
 )]
 fn test_dataset_name_generator(#[case] input: &str, #[case] expected: &str) {
     let static_uuid_v4 = uuid::Uuid::parse_str("00000000-1111-2222-3333-444444444444").unwrap();
@@ -48,6 +63,9 @@ fn test_dataset_name_generator(#[case] input: &str, #[case] expected: &str) {
     let name = DatasetNameGenerator::based_on_collection_path_with_uuid(&path, static_uuid_v4);
 
     assert_eq!(expected, name.as_str());
+
+    // TODO: This likely needs to be ensured by `DatasetName` type itself
+    assert!(name.len() <= odf::DatasetName::MAX_LEN);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
